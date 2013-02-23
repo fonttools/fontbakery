b11e60529add9f333b28ae11b0713ca611592004
xen <m@xen.ru>
2013-02-24 01:31:13 +0700

    App load

---
+++ setup.py
@@ -0,0 +1,28 @@
+# coding: utf-8
+
+from setuptools import setup
+
+setup(
+    name="Font Bakery",
+    version='0.1',
+    url='https://github.com/xen/bakery',
+    description='',
+    author='Mikhail Kashkin',
+    author_email='mkashkin@gmail.com',
+    packages=["bakery"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy',
+        'Flask-Babel',
+        'Flask-Mail',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GPLv3 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
