4547f0f95f04254a76d5d09d68899891892a9374
xen <m@xen.ru>
2013-04-26 03:08:18 +0300

    Support of new oauth API

--- bakery/settings/views.py
+++ bakery/settings/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import datetime
-from flask import Blueprint, render_template, request, flash, g, redirect, url_for
+import logging
+from flask import Blueprint, render_template, request, flash, g, redirect, url_for, session
 
 from ..extensions import github, db
 from ..decorators import login_required
@@ -25,10 +26,11 @@ def repos():
 @login_required
 @settings.route('/update', methods=['POST'])
 def update():
+    auth = github.get_session(token = session['token'])
     if g.user is not None:
-        resp = github.get('/user/repos', data = {'type': 'public'})
-        if resp.status == 200:
-            _repos = resp.data
+        resp = auth.get('/user/repos', data = {'type': 'public'})
+        if resp.status_code == 200:
+            _repos = resp.json()
             cache = ProjectCache.query.filter_by(login=g.user.login).first()
             if not cache:
                 cache = ProjectCache()
@@ -44,7 +46,6 @@ def update():
             flash('Unable to load repos list.')
     return redirect(url_for('settings.repos'))
 
-
 @login_required
 @settings.route('/profile', methods=['GET', 'POST'])
 def profile():
@@ -57,28 +58,73 @@ def profile():
             flash('Unable to load repos list.')
     return render_template('settings/repos.html', repos=_repos)
 
+HOOK_URL = 'http://requestb.in/185gvyw1'
+
 @login_required
-@settings.route('/addhook', methods=['GET'])
-def addhook(full_name=''):
-    z = request
-    _repos = None
-    if g.user is not None:
-        resp = github.get('/user/repos', data = {'type': 'public'})
-        if resp.status == 200:
-            _repos = resp.data
+@settings.route('/addhook/<path:full_name>') #, methods=['GET'])
+def addhook(full_name):
+    #import ipdb; ipdb.set_trace()
+    old_hooks = github.get('/repos/%s/hooks' % full_name)
+    if old_hooks.status != 200:
+        logging.error('Repos API reading error for user %s' % g.user.login)
+        flash('Github API access error, please try again later')
+        return redirect(url_for('settings.repos'))
+
+    exist_id = False
+    if old_hooks.data:
+        for i in old_hooks.data:
+            if i.has_key('name') and i['name'] == 'web':
+                if i.has_key('config') and i['config'].has_key('url') \
+                    and i['config']['url'] == HOOK_URL:
+                    exist_id = i['id']
+
+    import ipdb; ipdb.set_trace()
+    if exist_id:
+        logging.warn('Delete old webhook for user %s, repo %s and id %s' % (g.user.login, full_name, exist_id))
+        resp = github.delete('/repos/%(full_name)s/hooks/%(id)s' % {'full_name': full_name, 'id': exist_id})
+        if resp.status != 204:
+            flash('Error deleting old webhook, delete if manually or retry')
+            return redirect(url_for('settings.repos'))
+
+    resp = github.post('/repos/%(full_name)s/hooks' % {'full_name': full_name},
+        data = {
+            'name':'web',
+            'active': True,
+            'events': ['push'],
+            'config': {
+                'url': HOOK_URL,
+                'content_type': 'json',
+                'secret': '11' # TODO: sign from name and SECRET.
+            }
+         },
+         format = 'json',
+         headers = {'Authorization': 'token %s' % (github.make_client(token=None).token.key)}
+    )
+    if resp.status < 300: # no errors, in 2xx range
+        project = Project.query.filter_by(login = g.user.login, full_name = full_name).first()
+        if not project:
+            project = Project(
+                login = g.user.login, 
+                full_name = full_name,
+            )
+        project_data = github.get('/repos/%s' % full_name)
+        if project_data.status == 200:
+            project.cache_update(data = project_data.data)
         else:
-            flash('Unable to load repos list.')
-    return render_template('settings/repos.html', repos=_repos)
+            flash('Repository information update error')
+            return redirect(url_for('settings.repos'))
+        db.session.add(project)
+        db.session.commit()
+    else:
+        logging.error('Web hook registration error for %s' % full_name)
+        flash('Repository webhook update error')
+        return redirect(url_for('settings.repos'))
+
+    flash('Added webhook for %s.' % (full_name))
+    return redirect(url_for('settings.repos'))
 
 @login_required
 @settings.route('/delhook', methods=['GET'])
-def addhook(full_name=''):
-    z = request
-    _repos = None
-    if g.user is not None:
-        resp = github.get('/user/repos', data = {'type': 'public'})
-        if resp.status == 200:
-            _repos = resp.data
-        else:
-            flash('Unable to load repos list.')
-    return render_template('settings/repos.html', repos=_repos)
+def delhook(full_name=''):
+    flash('Deleted webhook for %s to list.' % full_name)
+    return redirect(url_for('settings.repos'))
