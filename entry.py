c886707ade84f9cff9abfb8b62bbed9d25a267de
xen <m@xen.ru>
2013-06-18 18:18:05 +0300

    Log to file

--- entry.py
+++ entry.py
@@ -5,5 +5,11 @@ app.config['DEBUG'] = True
 app.config.from_object('config')
 app.config.from_pyfile('local.cfg', silent=True)
 
+import logging
+LOG_FILENAME = 'data/run.log'
+logging.basicConfig(filename=LOG_FILENAME, level=logging.INFO)
+
+logging.info('Project start')
+
 if __name__ == '__main__':
     app.run(port=5000)
