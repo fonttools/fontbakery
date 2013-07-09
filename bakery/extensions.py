1ffc2e1912d3c31227ed35a509eadedaae8657a9
xen <m@xen.ru>
2013-07-09 04:36:33 +0300

    Cleanup and activate celery

--- bakery/extensions.py
+++ bakery/extensions.py
@@ -7,17 +7,8 @@ db = SQLAlchemy()
 from flask.ext.mail import Mail
 mail = Mail()
 
-# from celery import Celery
-# celery = Celery()
-
-# from flask.ext.rauth import RauthOAuth2
-
-# github = RauthOAuth2(
-#     name='github',
-#     base_url='https://api.github.com/',
-#     access_token_url='https://github.com/login/oauth/access_token',
-#     authorize_url='https://github.com/login/oauth/authorize'
-# )
+from celery import Celery
+celery = Celery()
 
 from rauth.service import OAuth2Service
 
