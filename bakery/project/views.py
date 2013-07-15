9dffa4b538c24420df4c229a3b72dae5f1a83af6
xen <m@xen.ru>
2013-07-16 01:45:32 +0300

    Made project data access code more restrictive

--- bakery/project/views.py
+++ bakery/project/views.py
@@ -1,4 +1,5 @@
 # coding: utf-8
+#pylint:disable-msg=E1101
 import logging
 
 from flask import (Blueprint, render_template, g, flash, request,
@@ -7,8 +8,7 @@ from flask.ext.babel import gettext as _
 
 from ..decorators import login_required
 # from ..extensions import db
-from ..tasks import (add_logger, git_clone, process_project, project_state_get,
-    project_state_save, project_state_push, remove_logger, read_tree, read_license,
+from ..tasks import (add_logger, process_project, project_state_push, remove_logger, read_tree, read_license,
     read_metadata, save_metadata, read_description, save_description, read_log, read_yaml,
     project_tests, clone_and_process)
 from .models import Project
@@ -27,21 +27,22 @@ def before_request():
 @project.route('/bump', methods=['GET'])
 def bump():
     project_id = request.args.get('project_id')
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    #pylint:disable-msg=E1101
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
     logging.info('Update for project %s by %s' % (project_id, g.user.login))
-    clone_and_process.delay(project)
-    flash(_("Git %s was updated" % project.clone))
+    clone_and_process.delay(p)
+    flash(_("Git %s was updated" % p.clone))
     return redirect(url_for('project.fonts', project_id = project_id))
 
 @login_required
 @project.route('/<int:project_id>/setup', methods=['GET', 'POST'])
 def setup(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
     state = project.state
 
     #import ipdb; ipdb.set_trace()
     if request.method == 'GET':
-        return render_template('project/setup.html', project = project, state = state,
+        return render_template('project/setup.html', project = p, state = state,
             subsetvals = DEFAULT_SUBSET_LIST)
     else:
         if request.form.get('step') == '2':
@@ -50,7 +51,7 @@ def setup(project_id):
                 state['license_file'] = request.form.get('license_file')
             else:
                 flash(_("Wrong license_file value, must be an error"))
-                return render_template('project/setup.html', project = project, state = state,
+                return render_template('project/setup.html', project = p, state = state,
                     subsetvals = DEFAULT_SUBSET_LIST)
 
             if request.form.get('rename') == 'yes':
@@ -62,7 +63,7 @@ def setup(project_id):
             for i in ufo_dirs:
                 if i not in state['ufo_dirs']:
                     flash(_("Wrong ufo_dir value, must be an error"))
-                    return render_template('project/setup.html', project = project, state = state,
+                    return render_template('project/setup.html', project = p, state = state,
                         subsetvals = DEFAULT_SUBSET_LIST)
                 if not state['out_ufo'].get(i):
                     # define font name based on ufo folder name.
@@ -81,7 +82,7 @@ def setup(project_id):
                     assert i in DEFAULT_SUBSET_LIST
                 except AssertionError:
                     flash('Subset value is wrong')
-                    return render_template('project/setup.html', project = project, state = state,
+                    return render_template('project/setup.html', project = p, state = state,
                         subsetvals = DEFAULT_SUBSET_LIST)
 
             state['subset'] = subset_list
@@ -97,12 +98,12 @@ def setup(project_id):
             # setup is done, now you can process files
             state['autoprocess'] = True
 
-            project.save_state()
+            p.save_state()
 
             if request.form.get('rename') == 'yes':
-                return render_template('project/setup2.html', project = project, state = state)
+                return render_template('project/setup2.html', project = p, state = state)
             else:
-                flash(_("Repository %s has been updated" % project.clone))
+                flash(_("Repository %s has been updated" % p.clone))
                 fh = add_logger(login = g.user.login, project_id = project_id)
 
                 # push check before project process
@@ -124,7 +125,7 @@ def setup(project_id):
                 else:
                     flash(_("Wrong parameter provided for ufo folder name"))
             state['out_ufo'] = out_ufo
-            project.save_state()
+            p.save_state()
 
             # push check before project process
             if project_state_push(login = g.user.login, project_id = project_id):
@@ -140,69 +141,69 @@ def setup(project_id):
 # @project.route('/<int:project_id>', methods=['GET'])
 @project.route('/<int:project_id>/', methods=['GET'])
 def fonts(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    if project.state.get('autoprocess'):
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    if p.state.get('autoprocess'):
         tree = read_tree(login = g.user.login, project_id = project_id)
-        return render_template('project/fonts.html', project = project, tree = tree)
+        return render_template('project/fonts.html', project = p, tree = tree)
     else:
         return redirect(url_for('project.setup', project_id = project_id))
 
 @project.route('/<int:project_id>/license', methods=['GET'])
 def plicense(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    license = read_license(login = g.user.login, project_id = project_id)
-    return render_template('project/license.html', project = project, license = license)
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    license = read_license(login = g.user.login, project_id = p.id)
+    return render_template('project/license.html', project = p, license = license)
 
 @project.route('/<int:project_id>/ace', methods=['GET'])
 def ace(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    metadata, metadata_new = read_metadata(login = g.user.login, project_id = project_id)
-    return render_template('project/ace.html', project = project,
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    metadata, metadata_new = read_metadata(login = g.user.login, project_id = p.id)
+    return render_template('project/ace.html', project = p,
         metadata = metadata, metadata_new = metadata_new)
 
 @project.route('/<int:project_id>/ace', methods=['POST'])
 def ace_save(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    save_metadata(login = g.user.login, project_id = project_id,
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    save_metadata(login = g.user.login, project_id = p.id,
         metadata = request.form.get('metadata'),
         del_new = request.form.get('delete', None))
     flash('METADATA.json saved')
-    return redirect(url_for('project.ace', project_id=project_id))
+    return redirect(url_for('project.ace', project_id=p.id))
 
 
 @project.route('/<int:project_id>/description_edit', methods=['GET'])
 def description_edit(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    description = read_description(login = g.user.login, project_id = project_id)
-    return render_template('project/description.html', project = project,
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    description = read_description(login = g.user.login, project_id = p.id)
+    return render_template('project/description.html', project = p,
         description = description)
 
 @project.route('/<int:project_id>/description_save', methods=['POST'])
 def description_save(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    save_description(login = g.user.login, project_id = project_id,
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    save_description(login = g.user.login, project_id = p.id,
         description = request.form.get('description'))
     flash('Description saved')
-    return redirect(url_for('project.description_edit', project_id=project_id, state=state))
+    return redirect(url_for('project.description_edit', project_id=p.id))
 
 @project.route('/<int:project_id>/log', methods=['GET'])
 def buildlog(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    log = read_log(login = g.user.login, project_id = project_id)
-    return render_template('project/log.html', project = project,
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    log = read_log(login = g.user.login, project_id = p.id)
+    return render_template('project/log.html', project = p,
         log = log)
 
 @project.route('/<int:project_id>/yaml', methods=['GET'])
 def bakeryyaml(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    yaml = read_yaml(login = g.user.login, project_id = project_id)
-    return render_template('project/yaml.html', project = project,
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    yaml = read_yaml(login = g.user.login, project_id = p.id)
+    return render_template('project/yaml.html', project = p,
         yaml = yaml)
 
 @project.route('/<int:project_id>/tests', methods=['GET'])
 def tests(project_id):
-    project = Project.query.filter_by(login = g.user.login, id = project_id).first()
-    tests = project_tests(login = g.user.login, project_id = project_id)
-    return render_template('project/tests.html', project = project,
-        tests = tests)
+    p = Project.query.filter_by(login = g.user.login, id = project_id).first()
+    test_result = project_tests(login = g.user.login, project_id = p.id)
+    return render_template('project/tests.html', project = p,
+        tests = test_result)
 
