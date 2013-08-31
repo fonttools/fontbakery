fonttools/fontbakery

commit 085515a83bdc265ce82f16d1169ebe3195aed326

Refactor filestree() to be available to any page
* state.py: Refactor rwalk() as walkWithoutGit() using os.walk()
* models.py: Refactor tree_in() as treeFromFilesystem(dir)
* Remove extrajs block from fonts.html and move folding JS to main.js
* Add filestree('out') to yaml.html
* Rework nav() to show Tests/Files for Upstream/Results (orthogonal UX)

bakery/project/models.py
@@ -20,7 +20,7 @@ from flask import current_app, json
 from ..decorators import lazy_property
 from ..extensions import db
 
-from .state import (project_state_get, project_state_save, rwalk)
+from .state import (project_state_get, project_state_save, walkWithoutGit)
 
 
 class Project(db.Model):
@@ -103,10 +103,24 @@ class Project(db.Model):
         else:
             return ''
 
-    def tree_in(self):
-        """ Read files tree in repository folder on request """
+    def treeFromFilesystem(self, dir=None):
+        """
+        Read files tree in specied directory
+        
+        :param dir: handle for tree, either 'in' or 'out'
+        
+        Returns:
+            dict: Dictionary of file and directory strings
+        """
         DATA_ROOT = current_app.config.get('DATA_ROOT')
-        return rwalk(os.path.join(DATA_ROOT, '%(login)s/%(id)s.in/' % self))
+        if dir == 'in':
+            dict = walkWithoutGit(os.path.join(DATA_ROOT, '%(login)s/%(id)s.in/' % self))
+        if dir == 'out':
+            dict = walkWithoutGit(os.path.join(DATA_ROOT, '%(login)s/%(id)s.out/' % self))
+        else:
+            dict = { 'Sorry': 'Filesystem unavailable' }
+        return dict
+        
 
 
     def save_asset(self, name = None, data = None, **kwarg):

bakery/project/state.py
@@ -22,25 +22,23 @@ import os
 ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..'))
 DATA_ROOT = os.path.join(ROOT, 'data')
 
-def rwalk(path):
+def walkWithoutGit(path):
     """
     Recursively walk a file system path, excluding .git folders
     
     :param path: path to walk down
     
     Returns:
-        h: Dictionary of file and directory paths
+        dict: Dictionary of file and directory strings
     """
-    h = {}
-    cd = os.path.abspath(path)
-    fs = os.listdir(path)
-    for f in fs:
-        cf = os.path.join(cd, f)
-        if os.path.isfile(cf):
-            h[f] = {}
-        elif os.path.isdir(cf) and not cf.endswith('.git'):
-            h[f] = rwalk(cf)
-    return h
+    dict = {}
+    for root, dirs, files in os.walk(path, topdown=False):
+        for name in files:
+            h[name] = {}
+        for name in dirs:
+            if not name == '.git':
+                h[name] = {}
+    return dict
 
 def load_yaml(default_yml, yml = None):
     """

bakery/templates/project/fonts.html
@@ -13,26 +13,11 @@
 
 <h4>{{ _("Repository Files") }}</h4>
 {% from 'project/macros.html' import filestree with context %}
-{{ filestree() }}
+{{ filestree('in') }}
 
 {% if license %}
 <h4>{{ _("License File") }}</h4>
 <pre>{{ license }}</pre>
 {% endif %}
-
-{% endblock %}
-
-{% block extrajs %}
-<script>
-$(document).ready(function(){
-  $('.mothership').click(function(){
-    $(this).children('ul').slideToggle();
-  });
-  $('.mother_of_glyphs').click(function(){
-    event.stopPropagation();
-    $(this).children('ul').slideToggle();
-  });
-});
-</script>
 
 {% endblock %}

bakery/templates/project/macros.html
@@ -11,26 +11,28 @@
                Upstream <i class="caret"></i>
           </a>
           <ul class="dropdown-menu">
-              <li {% if active == 'ufonts' %}class="active" {% endif %}><a href="{{ url_for('project.utests', project_id=project.id) }}">Upstream tests</a></li>
-              <li {% if active == 'fonts' %}class="active" {% endif %}><a href="{{ url_for('project.fonts', project_id=project.id) }}">Browse Files</a></li>
+              <li {% if active == 'ufonts' %}class="active" {% endif %}><a href="{{ url_for('project.utests', project_id=project.id) }}">Tests</a></li>
+              <li {% if active == 'fonts' %}class="active" {% endif %}><a href="{{ url_for('project.fonts', project_id=project.id) }}">Files</a></li>
           </ul>
         </li>
       {% if project.config['local'].get('setup', None) %}
           <li {% if active == 'log' %}class="active" {% endif %}><a href="{{ url_for('project.log', project_id=project.id ) }}" >Log</a></li>
           <li class="dropdown {% if active in ['yaml', 'metadatajson', 'description', 'rfonts'] %}active{% endif %}">
             <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                 _out <i class="caret"></i>
+                 Results <i class="caret"></i>
             </a>
             <ul class="dropdown-menu">
 <!-- TODO: Add a files page for browsing the _out directory, reading directly from the file system
                 <li class="nav-header">Folders</li>
                 <li {% if active == 'fonts' %}class="active" {% endif %}><a href="{{ url_for('project.fonts', project_id=project.id) }}">Browse</a></li>
 -->
-                <li {% if active == 'rfonts' %}class="active" {% endif %}><a href="{{ url_for('project.rtests', project_id=project.id) }}">Results tests</a></li>
+                <li {% if active == 'rfonts' %}class="active" {% endif %}><a href="{{ url_for('project.rtests', project_id=project.id) }}">Tests</a></li>
                 <li class="nav-header">Read Only Files</li>
-                <li {% if active == 'yaml' %}class="active" {% endif %}><a href="{{ url_for('project.bakeryyaml', project_id=project.id ) }}" >bakery.yaml</a></li>
+                <li {% if active == 'yaml' %}class="active" {% endif %}><a href="{{ url_for('project.bakeryyaml', project_id=project.id ) }}" >Files</a></li>
                 <li class="nav-header">Editable Files</li>
+
                 <li {% if active == 'metadatajson' %}class="active" {% endif %}><a href="{{ url_for('project.metadatajson', project_id=project.id ) }}" >METADATA.json</a></li>
+
                 <li {% if active == 'description' %}class="active" {% endif %}><a href="{{ url_for('project.description_edit', project_id=project.id ) }}" >DESCRIPTION.en_us.html</a></li>
             </ul>
           </li>
@@ -48,7 +50,7 @@
     <ul>
       <li><a><i class="icon-folder-open"></i></a></li>
       <ul>
-      {%- for key, value in project.tree_in().items() recursive %}
+      {%- for key, value in project.treeFromFilesystem(tree).items() recursive %}
         <li class="{% if key == 'glyphs' and value %}mother_of_glyphs{% elif key.lower().endswith('.ufo') %}mothership{% endif %}">
           <a {{ highlight(key) }}>{% if key == '' %}
             &nbsp;

bakery/templates/project/yaml.html
@@ -6,6 +6,7 @@
 {% from 'project/macros.html' import nav with context%}
 {{ nav('yaml') }}
 
+<h4>{{ _("bakery.yaml File") }}</h4>
 {% if project.config['local'].get('bakery_yaml_in_sync', None) %}
   <div class="alert alert-info">Your setup is saved in the upstream repository, in a <code><a href="{{ url_for('project.bakeryyaml', project_id=project.id ) }}" >bakery.yaml</a></code> file, displayed below.</div>
 {% else %}
@@ -14,6 +15,11 @@
 
 <div id="editor"></div>
 <textarea name="yaml">{{ yaml }}</textarea>
+
+<h4>{{ _("Repository Files") }}</h4>
+{% from 'project/macros.html' import filestree with context %}
+{{ filestree('out') }}
+
 {% endblock %}
 
 {% block extrajs %}

static/main.js
@@ -19,20 +19,20 @@ http://github.com/xen/fontbakery/
 
 */
 $(document).ready(function() {
-    $('a[data-confirm]').click(function(ev) {
-        var href = $(this).attr('href');
-        if (!$('#dataConfirmModal').length) {
-            $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
-        } 
-        $('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
-        $('#dataConfirmOK').attr('href', href);
-        $('#dataConfirmModal').modal({show:true});
-        return false;
-    });
+
+// Confirm Modal used to confirm license/name permissions
+$('a[data-confirm]').click(function(ev) {
+    var href = $(this).attr('href');
+    if (!$('#dataConfirmModal').length) {
+        $('body').append('<div id="dataConfirmModal" class="modal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button><h3 id="dataConfirmLabel">Please Confirm</h3></div><div class="modal-body"></div><div class="modal-footer"><button class="btn" data-dismiss="modal" aria-hidden="true">Cancel</button><a class="btn btn-primary" id="dataConfirmOK">OK</a></div></div>');
+    } 
+    $('#dataConfirmModal').find('.modal-body').text($(this).attr('data-confirm'));
+    $('#dataConfirmOK').attr('href', href);
+    $('#dataConfirmModal').modal({show:true});
+    return false;
 });
 
-$(document).ready(function () {
-
+// #notify is in navbar, showing worker state 
 var $notify = $('#notify');
 
 $("#notify").popover({
@@ -79,8 +79,18 @@ socket.on('stop', function (data) {
 
 });
 
-$(document).ready(function () {
-    $('.apopover').each(function (index, item) {
-        $(item).popover();
-    });
+// apopover class used on dashboard
+$('.apopover').each(function (index, item) {
+    $(item).popover();
+});
+
+// mother* classes used on filestree() trees
+$('.mothership').click(function(){
+  $(this).children('ul').slideToggle();
+});
+$('.mother_of_glyphs').click(function(){
+  event.stopPropagation();
+  $(this).children('ul').slideToggle();
+});
+
 });

commit fb4e3830162dbba41bfefeb75a79db8d7bb80f8b

Fix: walkWithoutGit() broken in 085515a83bdc265ce82f16d1169ebe3195aed326

bakery/project/models.py
@@ -98,7 +98,7 @@ class Project(db.Model):
 
     def read_asset(self, name = None):
         fn = self.asset_by_name(name)
-        if os.path.exists(fn):
+        if os.path.exists(fn) and os.path.isfile(fn):
             return unicode(open(fn, 'r').read(), "utf8")
         else:
             return ''

bakery/project/state.py
@@ -29,16 +29,16 @@ def walkWithoutGit(path):
     :param path: path to walk down
     
     Returns:
-        dict: Dictionary of file and directory strings
+        dictionary: Dictionary of file and directory strings
     """
-    dict = {}
+    dictionary = {}
     for root, dirs, files in os.walk(path, topdown=False):
         for name in files:
-            h[name] = {}
+            dictionary[name] = {}
         for name in dirs:
             if not name == '.git':
-                h[name] = {}
-    return dict
+                dictionary[name] = {}
+    return dictionary
 
 def load_yaml(default_yml, yml = None):
     """