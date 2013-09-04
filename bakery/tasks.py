f7c5e87d157c4ad5eb1e8936b81e41a975665caf
Dave Crossland <dave@lab6.com>
2013-09-04 12:16:33 +0100

    Fix _out rotation in sync_and_process() method
    * use shutil to move and copy files
    * don't ensure user folder exists, as it is implicitly created by
    os.makedirs when creating the _out dir
    * define paths needed
    * reverse if os.path.exists(_out) to fix incorrect initial creation of
    _out_old

--- bakery/tasks.py
+++ bakery/tasks.py
@@ -22,6 +22,7 @@ from flask.ext.rq import job
 import plistlib
 from utils import RedisFd
 import re
+import shutil
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
 DATA_ROOT = os.path.join(ROOT, 'data')
@@ -358,27 +359,29 @@ def sync_and_process(project, process = True, sync = False):
     :param sync: Boolean. Sync the project. Defaults to off. 
     :param process: Boolean. Process (Bake) the project. Default to on.
     """
-    # Ensure user folder exists
-    if not os.path.exists(os.path.join(DATA_ROOT, project.login)):
-        os.makedirs(os.path.join(DATA_ROOT, project.login))
+    _user = os.path.join(DATA_ROOT, '%(login)s/' % project)
+    _out = os.path.join(DATA_ROOT, '%(login)s/%(id)s.out/' % project)
+    _out_src = os.path.join(DATA_ROOT, '%(login)s/%(id)s.out/src/' % project)
+    _out_log = os.path.join(DATA_ROOT, '%(login)s/%(id)s.process.log' % project)
 
-    # Rotate away the _out dir if it exists
-    if os.path.exists(_out):
+    # Ensure the project folder exists
+    if not os.path.exists(_out):
+        os.makedirs(_out_src)
+    # Or rotate it away if it does
+    else:
+        # Recursively figure out the top out number
         project.revision = 1
         _out_old = os.path.join(DATA_ROOT, '%(login)s/%(id)s.out-%(revision)s' % project)
         while os.path.exists(_out_old):
             project.revision += 1
             _out_old = os.path.join(DATA_ROOT, '%(login)s/%(id)s.out-%(revision)s' % project)
         # Move the directory
-        run('mv "%s" "%s"' % (_out, _out_old), cwd = _user, log=log)
+        shutil.move(_out, _out_old)
         # Remake the directory 
-        run('mkdir -p "%s"' % (_out_src), cwd = _user, log=log)
+        os.makedirs(_out_src)
         # Copy the log
         _out_old_log = os.path.join(_out_old, '%(id)s.process.log' % project)
-        run('cp "%s" "%s"' % (_out_log, _out_old_log), cwd = _user, log=log)
-    # Or make the _out dir
-    else:
-        run('mkdir -p %s' % (_out_src), cwd = _user, log=log)
+         shutil.copyfile(_out_log, _out_old_log)
 
     # create log file and open it with Redis
     log = RedisFd(os.path.join(DATA_ROOT, '%(login)s/%(id)s.process.log' % {
