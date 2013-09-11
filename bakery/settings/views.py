0614f17fd372c6da2fd360f973bc5f86d9f43b3f
Dave Crossland <dave@lab6.com>
2013-09-11 03:30:34 +0200

    Redesign settings page, fix it to show all repos (nasty kludge to do it :)

--- bakery/settings/views.py
+++ bakery/settings/views.py
@@ -59,20 +59,33 @@ def repos():
 @settings.route('/update', methods=['POST'])
 @login_required
 def update():
+    """Update the list of the user's githib repos"""
     auth = github.get_session(token=g.user.token)
     if g.user is not None:
-        resp = auth.get('/user/repos', data={'type': 'public'})
-        if resp.status_code == 200:
-            _repos = resp.json()
+        resps = []
+        # TODO DC: figure out how to use resp.links to get everything, this is nasty :)
+        # get the first 10 pages of git repos
+        for page in range(1, 10):
+            resp = auth.get('/user/repos?per_page=100&page=%s' % page, data={'type': 'all'})
+            resps.append(resp)
+        # if the first page returned okay, process the results
+        if resps[0].status_code == 200:
+            # make a big list of all the repos in json form
+            _repos = []
+            for resp in resps:
+                _repos += resp.json()
             # pylint:disable-msg=E1101
+            # Set up a cache
             cache = ProjectCache.query.filter_by(login=g.user.login).first()
             if not cache:
                 cache = ProjectCache()
                 cache.login = g.user.login
-
+            # cache the repos
             cache.data = _repos
+            cache.orgs = _orgs
+            # note the time
             cache.updated = datetime.datetime.utcnow()
-
+            # add the cache to the database
             db.session.add(cache)
             db.session.commit()
             flash(_('Repositories refreshed.'))
