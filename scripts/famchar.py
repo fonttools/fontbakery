38ee52fc2a0f963d765a395af99700bb2d1457f8
Dave Crossland <dave@lab6.com>
2013-09-14 22:11:42 +0200

    Simplify famchar.py to the 2 charsets I'm most interested in atm

--- scripts/famchar.py
+++ scripts/famchar.py
@@ -66,6 +66,12 @@ for fontfilename, fontaine in fonts.iteritems():
         if len(family[charsetname]) == len(fonts.items()):
             family[charsetname] = sum(family[charsetname]) / len(fonts.items())
 
-# pprint as a dict for now, could be yaml/json/etc
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(family)
+# # pprint as a dict for now, could be yaml/json/etc
+# pp = pprint.PrettyPrinter(indent=4)
+# pp.pprint(family)
+
+# Make a plain dictionary with just the bits we want on the dashboard
+totals = {} 
+totals['gwf'] = family[u'GWF latin']
+totals['al3'] = family[u'Adobe Latin 3']
+pp.pprint(totals)
