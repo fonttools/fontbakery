dd2ad971ed82835b9538465fd10317d7a8a652f2
xen <m@xen.ru>
2013-12-18 17:43:54 +0200

    ?

--- checker/__init__.py
+++ checker/__init__.py
@@ -15,7 +15,7 @@
 #
 # See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
 
-from base import *
-from upstream import run_set as upstream_set
-from result import run_set as result_set
-from ttx import run_set as ttx_set
+from .base import *
+from .upstream import run_set as upstream_set
+from .result import run_set as result_set
+from .ttx import run_set as ttx_set
