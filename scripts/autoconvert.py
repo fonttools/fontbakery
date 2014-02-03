8f43c396cd5e050b54947eeceb5d80430b1f754e
xen <m@xen.ru>
2014-02-03 05:28:54 +0200

    magic converter

---
+++ scripts/autoconvert.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+# Copyright 2013 The Font Bakery Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
+
+import sys
+# hardcoded for OSX
+# sys.path.append('/usr/local/lib/python2.7/site-packages/')
+import fontforge
+
+# magic is that fontforge detect output format by extension
+font = fontforge.open(sys.argv[1])
+font.generate(sys.argv[2])
