ca21b8a396af3b7f9bf853b11925fb8b21d756f4
hash3g <hash.3g@gmail.com>
2014-04-23 11:58:49 +0300

    test for simple coverall

--- tests/test_example.py
+++ tests/test_example.py
@@ -5,3 +5,7 @@ class ExampleTest(unittest.TestCase):
 
     def test_simple(self):
         self.assertFalse(False)
+
+    def test_simple_coverage(self):
+        from bakery.app import app
+        self.assertEquals(app.config['DEBUG'], True)
