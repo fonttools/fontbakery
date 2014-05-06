2d8aacd43d8486bf6c66ea52e699e2702aa02d6d
hash3g <hash.3g@gmail.com>
2014-05-06 10:06:16 +0300

    gunicorn shell script to use in Docker.io #309

---
+++ gunicorn.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+gunicorn wsgi:app \
+    --log-file bakery-error.log \
+    -p /var/www/bakery.pid -D \
+    -w 4 \
+    --worker-class socketio.sgunicorn.GeventSocketIOWorker \
+    -b 0.0.0.0:5000 \
+    -k gevent
