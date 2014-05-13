7f085c7ffe02ba548a71ae47c588dcc309be29d8
hash3g <hash.3g@gmail.com>
2014-05-13 12:35:29 +0300

    check that font file is a TrueType font file

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -224,6 +224,15 @@ class CheckerTest(unittest.TestCase):
         self.assertTrue(result_test,
                         lookup('test_font_italicangle_is_zero_or_negative', tests))
 
+    def test_result_font_is_font_success(self):
+        p = op.join(app.config['ROOT'], 'tests/fixtures/ttf/Font-Italic.ttf')
+        r = run_set(p, 'result')
+        success_tests = r['success']
+        tests = exclude_from_resultlist(r, 'success')
+        result_test = check('test_font_is_font', success_tests)
+        self.assertTrue(result_test,
+                        lookup('test_font_is_font', tests))
+
     def test_result_METADATA_font_filename_canonical(self):
         # TODO: create XXX_failure test
         p = op.join(app.config['ROOT'], 'tests/fixtures/ttf/Font-Bold.ttf')
