3e7e2f12f85bc2fe18b9588aa662381de1c8f41e
hash3g <hash.3g@gmail.com>
2014-05-27 18:34:17 +0300

    show process of git clone when sync is in progress

--- bakery/tasks.py
+++ bakery/tasks.py
@@ -94,13 +94,15 @@ def prun(command, cwd, log=None):
     p = subprocess.Popen(command, shell=True, cwd=cwd,
                          stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                          close_fds=True, env=env)
+    if log:
+        log.write('$ %s\n' % command)
 
     stdout = ''
     for line in iter(p.stdout.readline, ''):
+        if log:
+            log.write(line)
         stdout += line
-    if log:
-        log.write('$ %s\n' % command)
-        log.write(stdout)
+        p.stdout.flush()
     return stdout
 
 
@@ -200,7 +202,7 @@ def project_git_sync(project):
             # TODO in the future, to validate the URL string use
             # http://schacon.github.io/git/git-ls-remote.html
             # http://stackoverflow.com/questions/9610131/how-to-check-the-validity-of-a-remote-git-repository-url
-            prun('git clone --depth=100 --branch=master %(clone)s .' % project, cwd=_in, log=log)
+            prun('git clone --progress --depth=100 --branch=master %(clone)s .' % project, cwd=_in, log=log)
         except:
             # if the clone action didn't work, just copy it
             # if this is a file URL, copy the files, and set up the _in directory as a git repo
