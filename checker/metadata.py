c6eead2f0d03978eb7a9ccd77fbcfedcc62ae705
hash3g <hash.3g@gmail.com>
2014-07-02 09:04:27 +0300

    Implement FamilyMetadata and FontMetadata
    This two classes are used to improve test readable existing and new ones. Now
    testcases for fonts looks awful and they are hard to read and understand what
    they exactly do.

---
+++ checker/metadata.py
@@ -0,0 +1,91 @@
+import json
+
+
+class FamilyMetadata(object):
+
+    def __init__(self, metadata_object):
+        assert type(metadata_object) == dict
+        self.metadata_object = metadata_object
+
+    @property
+    def name(self):
+        return self.metadata_object['name']
+
+    @property
+    def designer(self):
+        return self.metadata_object.get('designer', '')
+
+    @property
+    def license(self):
+        return self.metadata_object.get('license', '')
+
+    @property
+    def visibility(self):
+        return self.metadata_object.get('visibility', 'Sandbox')
+
+    @property
+    def category(self):
+        return self.metadata_object.get('category', '')
+
+    @property
+    def size(self):
+        return self.metadata_object.get('size', 0)
+
+    @property
+    def date_added(self):
+        # TODO: property has to return datetime object or None
+        #  For the moment it is enough to return string value
+        #  but need to remember that string value is hard to validate
+        #  to correct date
+        return self.metadata_object.get('dateAdded', '')
+
+    @property
+    def fonts(self):
+        for f in self.metadata_object['fonts']:
+            yield FontMetadata(f)
+
+
+class FontMetadata(object):
+
+    def __init__(self, metadata_object):
+        self.metadata_object = metadata_object
+
+    @property
+    def name(self):
+        return self.metadata_object['name']
+
+    @property
+    def post_script_name(self):
+        return self.metadata_object.get('post_script_name', '')
+
+    @property
+    def full_name(self):
+        return self.metadata_object.get('full_name', '')
+
+    @property
+    def style(self):
+        return self.metadata_object.get('style', 'normal')
+
+    @property
+    def weight(self):
+        return self.metadata_object.get('weight', 0)
+
+    @property
+    def filename(self):
+        return self.metadata_object.get('filename', '')
+
+    @property
+    def copyright(self):
+        return self.metadata_object.get('copyright', '')
+
+
+class Metadata(object):
+
+    @staticmethod
+    def get_family_metadata(contents):
+        """ Returns root of FamilyMetadata instance
+
+            Args:
+                contents: Metadata jsoned object dumped to string
+        """
+        return FamilyMetadata(json.loads(contents))
---
+++ tests/test_metadata.py
@@ -0,0 +1,35 @@
+import types
+import unittest
+
+from checker.metadata import Metadata, FamilyMetadata, FontMetadata
+
+
+class MetadataTestCase(unittest.TestCase):
+
+    def test_family_metadata_is_loaded(self):
+        """ Check if Metadata can read family metadata correctly """
+        fm = Metadata.get_family_metadata('{"name": "Family Name"}')
+        self.assertEqual(type(fm), FamilyMetadata)
+        self.assertEqual(fm.name, "Family Name")
+        self.assertEqual(fm.designer, "")
+        self.assertEqual(fm.license, "")
+        self.assertEqual(fm.visibility, "Sandbox")
+        self.assertEqual(fm.category, "")
+        self.assertEqual(fm.size, 0)
+        self.assertEqual(fm.date_added, "")
+
+    def test_font_metadata_is_loaded(self):
+        """ Check if font metadata can be read from family metadata """
+        fm = Metadata.get_family_metadata(
+            '{"name": "Family Name", "fonts": [{"name": "FontName"}]}')
+        fonts_metadata = fm.fonts
+        self.assertEqual(type(fonts_metadata), types.GeneratorType)
+        fm = fonts_metadata.next()
+        self.assertEqual(type(fm), FontMetadata)
+        self.assertEqual(fm.name, "FontName")
+        self.assertEqual(fm.post_script_name, "")
+        self.assertEqual(fm.full_name, "")
+        self.assertEqual(fm.style, "normal")
+        self.assertEqual(fm.weight, 0)
+        self.assertEqual(fm.filename, "")
+        self.assertEqual(fm.copyright, "")
