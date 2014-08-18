bbd93eea24ed4b3b546861a7d54dbb53762e1870
hash3g <hash.3g@gmail.com>
2014-08-18 18:49:40 +0300

    Update requirements, removed unused fixtures

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -18,15 +18,42 @@
 import os.path as op
 import unittest
 
+from mock import MagicMock
+
 from bakery.app import app
+from bakery.gitauth.views import authorize_user
 from bakery.tasks import get_subsets_coverage_data
+from bakery.tasks import refresh_repositories, refresh_latest_commits
 
 
 class TaskTestCase(unittest.TestCase):
 
     def test_subsets_coverage_data(self):
-        path = op.join(app.config['ROOT'], 'tests/fixtures/ttf/Font-Bold.ttf')
+        path = op.join(app.config['ROOT'], 'tests/fixtures/Font-Bold.ttf')
         self.assertTrue(get_subsets_coverage_data([path], ''))
 
-        path = op.join(app.config['ROOT'], 'tests/fixtures/src/Font-Bold.ttx')
+        path = op.join(app.config['ROOT'], 'tests/fixtures/Font-Bold.ttx')
         self.assertTrue(get_subsets_coverage_data([path], ''))
+
+
+class TestRepository(unittest.TestCase):
+
+    def test_execute_refresh_repos_task_after_login(self):
+        with app.test_request_context('/'):
+            refresh_repositories.delay = MagicMock(name='refresh_repos')
+            refresh_latest_commits.delay = MagicMock(name='refresh_commits')
+
+            authorize_user({'login': 'offline', 'gravatar_id': '',
+                            'email': ''}, token='')
+
+            refresh_repositories.delay.assert_called_once_with('offline', '')
+
+    def test_execute_refresh_latest_commit_after_login(self):
+        with app.test_request_context('/'):
+            refresh_repositories.delay = MagicMock(name='refresh_repos')
+            refresh_latest_commits.delay = MagicMock(name='refresh_commits')
+
+            authorize_user({'login': 'offline', 'gravatar_id': '',
+                            'email': ''}, token='')
+
+            refresh_latest_commits.delay.assert_called_once_with()
