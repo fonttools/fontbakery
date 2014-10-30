9706d4490615785bcec0571cfae0d6d73f2b053b
andriyko <ahrytskiv@gmail.com>
2014-10-30 16:40:26 +0200

    Fixing issue #472

--- static/js/controllers/logController.js
+++ static/js/controllers/logController.js
@@ -1,5 +1,9 @@
-angular.module('myApp').controller('logController', ['$scope', '$http', 'buildApi', function($scope, $http, buildApi) {
+angular.module('myApp').controller('logController', ['$scope', '$http', '$sce', 'buildApi', function($scope, $http, $sce, buildApi) {
     $scope.dataLoaded = false;
+
+    $scope.skipValidation = function(value) {
+        return $sce.trustAsHtml(value);
+    };
 
     buildApi.getBuildLog().then(function(response) {
         $scope.data = response.data;
