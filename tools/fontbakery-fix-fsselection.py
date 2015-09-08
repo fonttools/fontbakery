22d484be49864517b2b62c7fb06630960c3165ca
vitalyvolkov <hash.3g@gmail.com>
2015-09-08 12:24:26 +0300

    Add fixer to fontbakery-fix-fsselection.py & fontbakery-fix-macstyle.py #570

--- tools/fontbakery-fix-fsselection.py
+++ tools/fontbakery-fix-fsselection.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import argparse
+import csv
 import os
+import sys
 
 from fontTools import ttLib
 import tabulate
@@ -9,6 +11,7 @@ args = argparse.ArgumentParser(
 	description='Print out fsSelection bitmask of the fonts')
 args.add_argument('font', nargs="+")
 args.add_argument('--csv', default=False, action='store_true')
+args.add_argument('--autofix', default=False, action='store_true')
 
 
 def getByte2(ttfont):
@@ -19,27 +22,39 @@ def getByte1(ttfont):
 	return ttfont['OS/2'].fsSelection & 255
 
 
-if __name__ == '__main__':
-
-	arg = args.parse_args()
-	
+def printInfo(fonts, print_csv=False):
 	rows = []
 	headers = ['filename', 'fsSelection']
-	for font in arg.font:
+	for font in fonts:
 		ttfont = ttLib.TTFont(font)
 		row = [os.path.basename(font)]
 		row.append('{:#010b} {:#010b}'.format(getByte2(ttfont), getByte1(ttfont)).replace('0b', ''))
 		rows.append(row)
 
 	def as_csv(rows):
-		import csv
-		import sys
 		writer = csv.writer(sys.stdout)
 		writer.writerows([headers])
 		writer.writerows(rows)
 		sys.exit(0)
 
-	if arg.csv:
+	if print_csv:
 		as_csv(rows)
+	else:
+		print(tabulate.tabulate(rows, headers))
 
-	print(tabulate.tabulate(rows, headers))
+
+if __name__ == '__main__':
+	arg = args.parse_args()
+	if arg.autofix:
+		for font in arg.font:
+			ttfont = ttLib.TTFont(font)
+			if '-Regular' in font:
+				ttfont['OS/2'].fsSelection |= 0b1000000
+			if '-Bold' in font:
+				ttfont['OS/2'].fsSelection |= 0b100000
+			if 'Italic' in font:
+				ttfont['OS/2'].fsSelection |= 0b1
+			ttfont.save(font + '.fix')
+	printInfo(arg.font, print_csv=arg.csv)
+	
+	
