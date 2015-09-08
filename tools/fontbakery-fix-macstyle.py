b6b659ab6889bb17abe1ceeabe35437fdcf65594
vitalyvolkov <hash.3g@gmail.com>
2015-09-08 12:47:05 +0300

    set bits manually

--- tools/fontbakery-fix-macstyle.py
+++ tools/fontbakery-fix-macstyle.py
@@ -12,8 +12,8 @@ args = argparse.ArgumentParser(
 	description='Print out macStyle bitmask of the fonts')
 args.add_argument('font', nargs="+")
 args.add_argument('--csv', default=False, action='store_true')
-args.add_argument('--bit-bold', default=False, action='store_true')
-args.add_argument('--bit-italic', default=False, action='store_true')
+args.add_argument('--bit-bold', choices=[0, 1], type=int)
+args.add_argument('--bit-italic', choices=[0, 1], type=int)
 args.add_argument('--autofix', default=False, action='store_true')
 
 
@@ -46,16 +46,29 @@ def printInfo(fonts, print_csv=False):
 	print(tabulate.tabulate(rows, headers))
 
 
+def clearBit(int_type, offset):
+    mask = ~(1 << offset)
+    return(int_type & mask)
+
+
 if __name__ == '__main__':
 
 	arg = args.parse_args()
-	if arg.autofix or arg.bit_bold or arg.bit_italic:
+	if arg.autofix or arg.bit_bold != -1 or arg.bit_italic != -1:
 		for font in arg.font:
 			ttfont = ttLib.TTFont(font)
-			if '-Bold' in font or arg.bit_bold:
+			if '-Bold' in font:
 				ttfont['head'].macStyle |= 0b1
-			if 'Italic' in font or arg.bit_italic:
+			if arg.bit_bold:
+				ttfont['head'].macStyle |= 0b1
+			if arg.bit_bold == 0:
+				ttfont['head'].macStyle = clearBit(ttfont['head'].macStyle, 0)
+			if 'Italic' in font:
 				ttfont['head'].macStyle |= 0b10
+			if arg.bit_italic:
+				ttfont['head'].macStyle |= 0b10
+			if arg.bit_italic == 0:
+				ttfont['head'].macStyle = clearBit(ttfont['head'].macStyle, 1)
 			ttfont.save(font + '.fix')
 		printInfo([f + '.fix' for f in arg.font], print_csv=arg.csv)
 		sys.exit(0)
