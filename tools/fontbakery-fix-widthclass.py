f92ab1a3c60c96865b3adc83bf899019932ab7b3
vitalyvolkov <hash.3g@gmail.com>
2015-09-28 16:38:00 +0300

    convert output tables to Github Markdown syntax #571

--- tools/fontbakery-fix-widthclass.py
+++ tools/fontbakery-fix-widthclass.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import argparse
+import csv
 import os
+import sys
 import tabulate
 
 from fontTools import ttLib
@@ -11,25 +13,27 @@ args.add_argument('font', nargs="+")
 args.add_argument('--csv', default=False, action='store_true')
 
 
-if __name__ == '__main__':
-
-	arg = args.parse_args()
-	
+def print_info(fonts, print_csv=False):
 	headers = ['filename', 'usWidthClass']
 	rows = []
-	for font in arg.font:
+	for font in fonts:
 		ttfont = ttLib.TTFont(font)
 		rows.append([os.path.basename(font), ttfont['OS/2'].usWidthClass])
 
 	def as_csv(rows):
-		import csv
-		import sys
 		writer = csv.writer(sys.stdout)
 		writer.writerows([headers])
 		writer.writerows(rows)
 		sys.exit(0)
 
-	if arg.csv:
+	if print_csv:
 		as_csv(rows)
 		
-	print(tabulate.tabulate(rows, headers))
+	print(tabulate.tabulate(rows, headers, tablefmt="pipe"))
+
+
+if __name__ == '__main__':
+	arg = args.parse_args()
+	print_info(arg.font, print_csv=arg.csv)
+	
+	
