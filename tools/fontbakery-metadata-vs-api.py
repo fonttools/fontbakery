c6fa0d1418db6c20258b74a3ced47c970085caa9
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-01-26 16:01:05 -0200

    slight adjustment in nomenclature

--- tools/fontbakery-metadata-vs-api.py
+++ tools/fontbakery-metadata-vs-api.py
@@ -12,9 +12,9 @@ from google.protobuf import text_format
 
 args = argparse.ArgumentParser()
 args.add_argument('key', help='Key from Google Fonts Developer API')
-args.add_argument('repo', help='Directory tree that contains directories with METADATA.json')
+args.add_argument('repo', help='Directory tree that contains directories with METADATA.pb files')
 args.add_argument('--cache', help='Directory to store a copy of the files in the fonts developer api',
-                  default="/tmp/fontbakery-compare-github-api")
+                  default="/tmp/fontbakery-compare-git-api")
 args.add_argument('--verbose', help='Print additional information', action="store_true")
 args.add_argument('--ignore-copy-existing-ttf', action="store_true")
 args.add_argument('--autofix', help='Apply automatic fixes to files', action="store_true")
@@ -85,13 +85,13 @@ if __name__ == '__main__':
                 continue
 
             if subset not in metadata.subsets:
-                print('ER: {} has {} in API but not in Github'.format(family, subset), file=sys.stderr)
+                print('ER: {} has {} in API but not in repository'.format(family, subset), file=sys.stderr)
             elif argv.verbose:
-                print('OK: {} has {} in Github and API'.format(family, subset))
+                print('OK: {} has {} in repository and API'.format(family, subset))
 
         for subset in metadata.subsets:
             if subset != "menu" and subset not in webfontsItem['subsets']:
-                print('ER: {} has {} in Github but not in API'.format(family, subset), file=sys.stderr)
+                print('ER: {} has {} in repository but not in API'.format(family, subset), file=sys.stderr)
 
         log_messages = []
         for style in webfontStyles:
@@ -100,26 +100,26 @@ if __name__ == '__main__':
                 metadataFileName = metadata.fonts[filenameWeightStyleIndex].filename
 
                 import hashlib
-                github_md5 = hashlib.md5(open(os.path.join(dirpath, metadataFileName), 'rb').read()).hexdigest()
+                repo_md5 = hashlib.md5(open(os.path.join(dirpath, metadataFileName), 'rb').read()).hexdigest()
                 fonturl = webfontsItem['files'][style]
                 fontpath = get_cache_font_path(argv.cache, fonturl)
                 google_md5 = hashlib.md5(open(fontpath, 'rb').read()).hexdigest()
 
-                if github_md5 == google_md5:
+                if repo_md5 == google_md5:
                     if argv.verbose:
-                        log_messages.append([metadataFileName, 'OK', '{} in Github and in API'.format(metadataFileName)])
+                        log_messages.append([metadataFileName, 'OK', '{} in repository and in API'.format(metadataFileName)])
                 else:
-                    log_messages.append([metadataFileName, 'ER', '{}: Checksum mismatch: File in API does not match file on GitHub'.format(metadataFileName)])
+                    log_messages.append([metadataFileName, 'ER', '{}: Checksum mismatch: File in API does not match file in repository'.format(metadataFileName)])
 
             except ValueError:
                 name = '{}-{}'.format(family, style)
-                log_messages.append([name, 'ER', '{} in API but not in Github'.format(name)])
+                log_messages.append([name, 'ER', '{} in API but not in repository'.format(name)])
 
         for font in metadata.fonts:
             try:
                 webfontStyles.index(str(font.weight) + str(font.style))
             except ValueError:
-                log_messages.append([font.filename, 'ER', '{} in Github but not in API'.format(font.filename)])
+                log_messages.append([font.filename, 'ER', '{} in repository but not in API'.format(font.filename)])
 
         #sort all the messages by their respective metadataFileName and print them:
         for message in sorted(log_messages, key=lambda x: x[0].lower()):
