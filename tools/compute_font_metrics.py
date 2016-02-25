853593debec98d8d99c37ba99a3bd40061f3ba62
Dave Crossland <dave@lab6.com>
2016-02-25 18:39:01 -0500

    compute: relabel table

--- tools/compute_font_metrics.py
+++ tools/compute_font_metrics.py
@@ -248,19 +248,19 @@ def main():
   # double(<unit>, <precision>, <decimal_point>, <thousands_separator>, <show_unit_before_number>, <nansymbol>)
   grid_data = {
     "metadata": [
-      {"name":"fontfile","label":"FONTFILE","datatype":"string","editable":True},
+      {"name":"fontfile","label":"filename","datatype":"string","editable":True},
       {"name":"gfn","label":"GFN","datatype":"string","editable":True},
-      {"name":"weight","label":"WEIGHT","datatype":"double(, 2, dot, comma, 0, n/a)","editable":True},
+      {"name":"weight","label":"weight","datatype":"double(, 2, dot, comma, 0, n/a)","editable":True},
       {"name":"weight_int","label":"WEIGHT_INT","datatype":"integer","editable":True},
-      {"name":"width","label":"WIDTH","datatype":"double(, 2, dot, comma, 0, n/a)","editable":True},
+      {"name":"width","label":"width","datatype":"double(, 2, dot, comma, 0, n/a)","editable":True},
       {"name":"width_int","label":"WIDTH_INT","datatype":"integer","editable":True},
-      {"name":"angle","label":"ANGLE","datatype":"double(, 2, dot, comma, 0, n/a)","editable":True}, 
-      {"name":"angle_image","label":"ANGLE_IMAGE","datatype":"html","editable":False},
+      {"name":"angle","label":"angle","datatype":"double(, 2, dot, comma, 0, n/a)","editable":True}, 
+      {"name":"angle_image","label":"angle image","datatype":"html","editable":False},
       {"name":"angle_int","label":"ANGLE_INT","datatype":"integer","editable":True},
       {"name":"usage","label":"USAGE","datatype":"string","editable":True,
         "values": {"header":"header", "body":"body", "unknown":"unknown"}
       },
-      {"name":"image","label":"IMAGE","datatype":"html","editable":False},
+      {"name":"image","label":"image","datatype":"html","editable":False},
     ],
     "data": []
   }
