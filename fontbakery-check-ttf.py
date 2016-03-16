d14f4ae41e104fcc4cff09f82d9a4ac7aeca1a53
Dave Crossland <dave@lab6.com>
2016-03-15 21:59:20 -0400

    Imperative: Move check() inline

--- fontbakery-check-ttf.py
+++ fontbakery-check-ttf.py
@@ -7,66 +7,63 @@ import os, sys, argparse, glob, logging
 from fontTools import ttLib
 
 def main():
-    # set up a basic logging config
-    # to include timestamps
-    # log_format = '%(asctime)s %(levelname)-8s %(message)s'
-    log_format = '%(levelname)-8s %(message)s  '
-    logger = logging.getLogger()
-    handler = logging.StreamHandler()
-    formatter = logging.Formatter(log_format)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    # to show all log events
-    # logger.setLevel(logging.DEBUG)
-    logger.setLevel(logging.INFO)
-    
-    # set up some command line argument processing
-    parser = argparse.ArgumentParser(description="Check TTF files for common issues.")
-    parser.add_argument('filenames', nargs='+', 
-      help='file name(s) of fonts to check. Wildcards like *.ttf are allowed.')
-    args = parser.parse_args()
+  # set up a basic logging config
+  # to include timestamps
+  # log_format = '%(asctime)s %(levelname)-8s %(message)s'
+  log_format = '%(levelname)-8s %(message)s  '
+  logger = logging.getLogger()
+  handler = logging.StreamHandler()
+  formatter = logging.Formatter(log_format)
+  handler.setFormatter(formatter)
+  logger.addHandler(handler)
+  # to show all log events
+  # logger.setLevel(logging.DEBUG)
+  logger.setLevel(logging.INFO)
+  
+  # set up some command line argument processing
+  parser = argparse.ArgumentParser(description="Check TTF files for common issues.")
+  parser.add_argument('filenames', nargs='+', 
+    help='file name(s) of fonts to check. Wildcards like *.ttf are allowed.')
+  args = parser.parse_args()
 
-    logging.debug("Checking each file is a ttfs")
-    fonts_to_check = []
-    for filename in sorted(args.filenames):
-      # use glob.glob to accept *.ttf
-      for font_file in glob.glob(filename):
-        if font_file.endswith('.ttf'):
-          fonts_to_check.append(font_file)
-        else:
-          file_path, filename = os.path.split(font_file)
-          logging.error("Skipping " + filename + " as not a ttf") 
-    fonts_to_check.sort()
-
-    logging.debug("Checking files are named canonically")
-    not_canonical = []
-    for font_file in fonts_to_check:
-      file_path, filename = os.path.split(font_file)
-      filename_base, filename_extention = os.path.splitext(filename)
-      family, style = filename_base.split('-')
-      style_names = ["Thin", "ExtraLight", "Light", "Regular", "Medium", 
-               "SemiBold", "Bold", "ExtraBold", "Black", 
-               "Thin Italic", "ExtraLight Italic", "Light Italic", 
-               "Italic", "Medium Italic", "SemiBold Italic", 
-               "Bold Italic", "ExtraBold Italic", "Black Italic"]
-      # remove spaces
-      style_file_names = [name.replace(' ', '') for name in style_names]
-      if style in style_file_names:
-        logging.info(font_file + " is named canonically")
+  logging.debug("Checking each file is a ttfs")
+  fonts_to_check = []
+  for filename in sorted(args.filenames):
+    # use glob.glob to accept *.ttf
+    for font_file in glob.glob(filename):
+      if font_file.endswith('.ttf'):
+        fonts_to_check.append(font_file)
       else:
-          logging.critical(font_file + " is not named canonically")
-          not_canonical.append(font_file)
-    if not_canonical:
-      print '\nAborted, critical errors. Please rename these files canonically and try again:\n ',
-      print '\n  '.join(not_canonical)
-      print '\nCanonical names are defined in',
-      print 'https://github.com/googlefonts/gf-docs/blob/master/ProjectChecklist.md#instance-and-file-naming'
-      sys.exit(1)
+        file_path, filename = os.path.split(font_file)
+        logging.error("Skipping " + filename + " as not a ttf") 
+  fonts_to_check.sort()
 
-    for font_file in fonts_to_check:
-      check(font_file)
+  logging.debug("Checking files are named canonically")
+  not_canonical = []
+  for font_file in fonts_to_check:
+    file_path, filename = os.path.split(font_file)
+    filename_base, filename_extention = os.path.splitext(filename)
+    family, style = filename_base.split('-')
+    style_names = ["Thin", "ExtraLight", "Light", "Regular", "Medium", 
+             "SemiBold", "Bold", "ExtraBold", "Black", 
+             "Thin Italic", "ExtraLight Italic", "Light Italic", 
+             "Italic", "Medium Italic", "SemiBold Italic", 
+             "Bold Italic", "ExtraBold Italic", "Black Italic"]
+    # remove spaces
+    style_file_names = [name.replace(' ', '') for name in style_names]
+    if style in style_file_names:
+      logging.info(font_file + " is named canonically")
+    else:
+        logging.critical(font_file + " is not named canonically")
+        not_canonical.append(font_file)
+  if not_canonical:
+    print '\nAborted, critical errors. Please rename these files canonically and try again:\n ',
+    print '\n  '.join(not_canonical)
+    print '\nCanonical names are defined in',
+    print 'https://github.com/googlefonts/gf-docs/blob/master/ProjectChecklist.md#instance-and-file-naming'
+    sys.exit(1)
 
-def check(font_file):
+  for font_file in fonts_to_check:
     logging.debug("Opening " + font_file)
     font = ttLib.TTFont(font_file)
     logging.info(font_file + " opened")
