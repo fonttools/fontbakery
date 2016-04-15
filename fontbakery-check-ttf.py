206c08dfcf4116fe0794fdc3636ef891e49eb1bd
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-04-15 18:55:37 -0300

    more usage of symbolic names

--- fontbakery-check-ttf.py
+++ fontbakery-check-ttf.py
@@ -677,12 +677,26 @@ def main():
  
     #----------------------------------------------------
     # TODO: Add a description/rationale to this check here
-    logging.debug("Checking name table for items without platformID=1")
+    logging.debug("Checking name table for items without platformID = 1 (MACHINTOSH)")
     new_names = []
     changed = False
     for name in font['name'].names:
-      if name.platformID != 1 and name.nameID not in [0, 1, 2, 3, 4, 5, 6, 18]\
-         or name.platformID == 1 and name.nameID in [1,2,4,6]: #see https://github.com/googlefonts/fontbakery/issues/649
+      if (name.platformID != PLATFORM_ID_MACHINTOSH and
+          name.nameID not in [NAMEID_COPYRIGHT_NOTICE,
+                              NAMEID_FONT_FAMILY_NAME,
+                              NAMEID_FONT_SUBFAMILY_NAME,
+                              NAMEID_UNIQUE_FONT_IDENTIFIER,
+                              NAMEID_FULL_FONT_NAME,
+                              NAMEID_VERSION_STRING,
+                              NAMEID_POSTSCRIPT_NAME,
+                              NAMEID_COMPATIBLE_FULL_MACONLY
+                             ]) or \
+         (name.platformID == PLATFORM_ID_MACHINTOSH and
+          name.nameID in [NAMEID_FONT_FAMILY_NAME,
+                          NAMEID_FONT_SUBFAMILY_NAME,
+                          NAMEID_FULL_FONT_NAME,
+                          NAMEID_POSTSCRIPT_NAME
+                         ]): #see https://github.com/googlefonts/fontbakery/issues/649
         new_names.append(name)
       else:
         changed = True
