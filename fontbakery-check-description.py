93ffdff0934dce4cede1e320dff71b240e3a98e2
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-06-15 22:22:35 -0300

    boilerplate selection of old code that we still must preserve
    related to fontbakery-check-description.py and all placed in a single file.

--- fontbakery-check-description.py
+++ fontbakery-check-description.py
@@ -17,12 +17,56 @@
 # See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
 from __future__ import print_function
 import argparse
+import defusedxml.lxml
+import magic
 import os
+import requests
 import sys
+import unittest
+from lxml.html import HTMLParser
 
-from bakery_lint.fonttests.test_description import get_suite
-from bakery_lint.base import run_suite
+print ("This will very soon be a small tool for running"
+       " a few checks on DESCRIPTION.txt files.\n")
+exit(0)
+#TODO: refactor this code:
 
+class TestDescriptionHtmlEnUs():
+
+    targets = ['description']
+    tool = 'FontBakery'
+    name = __name__
+
+    def test_broken_links(self):
+        """ Check if DESCRIPTION do not have broken links """
+        try:
+            contents = open(self.operator.path).read()
+        except:
+            self.fail('File {} does not exist'.format(self.operator.path))
+        doc = defusedxml.lxml.fromstring(contents, parser=HTMLParser())
+        for link in doc.xpath('//a/@href'):
+            try:
+                response = requests.head(link)
+                self.assertEqual(response.status_code, requests.codes.ok,
+                                 msg='%s is broken' % link)
+            except requests.exceptions.RequestException as ex:
+                self.fail('%s raises exception [%r]' % (link, ex))
+
+    def test_description_is_valid_html(self):
+        """ DESCRIPTION.en_us.html is not real html file """
+        msg = 'DESCRIPTION.en_us.html is not real html file'
+        if not os.path.exists(self.operator.path):
+            self.fail('File {} does not exist'.format(self.operator.path))
+        self.assertEqual(magic.from_file(self.operator.path, mime=True), 'text/html', msg)
+
+    def test_description_is_more_than_500b(self):
+        """ DESCRIPTION.en_us.html is more than 500 bytes """
+        msg = 'DESCRIPTION.en_us.html is not real html file'
+        try:
+            statinfo = os.stat(self.operator.path)
+        except:
+            self.fail('File {} does not exist'.format(self.operator.path))
+        msg = 'DESCRIPTION.en_us.html must have size larger than 500 bytes'
+        self.assertGreater(statinfo.st_size, 500, msg)
 
 if __name__ == '__main__':
     description = 'Runs checks or tests on specified DESCRIPTION.txt file(s)'
@@ -31,20 +75,11 @@ if __name__ == '__main__':
     parser.add_argument('--verbose', '-v', action='count',
                         help="Verbosity level", default=False)
 
-    parser.add_argument('--list-checks', '-l', action='store_true',
-                        help="Print available checks", default=False)
-
     args = parser.parse_args()
-
-    if args.list_checks:
-        from bakery_lint.base import tests_report
-        print(tests_report('description'))
-        sys.exit()
 
     for x in args.file:
         if not os.path.basename(x).startswith('DESCRIPTION.'):
             print('ER: {} is not DESCRIPTION'.format(x), file=sys.stderr)
             continue
 
-        suite = get_suite(x)
-        run_suite(suite)
+        #TODO: open the 'x' file and run the tests here.
