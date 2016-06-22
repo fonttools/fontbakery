8b21e4156af6345ba9910229149394edb0cb9670
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-06-22 18:19:58 -0300

    fontbakery-fix-nameids.py tool (issue #806)

---
+++ fontbakery-fix-nameids.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import argparse
+import os
+import tabulate
+from fontTools import ttLib
+
+args = argparse.ArgumentParser(
+    description='Print out nameIDs strings of the fonts')
+args.add_argument('font', nargs="+")
+args.add_argument('--autofix', default=False,
+                  action='store_true', help='Apply autofix')
+args.add_argument('--csv', default=False, action='store_true',
+                  help="Output data in comma-separate-values"
+                       " (CSV) file format")
+args.add_argument('--id', '-i', default='all')
+args.add_argument('--platform', '-p', type=int, default=3)
+
+if __name__ == '__main__':
+    arg = args.parse_args()
+    nameids = ['1', '2', '4', '6', '16', '17', '18']
+    user_nameids = [x.strip() for x in arg.id.split(',')]
+
+    if 'all' not in user_nameids:
+        nameids = set(nameids) & set(user_nameids)
+
+    rows = []
+    for font in arg.font:
+        ttfont = ttLib.TTFont(font)
+        row = [os.path.basename(font)]
+        for name in ttfont['name'].names:
+            if str(name.nameID) not in nameids or\
+               name.platformID != arg.platform:
+                continue
+
+            value = name.string.decode(name.getEncoding()) or ''
+            row.append(value)
+
+        rows.append(row)
+
+    header = ['filename'] + ['id' + x for x in nameids]
+
+    def as_csv(rows):
+        import csv
+        import sys
+        writer = csv.writer(sys.stdout)
+        writer.writerows([header])
+        writer.writerows(rows)
+        sys.exit(0)
+
+    if arg.csv:
+        as_csv(rows)
+
+    print(tabulate.tabulate(rows, header, tablefmt="pipe"))
+
+    for path in arg.font:
+        if arg.autofix:
+            font = ttLib.TTFont(path)
+            saveit = False
+            for name in font['name'].names:
+                if name.platformID != 1:
+                    saveit = True
+                    del name
+            if saveit:
+                font.save(path + ".fix")
