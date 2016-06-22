aa321dcf4063898aa5d3ce952ab37c31efbb74ed
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-06-22 18:26:09 -0300

    fontbakery-fix-weightclass.py tool (issue #806)

---
+++ fontbakery-fix-weightclass.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import argparse
+import os
+import tabulate
+from fontTools import ttLib
+
+args = argparse.ArgumentParser(
+    description='Print out usWeightClass of the fonts')
+args.add_argument('font', nargs="+")
+args.add_argument('--csv', default=False, action='store_true')
+
+if __name__ == '__main__':
+    arg = args.parse_args()
+    headers = ['filename', 'usWeightClass']
+    rows = []
+    for font in arg.font:
+        ttfont = ttLib.TTFont(font)
+        rows.append([os.path.basename(font), ttfont['OS/2'].usWeightClass])
+
+    def as_csv(rows):
+        import csv
+        import sys
+        writer = csv.writer(sys.stdout)
+        writer.writerows([headers])
+        writer.writerows(rows)
+        sys.exit(0)
+
+    if arg.csv:
+        as_csv(rows)
+
+    print(tabulate.tabulate(rows, headers, tablefmt="pipe"))
