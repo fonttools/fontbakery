a511ce3475caa0d2243cb158de3880e8a37c1c66
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-06-27 21:03:35 -0300

    small script to automate testing all fonts in a checkout of the https://github.com/google/fonts/ repo.

---
+++ test_all_gfonts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+FOLDERS=~/devel/github_google/fonts/ofl/*/
+for f in $FOLDERS
+do
+  JSON_LOG=./check_results/$(basename $f).json
+
+  if [ -f $JSON_LOG ]
+  then
+    echo "$JSON_LOG found. Skipping."
+  else
+    echo "Processing '$f'..."
+    ./fontbakery-check-ttf.py "$f/*.ttf" -vv
+    mv ./fontbakery-check-results.json ./check_results/$(basename $f).json || exit 1
+  fi
+done
+
