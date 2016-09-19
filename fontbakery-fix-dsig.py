da1ca2d27f0826d34f0a676ba7cc40d8a57ec380
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-09-19 16:07:31 -0300

    fix implementation of dsig-fixer script
    (issue #1001)

--- fontbakery-fix-dsig.py
+++ fontbakery-fix-dsig.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
-# Copyright 2013 The Font Bakery Authors. All Rights Reserved.
+# Copyright 2013,2016 The Font Bakery Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,63 +17,63 @@
 # See AUTHORS.txt for the list of Authors and LICENSE.txt for the License.
 import argparse
 import os
-from bakery_cli.fixers import CreateDSIGFixer
+from fontTools import ttLib
 
-class CreateDSIGFixer(Fixer):
-    """ Create DSIG table in font if it does not exist """
-
-    def get_shell_command(self):
-        return "fontbakery-fix-dsig.py {}".format(self.fontpath)
-
-    def fix(self):
-        if 'DSIG' in self.font:
-            return False
-
-        try:
-            from fontTools.ttLib.tables.D_S_I_G_ import SignatureRecord
-        except ImportError:
-            error_message = ("The '{}' font does not have an existing"
-                             " digital signature proving its authenticity,"
-                             " so Fontbakery needs to add one. To do this"
-                             " requires version 2.3 or later of Fonttools"
-                             " to be installed. Please upgrade at"
-                             " https://pypi.python.org/pypi/FontTools/2.4")
-            logger.error(error_message.format(os.path.basename(self.fontpath)))
-            return False
+description = 'Fixes TTF to have a dummy DSIG table'
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument('ttf_font',
+                    nargs='+',
+                    help="One or more font files")
+parser.add_argument('--autofix',
+                    action='store_true',
+                    help='Apply autofix')
 
 
-        newDSIG = ttLib.newTable("DSIG")
-        newDSIG.ulVersion = 1
-        newDSIG.usFlag = 1
-        newDSIG.usNumSigs = 1
-        sig = SignatureRecord()
-        sig.ulLength = 20
-        sig.cbSignature = 12
-        sig.usReserved2 = 0
-        sig.usReserved1 = 0
-        sig.pkcs7 = '\xd3M4\xd3M5\xd3M4\xd3M4'
-        sig.ulFormat = 1
-        sig.ulOffset = 20
-        newDSIG.signatureRecords = [sig]
-        self.font.tables["DSIG"] = newDSIG
-return True
+def main():
+  args = parser.parse_args()
+  for path in args.ttf_font:
+    if not os.path.exists(path):
+      continue
+    font = ttLib.TTFont(path)
+    if "DSIG" not in font:
+      try:
+        if args.autofix:
+          from ttLib.tables.D_S_I_G_ import SignatureRecord
+          newDSIG = ttLib.newTable("DSIG")
+          newDSIG.ulVersion = 1
+          newDSIG.usFlag = 1
+          newDSIG.usNumSigs = 1
+          sig = SignatureRecord()
+          sig.ulLength = 20
+          sig.cbSignature = 12
+          sig.usReserved2 = 0
+          sig.usReserved1 = 0
+          sig.pkcs7 = '\xd3M4\xd3M5\xd3M4\xd3M4'
+          sig.ulFormat = 1
+          sig.ulOffset = 20
+          newDSIG.signatureRecords = [sig]
+          font.tables["DSIG"] = newDSIG
+          print("The font does not have an existing digital"
+                " signature (DSIG), so we just added a dummy"
+                " placeholder that should be enough for the"
+                " applications that require its presence in"
+                " order to work properly.")
+        else:
+          print("ERROR: This font lacks a digital signature"
+                " (DSIG table). Some applications may required"
+                " one (even if only a dummy placeholder)"
+                " in order to work properly.")
 
-
-
+      except ImportError:
+        error_message = ("The '{}' font does not have an existing"
+                         " digital signature (DSIG), so OpenType features"
+                         " will not be available in some applications that"
+                         " use its presense as a (stupid) heuristic."
+                         " So we need to add one. But for that we'll need "
+                         "Fonttools v2.3+ so you need to upgrade it. Try:"
+                         " $ pip install --upgrade fontTools; or see"
+                         " https://pypi.python.org/pypi/FontTools")
+        print ("ERROR: " + error_message.format(font_file))
 
 if __name__ == '__main__':
-    description = 'Fixes TTF to have a dummy DSIG table'
-    parser = argparse.ArgumentParser(description=description)
-    parser.add_argument('ttf_font', nargs='+',
-                        help="Font where to create DSIG table inside")
-    parser.add_argument('--autofix', action='store_true', help='Apply autofix')
-
-    args = parser.parse_args()
-
-    for path in args.ttf_font:
-        if not os.path.exists(path):
-            continue
-        CreateDSIGFixer(None, path).apply()
-
-
-
+  main()
