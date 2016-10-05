6ae253124c56e410a7505a67627133ba604d415f
Felipe Corrêa da Silva Sanches <fsanches@metamaquina.com.br>
2016-10-05 18:54:29 -0300

    make "coords out of bounds" check unconditionaly a warning
    (issue #1099)

--- fontbakery-check-ttf.py
+++ fontbakery-check-ttf.py
@@ -2979,17 +2979,13 @@ def main():
            y < glyph.yMin or y > glyph.yMax or \
            abs(x) > 32766 or abs(y) > 32766:
           failed = True
-          if style == "Italic":
-            fb.warning(("Glyph '{}' coordinates ({},{})"
-                        " out of bounds."
-                        " This happens a lot when points are not extremes."
-                        " Beware though that placing points on extremes"
-                        " in the Italics does more harm than good,"
-                        " especially with rounded fonts."
-                        "").format(glyphName, x, y))
-          else:
-            fb.error(("Glyph '{}' coordinates ({},{})"
-                      " out of bounds!").format(glyphName, x, y))
+          fb.warning(("Glyph '{}' coordinates ({},{})"
+                      " out of bounds."
+                      " This happens a lot when points are not extremes."
+                      " Beware though that placing points on extremes"
+                      " in the Italics does more harm than good,"
+                      " especially with rounded fonts."
+                      "").format(glyphName, x, y))
     if not failed:
       fb.ok("All glyph paths have coordinates within bounds!")
 
