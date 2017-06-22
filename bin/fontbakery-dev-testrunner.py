f115f39c687fa415834dc0f3ad56f8c6ce632fbe
Lasse Fister <commander@graphicore.de>
2017-06-22 15:55:13 +0200

    Finetune log-statuses: add DEBUG, reorder weights, relax some rules.

--- bin/fontbakery-dev-testrunner.py
+++ bin/fontbakery-dev-testrunner.py
@@ -3,7 +3,8 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
 from fontbakery.testrunner import (
-              INFO
+              DEBUG
+            , INFO
             , WARN
             , ERROR
             , SKIP
