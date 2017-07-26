7e2f964a634661cf4917f59eb4b13ec5998d3ae9
Lasse Fister <commander@graphicore.de>
2017-07-26 18:19:03 +0200

    Introduce a Message class for test cases and better unit testing.

--- Lib/fontbakery/specifications/googlefonts_test.py
+++ Lib/fontbakery/specifications/googlefonts_test.py
@@ -4,15 +4,17 @@ from __future__ import absolute_import, print_function, unicode_literals, divisi
 import pytest
 
 from fontbakery.testrunner import (
-              PASS
-#           , INFO
-#           , WARN
-#           , ERROR
-#           , SKIP
+              DEBUG
+            , INFO
+            , WARN
+            , ERROR
+            , SKIP
+            , PASS
             , FAIL
             , ENDTEST
-#            , Section
             )
+
+test_statuses = (ERROR, FAIL, SKIP, PASS, WARN, INFO, DEBUG)
 
 from fontTools.ttLib import TTFont
 
@@ -36,8 +38,13 @@ def delete_name_table_id(ttFont, nameID):
   for i in sorted(delete, reverse=True):
     del(ttFont['name'].names[i])
 
-def test_id_029(font_1):
-  """ This test is run via the testRunner and demonstrate how to get
+
+def test_example_testrunner_based(font_1):
+  """ This is just an example test. We'll probably need something like
+      this setup in a testrunner_test.py testsuite.
+      Leave it here for the moment until we implemented a real case.
+
+      This test is run via the testRunner and demonstrate how to get
       (mutable) objects from the conditions cache and change them.
 
       NOTE: the actual fontbakery tests of conditions should never change
@@ -52,8 +59,10 @@ def test_id_029(font_1):
   print('Test PASS ...')
   # run
   for status, message, (section, test, iterargs) in runner.run():
+    if status in test_statuses:
+      last_test_message = message
     if status == ENDTEST:
-     assert message == PASS
+     assert message == PASS and last_test_message.code == 'good'
      break
 
   # we could also reuse the `iterargs` that was assigned in the previous
@@ -66,9 +75,10 @@ def test_id_029(font_1):
   change_name_table_id(ttFont, NAMEID_LICENSE_DESCRIPTION, 'failing entry')
   # run
   for status, message, (section, test, iterargs) in runner.run():
-    print(status, 'message', message)
+    if status in test_statuses:
+      last_test_message = message
     if status == ENDTEST:
-     assert message == FAIL
+     assert message == FAIL and last_test_message.code == 'wrong'
      break
 
   print('Test missing entry ...')
@@ -76,8 +86,10 @@ def test_id_029(font_1):
   delete_name_table_id(ttFont, NAMEID_LICENSE_DESCRIPTION)
   # run
   for status, message, (section, test, iterargs) in runner.run():
+    if status in test_statuses:
+      last_test_message = message
     if status == ENDTEST:
-     assert message == FAIL
+     assert message == FAIL and last_test_message.code == 'missing'
      break
 
 def test_id_029_shorter(font_1):
@@ -95,19 +107,19 @@ def test_id_029_shorter(font_1):
   print('Test PASS ...')
   # run
   status, message = list(check_copyright_entries_match_license(ttFont, license))[-1]
-  assert status == PASS
+  assert status == PASS and message.code == 'good'
 
   print('Test failing entry ...')
   # prepare
   change_name_table_id(ttFont, NAMEID_LICENSE_DESCRIPTION, 'failing')
   # run
   status, message = list(check_copyright_entries_match_license(ttFont, license))[-1]
-  assert status == FAIL
+  assert status == FAIL and message.code == 'wrong'
 
   print('Test missing entry ...')
   # prepare
   delete_name_table_id(ttFont, NAMEID_LICENSE_DESCRIPTION)
   # run
   status, message = list(check_copyright_entries_match_license(ttFont, license))[-1]
-  assert status == FAIL
+  assert status == FAIL and message.code == 'missing'
 
