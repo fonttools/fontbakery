e3d5cccaa73d1e49fb80eb0f8ff22dd43e8531ab
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2017-08-16 15:48:14 -0300

    chmod a+x check-noto (and add #!/bin/bash line)

--- bin/fontbakery-check-noto-version.sh
+++ bin/fontbakery-check-noto-version.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 python bin/fontbakery-check-font-version.py "Noto Sans" -wc='http://noto-website.storage.googleapis.com/pkgs/NotoSans-unhinted.zip'
 python bin/fontbakery-check-font-version.py "Noto Serif" -wc='https://noto-website.storage.googleapis.com/pkgs/NotoSerif-unhinted.zip'
