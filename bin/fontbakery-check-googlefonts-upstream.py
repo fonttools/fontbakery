967bb0df004f2de81e940e7d1fd1ff521c3a888a
Marc Foley <m.foley.88@gmail.com>
2017-12-06 14:10:42 +0000

    Added bin script

---
+++ bin/fontbakery-check-googlefonts-upstream.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+if __name__ == '__main__':
+  from fontbakery.commands.check_googlefonts_upstream import main
+  main()
