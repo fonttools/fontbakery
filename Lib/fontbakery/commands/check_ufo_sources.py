f6174728a5bf3d012285ada7fbef0a62e705c37e
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2018-03-19 13:03:20 +0000

    Initial support for running on UFO directories.
    First test runs ufolint on *ufo.

---
+++ Lib/fontbakery/commands/check_ufo_sources.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, print_function, unicode_literals
+
+from functools import partial
+from fontbakery.specifications.ufo_sources import specification
+from fontbakery.commands.check_spec import (
+                                            runner_factory as super_runner_factory
+                                          , main as super_main
+                                          )
+
+# The values dict will probably get one or more specific blacklists
+# for the google font project. It would be good if it was not necessary
+# to copy paste this kind of configuration, thus a central init for
+# the google/fonts repository is good.
+GOOGLEFONTS_SPECIFICS = {}
+
+# runner_factory is used by the fontbakery dashboard.
+# It is here in order to have a single place from which
+# the spec is configured for the CLI and the worker.
+def runner_factory(fonts):
+    values = {}
+    values.update(GOOGLEFONTS_SPECIFICS)
+    values['fonts'] = fonts
+    return super_runner_factory(specification, values=values)
+
+main = partial(super_main, specification, values=GOOGLEFONTS_SPECIFICS)
+
---
+++ Lib/fontbakery/specifications/ufo_sources.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+#
+# This file has been automatically formatted with `yapf --style '
+# {based_on_style: google}'` and `docformatter`.
+from __future__ import (absolute_import, print_function, unicode_literals,
+                        division)
+
+from fontbakery.checkrunner import (INFO, WARN, ERROR, SKIP, PASS, FAIL,
+                                    Section, Spec)
+import os
+from fontbakery.callable import condition, check
+from fontbakery.message import Message
+from fontbakery.constants import (
+    # TODO: priority levels are not yet part of the new runner/reporters.
+    # How did we ever use this information?
+    # Check priority levels:
+    CRITICAL,
+    IMPORTANT
+    #     , NORMAL
+    #     , LOW
+    #     , TRIVIAL
+)
+
+default_section = Section('Default')
+
+
+class UFOSpec(Spec):
+
+    def setup_argparse(self, argument_parser):
+        """Set up custom arguments needed for this spec."""
+        import glob
+        import logging
+        import argparse
+
+        def get_fonts(pattern):
+
+            fonts_to_check = []
+            # use glob.glob to accept *.ufo
+
+            for fullpath in glob.glob(pattern):
+                if fullpath.endswith(".ufo"):
+                    fonts_to_check.append(fullpath)
+                else:
+                    logging.warning(
+                        ("Skipping '{}' as it does not seem "
+                         "to be valid UFO source directory.").format(fullpath))
+            return fonts_to_check
+
+        class MergeAction(argparse.Action):
+
+            def __call__(self, parser, namespace, values, option_string=None):
+                target = [item for l in values for item in l]
+                setattr(namespace, self.dest, target)
+
+        argument_parser.add_argument(
+            'fonts',
+            nargs='+',
+            type=get_fonts,
+            action=MergeAction,
+            help='font file path(s) to check.'
+            ' Wildcards like *.ufo are allowed.')
+        return ('fonts',)
+
+
+# ----------------------------------------------------------------------------
+# This variable serves as an exportable anchor point, see e.g. the
+# Lib/fontbakery/commands/check_ufo_sources.py script.
+specification = UFOSpec(
+    default_section=default_section,
+    iterargs={'font': 'fonts'},
+    derived_iterables={'ufo_sources': ('ufo_source', True)}
+)
+
+register_check = specification.register_check
+register_condition = specification.register_condition
+# ----------------------------------------------------------------------------
+
+
+@register_condition
+@condition
+def ufo_source(font):
+    import defcon
+    return defcon.Font(font)
+
+
+@register_check
+@check(id='com.google.ufos/check/001', priority=CRITICAL)
+def com_google_ufos_check_001(font):
+    """Run ufolint on UFO source directory."""
+    import subprocess
+    ufolint_cmd = ["ufolint", font]
+
+    try:
+        subprocess.check_output(ufolint_cmd, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        yield FAIL, ("ufolint failed the UFO source. Output follows :"
+                     "\n\n{}\n").format(e.output)
+    except OSError:
+        yield ERROR, "ufolint is not available!"
+
+    yield PASS, "ufolint passed the UFO source."
+
+
+for section_name, section in specification._sections.items():
+    print("There is a total of {} checks on {}.".format(
+        len(section._checks), section_name))
