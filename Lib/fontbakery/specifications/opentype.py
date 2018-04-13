2169731c2f236a39210d650847c1e2085e00ee2a
Lasse Fister <commander@graphicore.de>
2018-04-13 19:51:56 +0200

    [specifications] add opentype general specification

---
+++ Lib/fontbakery/specifications/opentype.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from fontbakery.fonts_spec import spec_factory  # NOQA pylint: disable=unused-import
+spec_imports = (
+    ('.', ('general', 'cmap', 'head', 'os2', 'post', 'name',
+       'hhea', 'dsig', 'hmtx', 'gpos', 'gdef', 'kern', 'glyf',
+       'prep', 'fvar', 'shared_conditions')
+    ),
+)
+
