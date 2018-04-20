726d3a8a723b9b0ee9ad72b03fde355dd99f9628
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2018-04-20 13:51:47 +0100

    Skip glyph name tests if post format 3.0

--- Lib/fontbakery/specifications/general.py
+++ Lib/fontbakery/specifications/general.py
@@ -563,7 +563,7 @@ def com_google_fonts_check_058(ttFont):
   """Glyph names are all valid?"""
   if ttFont.sfntVersion == b'\x00\x01\x00\x00' and ttFont.get(
       "post") and ttFont["post"].formatType == 3.0:
-    yield PASS, ("TrueType fonts with a format 3.0 post table contain no"
+    yield SKIP, ("TrueType fonts with a format 3.0 post table contain no"
                  " glyph names.")
   else:
     import re
@@ -603,7 +603,7 @@ def com_google_fonts_check_059(ttFont):
   """Font contains unique glyph names?"""
   if ttFont.sfntVersion == b'\x00\x01\x00\x00' and ttFont.get(
       "post") and ttFont["post"].formatType == 3.0:
-    yield PASS, ("TrueType fonts with a format 3.0 post table contain no"
+    yield SKIP, ("TrueType fonts with a format 3.0 post table contain no"
                  " glyph names.")
   else:
     import re
