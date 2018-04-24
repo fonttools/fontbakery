ef72f5b16432ddc4f3259257bc0f4a8d6bd41671
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2018-04-24 14:28:24 +0100

    Test check 050
    Simplify test to check for equal width of space and nbsp.

--- Lib/fontbakery/specifications/hmtx.py
+++ Lib/fontbakery/specifications/hmtx.py
@@ -3,41 +3,30 @@ from __future__ import (absolute_import, division, print_function,
 
 from fontbakery.callable import check
 from fontbakery.checkrunner import FAIL, PASS
-from fontbakery.message import Message
 # used to inform get_module_specification whether and how to create a specification
-from fontbakery.fonts_spec import spec_factory # NOQA pylint: disable=unused-import
+from fontbakery.fonts_spec import spec_factory  # NOQA pylint: disable=unused-import
 
-spec_imports = [
-    ('.shared_conditions', ('missing_whitespace_chars', ))
-]
+spec_imports = [('.shared_conditions', ('missing_whitespace_chars',))]
 
 
 @check(
     id='com.google.fonts/check/050',
     conditions=['not missing_whitespace_chars'])
 def com_google_fonts_check_050(ttFont):
-  """Whitespace glyphs have coherent widths?"""
-  from fontbakery.utils import getGlyph
+  """Whitespace and non-breaking space have the same width?"""
+  from fontbakery.utils import get_glyph_name
 
-  def getGlyphWidth(font, glyph):
-    return font['hmtx'][glyph][0]
+  space_name = get_glyph_name(ttFont, 0x0020)
+  nbsp_name = get_glyph_name(ttFont, 0x00A0)
 
-  space = getGlyph(ttFont, 0x0020)
-  nbsp = getGlyph(ttFont, 0x00A0)
+  space_width = ttFont['hmtx'][space_name][0]
+  nbsp_width = ttFont['hmtx'][nbsp_name][0]
 
-  spaceWidth = getGlyphWidth(ttFont, space)
-  nbspWidth = getGlyphWidth(ttFont, nbsp)
-
-  if spaceWidth != nbspWidth or nbspWidth < 0:
-    if nbspWidth > spaceWidth and spaceWidth >= 0:
-      yield FAIL, Message("bad_space", ("space {} nbsp {}: Space advanceWidth"
-                                        " needs to be fixed"
-                                        " to {}.").format(
-                                            spaceWidth, nbspWidth, nbspWidth))
-    else:
-      yield FAIL, Message("bad_nbsp", ("space {} nbsp {}: Nbsp advanceWidth"
-                                       " needs to be fixed "
-                                       "to {}").format(spaceWidth, nbspWidth,
-                                                       spaceWidth))
+  if space_width > 0 and space_width == nbsp_width:
+    yield PASS, "Whitespace and non-breaking space have the same width."
   else:
-    yield PASS, "Whitespace glyphs have coherent widths."
+    yield FAIL, ("Whitespace and non-breaking space have differing width:"
+                 " Whitespace ({}) is {} font units wide, non-breaking space"
+                 " ({}) is {} font units wide. Both should be positive and the"
+                 " same.").format(space_name, space_width, nbsp_name,
+                                  nbsp_width)
--- tests/specifications/hmtx_test.py
+++ tests/specifications/hmtx_test.py
@@ -1,27 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals, division
 
-from fontbakery.checkrunner import (
-              DEBUG
-            , INFO
-            , WARN
-            , ERROR
-            , SKIP
-            , PASS
-            , FAIL
-            )
+import os
+
+from fontbakery.checkrunner import (DEBUG, INFO, WARN, ERROR, SKIP, PASS, FAIL)
+from fontTools.ttLib import TTFont
 
 check_statuses = (ERROR, FAIL, SKIP, PASS, WARN, INFO, DEBUG)
 
-# from fontTools.ttLib import TTFont
 
-# the original check itself has unclear semantics, so that needs to be reviewed first
-def NOT_IMPLEMENTED_test_check_050():
+def test_check_050():
   """ Whitespace glyphs have coherent widths? """
-  # from fontbakery.specifications.hmtx import com_google_fonts_check_050 as check
-  # TODO: Implement-me!
-  #
-  # code-paths:
-  # - FAIL, code="bad_space"
-  # - FAIL, code="bad_nbsp"
-  # - PASS
+  from fontbakery.specifications.hmtx import com_google_fonts_check_050 as check
+
+  test_font = TTFont(
+      os.path.join("data", "test", "nunito", "Nunito-Regular.ttf"))
+  status, _ = list(check(test_font))[-1]
+  assert status == PASS
+
+  test_font["hmtx"].metrics["space"] = (0, 1)
+  status, _ = list(check(test_font))[-1]
+  assert status == FAIL
