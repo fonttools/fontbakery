89dea9e11e376df1b8a53a7e6585e278e5f138f9
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2018-04-24 12:43:37 +0100

    Reformat ufo_sources.py to match indent of 2

--- Lib/fontbakery/specifications/ufo_sources.py
+++ Lib/fontbakery/specifications/ufo_sources.py
@@ -12,47 +12,48 @@ from fontbakery.callable import FontBakeryExpectedValue as ExpectedValue
 from fontbakery.checkrunner import ERROR, FAIL, PASS, WARN, Section, Spec
 from fontbakery.constants import CRITICAL
 
+
 class UFOSpec(Spec):
 
-    def setup_argparse(self, argument_parser):
-        """Set up custom arguments needed for this spec."""
-        import glob
-        import logging
-        import argparse
+  def setup_argparse(self, argument_parser):
+    """Set up custom arguments needed for this spec."""
+    import glob
+    import logging
+    import argparse
 
-        def get_fonts(pattern):
+    def get_fonts(pattern):
 
-            fonts_to_check = []
-            # use glob.glob to accept *.ufo
+      fonts_to_check = []
+      # use glob.glob to accept *.ufo
 
-            for fullpath in glob.glob(pattern):
-                fullpath_absolute = os.path.abspath(fullpath)
-                if fullpath_absolute.lower().endswith(".ufo") and os.path.isdir(
-                        fullpath_absolute):
-                    fonts_to_check.append(fullpath)
-                else:
-                    logging.warning(
-                        ("Skipping '{}' as it does not seem "
-                         "to be valid UFO source directory.").format(fullpath))
-            return fonts_to_check
+      for fullpath in glob.glob(pattern):
+        fullpath_absolute = os.path.abspath(fullpath)
+        if fullpath_absolute.lower().endswith(".ufo") and os.path.isdir(
+            fullpath_absolute):
+          fonts_to_check.append(fullpath)
+        else:
+          logging.warning(
+              ("Skipping '{}' as it does not seem "
+               "to be valid UFO source directory.").format(fullpath))
+      return fonts_to_check
 
-        class MergeAction(argparse.Action):
+    class MergeAction(argparse.Action):
 
-            def __call__(self, parser, namespace, values, option_string=None):
-                target = [item for l in values for item in l]
-                setattr(namespace, self.dest, target)
+      def __call__(self, parser, namespace, values, option_string=None):
+        target = [item for l in values for item in l]
+        setattr(namespace, self.dest, target)
 
-        argument_parser.add_argument(
-            'fonts',
-            # To allow optional commands like "-L" to work without other input
-            # files:
-            nargs='*',
-            type=get_fonts,
-            action=MergeAction,
-            help='font file path(s) to check.'
-            ' Wildcards like *.ufo are allowed.')
+    argument_parser.add_argument(
+        'fonts',
+        # To allow optional commands like "-L" to work without other input
+        # files:
+        nargs='*',
+        type=get_fonts,
+        action=MergeAction,
+        help='font file path(s) to check.'
+        ' Wildcards like *.ufo are allowed.')
 
-        return ('fonts',)
+    return ('fonts',)
 
 
 fonts_expected_value = ExpectedValue(
@@ -81,79 +82,78 @@ register_condition = specification.register_condition
 @register_condition
 @condition
 def ufo_font(font):
-    import defcon
-    return defcon.Font(font)
+  import defcon
+  return defcon.Font(font)
 
 
 @register_check
 @check(id='com.daltonmaag/check/ufolint', priority=CRITICAL)
 def com_daltonmaag_check_ufolint(font):
-    """Run ufolint on UFO source directory."""
-    import subprocess
-    ufolint_cmd = ["ufolint", font]
+  """Run ufolint on UFO source directory."""
+  import subprocess
+  ufolint_cmd = ["ufolint", font]
 
-    try:
-        subprocess.check_output(ufolint_cmd, stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as e:
-        yield FAIL, ("ufolint failed the UFO source. Output follows :"
-                     "\n\n{}\n").format(e.output)
-    except OSError:
-        yield ERROR, "ufolint is not available!"
-    else:
-        yield PASS, "ufolint passed the UFO source."
+  try:
+    subprocess.check_output(ufolint_cmd, stderr=subprocess.STDOUT)
+  except subprocess.CalledProcessError as e:
+    yield FAIL, ("ufolint failed the UFO source. Output follows :"
+                 "\n\n{}\n").format(e.output)
+  except OSError:
+    yield ERROR, "ufolint is not available!"
+  else:
+    yield PASS, "ufolint passed the UFO source."
 
 
 @register_check
 @check(id='com.daltonmaag/check/required-fields')
 def com_daltonmaag_check_required_fields(ufo_font):
-    """Check that required fields are present in the UFO fontinfo.
+  """Check that required fields are present in the UFO fontinfo.
 
     ufo2ft requires these info fields to compile a font binary:
     unitsPerEm, ascender, descender, xHeight, capHeight and familyName.
     """
-    recommended_fields = []
+  recommended_fields = []
 
-    for field in [
-            "unitsPerEm", "ascender", "descender", "xHeight", "capHeight",
-            "familyName"
-    ]:
-        if ufo_font.info.__dict__.get("_" + field) is None:
-            recommended_fields.append(field)
+  for field in [
+      "unitsPerEm", "ascender", "descender", "xHeight", "capHeight",
+      "familyName"
+  ]:
+    if ufo_font.info.__dict__.get("_" + field) is None:
+      recommended_fields.append(field)
 
-    if recommended_fields:
-        yield FAIL, "Required field(s) missing: {}".format(recommended_fields)
-    else:
-        yield PASS, "Required fields present."
+  if recommended_fields:
+    yield FAIL, "Required field(s) missing: {}".format(recommended_fields)
+  else:
+    yield PASS, "Required fields present."
 
 
 @register_check
 @check(id='com.daltonmaag/check/recommended-fields')
 def com_daltonmaag_check_recommended_fields(ufo_font):
-    """Check that recommended fields are present in the UFO fontinfo.
+  """Check that recommended fields are present in the UFO fontinfo.
 
     This includes fields that should be in any production font.
     """
-    recommended_fields = []
+  recommended_fields = []
 
-    for field in [
-            "postscriptUnderlineThickness", "postscriptUnderlinePosition",
-            "versionMajor", "versionMinor", "styleName", "copyright",
-            "openTypeOS2Panose"
-    ]:
-        if ufo_font.info.__dict__.get("_" + field) is None:
-            recommended_fields.append(field)
+  for field in [
+      "postscriptUnderlineThickness", "postscriptUnderlinePosition",
+      "versionMajor", "versionMinor", "styleName", "copyright",
+      "openTypeOS2Panose"
+  ]:
+    if ufo_font.info.__dict__.get("_" + field) is None:
+      recommended_fields.append(field)
 
-    if recommended_fields:
-        yield WARN, "Recommended field(s) missing: {}".format(
-            recommended_fields)
-    else:
-        yield PASS, "Recommended fields present."
+  if recommended_fields:
+    yield WARN, "Recommended field(s) missing: {}".format(recommended_fields)
+  else:
+    yield PASS, "Recommended fields present."
 
 
 @register_check
 @check(id='com.daltonmaag/check/unnecessary-fields')
 def com_daltonmaag_check_unnecessary_fields(ufo_font):
-    """Check that no unnecessary fields are present in the UFO fontinfo.
+  """Check that no unnecessary fields are present in the UFO fontinfo.
 
     ufo2ft will generate these.
 
@@ -163,20 +163,19 @@ def com_daltonmaag_check_unnecessary_fields(ufo_font):
 
     year is deprecated since UFO v2.
     """
-    unnecessary_fields = []
+  unnecessary_fields = []
 
-    for field in [
-            "openTypeNameUniqueID", "openTypeNameVersion", "postscriptUniqueID",
-            "year"
-    ]:
-        if ufo_font.info.__dict__.get("_" + field) is not None:
-            unnecessary_fields.append(field)
+  for field in [
+      "openTypeNameUniqueID", "openTypeNameVersion", "postscriptUniqueID",
+      "year"
+  ]:
+    if ufo_font.info.__dict__.get("_" + field) is not None:
+      unnecessary_fields.append(field)
 
-    if unnecessary_fields:
-        yield WARN, "Unnecessary field(s) present: {}".format(
-            unnecessary_fields)
-    else:
-        yield PASS, "Unnecessary fields omitted."
+  if unnecessary_fields:
+    yield WARN, "Unnecessary field(s) present: {}".format(unnecessary_fields)
+  else:
+    yield PASS, "Unnecessary fields omitted."
 
 
 # The following fields are always generated empty by defcon:
@@ -186,5 +185,5 @@ def com_daltonmaag_check_unnecessary_fields(ufo_font):
 # is useful.
 
 for section_name, section in specification._sections.items():
-    print("There is a total of {} checks on {}.".format(
-        len(section._checks), section_name))
+  print("There is a total of {} checks on {}.".format(
+      len(section._checks), section_name))
