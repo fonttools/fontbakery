873fb44701172d2260844e4ab39d64e2292b0ce1
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2018-05-28 18:03:36 +0100

    Test for expected and unexpected checks

--- tests/specifications/external_spec_test.py
+++ tests/specifications/external_spec_test.py
@@ -20,6 +20,13 @@ def test_external_specification():
   specification = spec_factory(default_section=Section("Dalton Maag OpenType"))
   specification.set_check_filter(check_filter)
   specification.auto_register(
-      globals(), spec_imports=['fontbakery.specifications.opentype'])
+      globals(), spec_imports=["fontbakery.specifications.opentype"])
+
+  # Probe some tests
+  expected_tests = ["com.google.fonts/check/002", "com.google.fonts/check/180"]
+  specification.test_expected_checks(expected_tests)
+
+  # Probe tests we don't want
+  assert "com.google.fonts/check/035" not in specification._check_registry.keys()
 
   assert len(specification.sections) > 1
