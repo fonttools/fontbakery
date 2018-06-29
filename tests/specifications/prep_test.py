e26b0bc993aa136cf6ff68321b5738193615df94
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-06-29 02:39:38 -0300

    fix check and code-test: check/072 (prep table)
    "smart dropout control enabled in prep table"
    (issue #1945)

--- tests/specifications/prep_test.py
+++ tests/specifications/prep_test.py
@@ -19,22 +19,18 @@ from fontTools.ttLib import TTFont
 
 def test_check_072():
   """ Font enables smart dropout control in "prep" table instructions? """
-  # TODO: Implement-me!
-  #
-  # code-paths:
-  # - SKIP, "Not applicable to a CFF font."
-  # - PASS, "Program at 'prep' table contains instructions enabling smart dropout control."
-  # - WARN, "Font does not contain TrueType instructions enabling
-  #          smart dropout control in the 'prep' table program."
   from fontbakery.specifications.prep import com_google_fonts_check_072 as check
 
   test_font_path = os.path.join("data", "test", "nunito", "Nunito-Regular.ttf")
 
+  # - PASS, "Program at 'prep' table contains instructions enabling smart dropout control."
   test_font = TTFont(test_font_path)
   status, _ = list(check(test_font))[-1]
   assert status == PASS
 
+  # - FAIL, "Font does not contain TrueType instructions enabling
+  #          smart dropout control in the 'prep' table program."
   import array
   test_font["prep"].program.bytecode = array.array('B', [0])
   status, _ = list(check(test_font))[-1]
-  assert status == WARN
+  assert status == FAIL
