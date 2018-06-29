71d2abb615d4c95817468dc95b4a7f6f78fda03e
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2018-06-29 02:39:38 -0300

    fix check and code-test: check/072 (prep table)
    "smart dropout control enabled in prep table"
    (issue #1945)

--- Lib/fontbakery/specifications/prep.py
+++ Lib/fontbakery/specifications/prep.py
@@ -2,7 +2,7 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from fontbakery.callable import check
-from fontbakery.checkrunner import PASS, WARN
+from fontbakery.checkrunner import PASS, FAIL
 # used to inform get_module_specification whether and how to create a specification
 from fontbakery.fonts_spec import spec_factory # NOQA pylint: disable=unused-import
 
