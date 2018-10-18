24677ac6216ee94562da64eac0eea44688ba9b83
Chris Simpkins <git.simpkins@gmail.com>
2018-10-17 21:45:00 -0400

    [docs] convert to default RTD theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,6 +13,8 @@
 import os
 import sys
 
+import recommonmark.parser
+
 sys.path.insert(0, os.path.abspath("../Lib"))
 
 
@@ -29,8 +31,6 @@ release = ""
 
 
 # -- General configuration ---------------------------------------------------
-
-import recommonmark.parser
 
 # If your documentation needs a minimal Sphinx version, state it here.
 #
@@ -74,7 +74,7 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
