957ca7427fa0818c7ef7102fa85464fa00610311
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-01-28 20:53:44 -0200

    wip

---
+++ ligature_stats.py
@@ -0,0 +1,116 @@
+import os
+import sys
+from fontTools.ttLib import TTFont
+
+#if len(sys.argv) != 2:
+#  sys.exit(f"usage: {sys.argv[0]} fontfile.ttf")
+#
+#fontname = sys.argv[1]
+
+def ligatures(L):
+  ligs = []
+  for x in L:
+    for subtable in x.SubTable:
+      try:
+        for glyph, ligatures in subtable.ligatures.items():
+          for ligature in ligatures:
+            lig = f"{glyph} {' '.join(ligature.Component)}" #" -> {ligature.LigGlyph}"
+            ligs.extend([lig])
+      except:
+        print("duh!")
+  return ligs
+
+stats = {}
+def compute_stats(dlig_list,
+                  clig_list,
+                  liga_list):
+  global stats
+  for ligature in dlig_list:
+    if ligature in stats:
+      stats[ligature]["dlig"] += 1
+    else:
+      stats[ligature] = {"dlig": 1, "clig": 0, "liga": 0}
+
+  for ligature in clig_list:
+    if ligature in stats:
+      stats[ligature]["clig"] += 1
+    else:
+      stats[ligature] = {"dlig": 0, "clig": 1, "liga": 0}
+
+  for ligature in liga_list:
+    if ligature in stats:
+      stats[ligature]["liga"] += 1
+    else:
+      stats[ligature] = {"dlig": 0, "clig": 0, "liga": 1}
+
+badguys = []
+for fontdir in open("latin-ext.txt").readlines():
+  dligs_in_family = []
+  cligs_in_family = []
+  ligas_in_family = []
+  for fontname in os.listdir(fontdir.strip()):
+    if not fontname.endswith(".ttf"):
+      continue
+
+    fullpath = os.path.join(fontdir.strip(), fontname)
+    print(f"fullpath: '{fullpath}'")
+    try:
+      ttFont = TTFont(fullpath)
+    except:
+      print ("OuCH!!")
+      continue
+
+    if "GSUB" not in ttFont:
+      continue #sys.exit("Font lacks a GSUB table!")
+
+    dlig_indices = set()
+    clig_indices = set()
+    liga_indices = set()
+    if not ttFont["GSUB"].table.FeatureList:
+      continue
+
+    for f in ttFont["GSUB"].table.FeatureList.FeatureRecord:
+      for index in f.Feature.LookupListIndex:
+        if f.FeatureTag == 'dlig':
+            dlig_indices.add(index)
+        elif f.FeatureTag == 'clig':
+            clig_indices.add(index)
+        elif f.FeatureTag == 'liga':
+            liga_indices.add(index)
+
+    dlig = [ttFont["GSUB"].table.LookupList.Lookup[index]
+            for index in dlig_indices]
+    clig = [ttFont["GSUB"].table.LookupList.Lookup[index]
+            for index in clig_indices]
+    liga = [ttFont["GSUB"].table.LookupList.Lookup[index]
+            for index in liga_indices]
+
+    for l in ligatures(dlig):
+      if l not in dligs_in_family:
+        dligs_in_family.append(l)
+
+    for l in ligatures(liga):
+      if l == 's t' and l not in dlig:
+        if l not in badguys:
+          print(f"DEBUG: '{l}': {fullpath}")
+          badguys.append(l)
+
+      if l not in ligas_in_family:
+        ligas_in_family.append(l)
+
+    for l in ligatures(clig):
+      if l not in cligs_in_family:
+        cligs_in_family.append(l)
+
+  compute_stats(dligs_in_family,
+                cligs_in_family,
+                ligas_in_family)
+
+#import ipdb; ipdb.set_trace()
+stats = sorted(stats.items(), key=lambda x: x[1]["dlig"] + x[1]["clig"] + x[1]["liga"], reverse=False)
+for l in stats:
+  total = l[1]["dlig"] + l[1]["clig"] + l[1]["liga"]
+  if l[1]["liga"] not in [0, total] or \
+     l[1]["clig"] not in [0, total] or \
+     l[1]["dlig"] not in [0, total]:
+    print (f"'{l[0]}': {l[1]}")
