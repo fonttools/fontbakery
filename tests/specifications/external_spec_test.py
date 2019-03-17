4c933293c9a9993ff0ed1516ec1ebbd78327543b
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-03-17 17:08:17 -0300

    updating renamed check IDs in external_spec_test.py
    (issue #2238)

--- tests/specifications/external_spec_test.py
+++ tests/specifications/external_spec_test.py
@@ -5,11 +5,11 @@ from fontbakery.fonts_spec import spec_factory
 def check_filter(item_type, item_id, item):
   # Filter out external tool checks for testing purposes.
   if item_type == "check" and item_id in (
-      "com.google.fonts/check/035",  # ftxvalidator
-      "com.google.fonts/check/036",  # ots-sanitize
+      "com.google.fonts/check/ftxvalidator",
+      "com.google.fonts/check/ots",
       "com.google.fonts/check/037",  # Font Validator
-      "com.google.fonts/check/038",  # Fontforge
-      "com.google.fonts/check/039",  # Fontforge
+      "com.google.fonts/check/fontforge_stderr",
+      "com.google.fonts/check/fontforge",
   ):
     return False
 
@@ -30,7 +30,7 @@ def test_external_specification():
   specification.test_expected_checks(expected_tests)
 
   # Probe tests we don't want
-  assert "com.google.fonts/check/035" not in specification._check_registry.keys()
+  assert "com.google.fonts/check/ftxvalidator" not in specification._check_registry.keys()
 
   assert len(specification.sections) > 1
 
