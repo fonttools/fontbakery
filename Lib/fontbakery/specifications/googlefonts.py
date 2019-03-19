24e608f126ceb28ad54771c2c1103315b65fd3b7
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-03-19 18:58:57 -0300

    check/065 => com.google.fonts/check/kerning_for_non_ligated_sequences
    (issue #2238)

--- Lib/fontbakery/specifications/googlefonts.py
+++ Lib/fontbakery/specifications/googlefonts.py
@@ -131,7 +131,7 @@ expected_check_ids = \
       , 'com.google.fonts/check/062' # Is 'gasp' table correctly set?
       , 'com.google.fonts/check/063' # Does GPOS table have kerning information?
       , 'com.google.fonts/check/ligature_carets'
-      , 'com.google.fonts/check/065' # Is there kerning info for non-ligated sequences?
+      , 'com.google.fonts/check/kerning_for_non_ligated_sequences'
       , 'com.google.fonts/check/066' # Is there a "kern" table declared in the font?
       , 'com.google.fonts/check/067' # Make sure family name does not begin with a digit.
       , 'com.google.fonts/check/068' # Does full font name begin with the font family name?
