10ec2b06c7118d9c37189663d30ef2a86a3f7178
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-03-19 19:41:47 -0300

    check/168 => com.google.fonts/check/varfont/regular_wdth_coord
    (issue #2238)

--- Lib/fontbakery/specifications/adobe_fonts.py
+++ Lib/fontbakery/specifications/adobe_fonts.py
@@ -69,7 +69,7 @@ expected_check_ids = [
     'com.google.fonts/check/152',  # Name table strings must not contain 'Reserved Font Name'.
     'com.google.fonts/check/family_and_style_max_length',
     'com.google.fonts/check/varfont/regular_wght_coord',
-    'com.google.fonts/check/168',  # The variable font 'wdth' (Width) axis coordinate must be 100 on the 'Regular' instance.
+    'com.google.fonts/check/varfont/regular_wdth_coord',
     'com.google.fonts/check/169',  # The variable font 'slnt' (Slant) axis coordinate must be zero on the 'Regular' instance.
     'com.google.fonts/check/170',  # The variable font 'ital' (Italic) axis coordinate must be zero on the 'Regular' instance.
     'com.google.fonts/check/171',  # The variable font 'opsz' (Optical Size) axis coordinate should be between 9 and 13 on the 'Regular' instance.
