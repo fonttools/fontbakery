77fc7c45bbb4cdb2ff3172dd6c13d90ec0cc94f0
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-03-19 21:45:02 -0300

    check/180 => com.google.fonts/check/loca/maxp_num_glyphs
    (issue #2238)

--- tests/specifications/loca_test.py
+++ tests/specifications/loca_test.py
@@ -7,9 +7,9 @@ from fontbakery.utils import TEST_FILE
 from fontbakery.checkrunner import PASS, FAIL
 
 
-def test_check_180():
+def test_check_loca_maxp_num_glyphs():
   """Does the number of glyphs in the loca table match the maxp table?"""
-  from fontbakery.specifications.loca import com_google_fonts_check_180 as check
+  from fontbakery.specifications.loca import com_google_fonts_check_loca_maxp_num_glyphs as check
 
   test_font_path = TEST_FILE("nunito/Nunito-Regular.ttf")
 
