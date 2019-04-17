98445f2b9e141b2c8b1032960e888aac8771edcc
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-04-17 15:57:28 -0300

    montserrat_ttFonts fixture for the universal profile tests
    (#2468)

--- tests/profiles/universal_test.py
+++ tests/profiles/universal_test.py
@@ -20,6 +20,31 @@ from fontbakery.checkrunner import (
 check_statuses = (ERROR, FAIL, SKIP, PASS, WARN, INFO, DEBUG)
 
 
+@pytest.fixture
+def montserrat_ttFonts():
+  paths = [
+    TEST_FILE("montserrat/Montserrat-Black.ttf"),
+    TEST_FILE("montserrat/Montserrat-BlackItalic.ttf"),
+    TEST_FILE("montserrat/Montserrat-Bold.ttf"),
+    TEST_FILE("montserrat/Montserrat-BoldItalic.ttf"),
+    TEST_FILE("montserrat/Montserrat-ExtraBold.ttf"),
+    TEST_FILE("montserrat/Montserrat-ExtraBoldItalic.ttf"),
+    TEST_FILE("montserrat/Montserrat-ExtraLight.ttf"),
+    TEST_FILE("montserrat/Montserrat-ExtraLightItalic.ttf"),
+    TEST_FILE("montserrat/Montserrat-Italic.ttf"),
+    TEST_FILE("montserrat/Montserrat-Light.ttf"),
+    TEST_FILE("montserrat/Montserrat-LightItalic.ttf"),
+    TEST_FILE("montserrat/Montserrat-Medium.ttf"),
+    TEST_FILE("montserrat/Montserrat-MediumItalic.ttf"),
+    TEST_FILE("montserrat/Montserrat-Regular.ttf"),
+    TEST_FILE("montserrat/Montserrat-SemiBold.ttf"),
+    TEST_FILE("montserrat/Montserrat-SemiBoldItalic.ttf"),
+    TEST_FILE("montserrat/Montserrat-Thin.ttf"),
+    TEST_FILE("montserrat/Montserrat-ThinItalic.ttf")
+  ]
+  return [TTFont(path) for path in paths]
+
+
 def test_check_family_single_directory():
   """ Fonts are all in the same directory. """
   from fontbakery.profiles.universal import com_google_fonts_check_family_single_directory as check
