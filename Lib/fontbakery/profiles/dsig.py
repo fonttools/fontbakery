e45721492815dff4606260980db65d0a1f85a637
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-05-08 01:55:03 -0300

    add rationale to com.google.fonts/check/dsig

--- Lib/fontbakery/profiles/dsig.py
+++ Lib/fontbakery/profiles/dsig.py
@@ -4,7 +4,18 @@ from fontbakery.checkrunner import FAIL, PASS
 from fontbakery.fonts_profile import profile_factory # NOQA pylint: disable=unused-import
 
 @check(
-  id = 'com.google.fonts/check/dsig'
+  id = 'com.google.fonts/check/dsig',
+  rationale = """
+    Some programs expect fonts to have a digital signature
+    declared in their DSIG table in order to work properly.
+
+    This checks verifies that such signature is available in the font.
+
+    Typically, even a fake signature would be enough to make the
+    fonts work. If needed, such dummy-placeholder can be added to
+    the font by using the `gftools fix-dsig` script available at
+    https://github.com/googlefonts/gftools
+  """
 )
 def com_google_fonts_check_dsig(ttFont):
   """Does the font have a DSIG table?"""
