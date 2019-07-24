228f49f000ee9907cc4085ac23380d60e275ed63
Jérémie Hornus <45566589+JeremieHornus@users.noreply.github.com>
2019-07-24 12:27:22 +0200

    Create CJK_Check_OS2.py

---
+++ snippets/CJK_Check_OS2.py
@@ -0,0 +1,32 @@
+from fontTools import ttLib
+import sys
+
+unicodeRanges = {
+    48: 'CJK Symbols And Punctuation',
+    49: 'Hiragana',
+    50: 'Katakana',
+    51: 'Bopomofo',
+    52: 'Hangul Compatibility Jamo',
+    54: 'Enclosed CJK Letters And Months',
+    55: 'CJK Compatibility',
+    56: 'Hangul Syllables',
+    59: 'CJK Unified Ideographs',
+    61: 'CJK Strokes',
+    83: 'Yi Syllables'
+}
+
+def CheckOS2uRanges(font):
+    OS2 = font['OS/2']
+    range1 = bin(OS2.ulUnicodeRange1)[2:].zfill(32)
+    range2 = bin(OS2.ulUnicodeRange2)[2:].zfill(32)
+    range3 = bin(OS2.ulUnicodeRange3)[2:].zfill(32)
+    range4 = bin(OS2.ulUnicodeRange4)[2:].zfill(32)
+    allRanges = range1+range2+range3+range4
+    print("UnicodeRanges:", allRanges)
+    for k, v in unicodeRanges.items():
+        print(v, ':', int(allRanges[k])==1)
+
+if __name__ == '__main__':
+    if len(sys.argv):
+        inputFont = ttLib.TTFont(sys.argv[1])
+        CheckOS2uRanges(inputFont)
