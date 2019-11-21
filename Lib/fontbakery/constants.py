4a4804fa77ecb74c147fe676a1b75eb2886f0d2e
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2019-11-21 11:44:06 -0300

    Adding is_cjk_font condition originally contributed by @chrissimpkins
    at pull request #2594

--- Lib/fontbakery/constants.py
+++ Lib/fontbakery/constants.py
@@ -414,3 +414,55 @@ GF_latin_core = {
   # 0xEFFD: ("", "PRIVATE USE AREA U+EFFD"),
   # 0xF000: ("", "PRIVATE USE AREA U+F000"),
 }
+
+
+CJK_CODEPAGE_BITS = {
+  "JIS/Japan": 17,
+  "Chinese: Simplified chars—PRC and Singapore": 18,
+  "Korean Wansung": 19,
+  "Chinese: Traditional chars—Taiwan and Hong Kong": 20,
+  "Korean Johab": 21
+}
+
+
+CJK_UNICODE_RANGE_BITS = {
+  'Hangul Jamo': 28,
+  'CJK Symbols And Punctuation': 48,
+  'Hiragana': 49,
+  'Katakana': 50,
+  'Bopomofo': 51,
+  'Hangul Compatibility Jamo': 52,
+  'Enclosed CJK Letters And Months': 54,
+  'CJK Compatibility': 55,
+  'Hangul Syllables': 56,
+  'CJK Unified Ideographs': 59,
+  'CJK Strokes': 61,
+  'Yi Syllables': 83
+}
+
+
+CJK_UNICODE_RANGES = [
+  [0x1100, 0x11FF],  # Hangul Jamo
+  [0x3000, 0x303F],  # CJK Symbols and Punctuation
+  [0x3040, 0x309F],  # Hiragana
+  [0x30A0, 0x30FF],  # Katakana
+  [0x31F0, 0x31FF],  # Katakana Phonetic Extensions
+  [0x3100, 0x312F],  # Bopomofo
+  [0x31A0, 0x31BF],  # Bopomofo Extended (Bopomofo)
+  [0x3130, 0x318F],  # Hangul Compatibility Jamo
+  [0x3200, 0x32FF],  # Enclosed CJK Letters and Months
+  [0x3300, 0x33FF],  # CJK Compatibility
+  [0xAC00, 0xD7AF],  # Hangul Syllables
+  [0x4E00, 0x9FFF],  # CJK Unified Ideographs
+  [0x2E80, 0x2EFF],  # CJK Radicals Supplement (CJK Unified Ideographs)
+  [0x2F00, 0x2FDF],  # Kangxi Radicals (CJK Unified Ideographs)
+  [0x2FF0, 0x2FFF],  # Ideographic Description Characters (CJK Unified Ideographs)
+  [0x3400, 0x4DBF],  # CJK Unified Ideographs Extension A (CJK Unified Ideographs)
+  [0x20000, 0x2A6DF],  # CJK Unified Ideographs Extension B (CJK Unified Ideographs)
+  [0x3190, 0x319F],  # Kanbun (CJK Unified Ideographs)
+  [0x31C0, 0x31EF],  # CJK Strokes
+  [0xF900, 0xFAFF],  # CJK Compatibility Ideographs (CJK Strokes)
+  [0x2F800, 0x2FA1F],  # CJK Compatibility Ideographs Supplement (CJK Strokes)
+  [0xA000, 0xA48F],  # Yi Syllables
+  [0xA490, 0xA4CF],  # Yi Radicals
+]
