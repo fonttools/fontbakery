ae14cfc24a48252ca9173884884f051f4c34651c
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2020-03-06 21:24:24 -0300

    update version on docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,13 +21,13 @@ sys.path.insert(0, os.path.abspath("../Lib"))
 # -- Project information -----------------------------------------------------
 
 project = "Font Bakery"
-copyright = "2019 The Font Bakery Authors"
+copyright = "2020 The Font Bakery Authors"
 author = "The Font Bakery Authors"
 
 # The short X.Y version
 version = "0.7"
 # The full version, including alpha/beta/rc tags
-release = "0.7.20"
+release = "0.7.21"
 
 
 # -- General configuration ---------------------------------------------------
@@ -190,7 +190,7 @@ def linkcode_resolve(domain, info):
     # AND We can link to a tag i.e. a release tag. This is awesome:
     # tag is: "v0.7.2"
     # https://github.com/googlefonts/fontbakery/tree/v0.7.2/Lib/fontbakery/profiles
-    tree = 'v0.7.20'
+    tree = 'v0.7.21'
     # It's not planned for us to get the line number :-(
     # I had to hammer this data into the info.
     if 'lineno' in info:
