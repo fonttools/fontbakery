6f515a9a22ccb5e0738714f450dfec2a0c99a437
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2020-05-14 16:31:09 -0300

    New check: com.google.fonts/check/STAT_strings
    Check correctness of name table strings referenced by STAT table
    (issue #2863)

--- Lib/fontbakery/profiles/universal.py
+++ Lib/fontbakery/profiles/universal.py
@@ -42,6 +42,7 @@ UNIVERSAL_PROFILE_CHECKS = \
   'com.google.fonts/check/unique_glyphnames',
 #  'com.google.fonts/check/glyphnames_max_length',
   'com.google.fonts/check/family/vertical_metrics',
+  'com.google.fonts/check/STAT_strings'
 ]
 
 @check(
@@ -543,6 +544,51 @@ def com_google_fonts_check_unwanted_tables(ttFont):
                  ).format("\n".join(unwanted_tables_found))
   else:
     yield PASS, "There are no unwanted tables."
+
+
+@condition
+def STAT_table(ttFont):
+  return "STAT" in ttFont
+
+
+@check(
+  id = 'com.google.fonts/check/STAT_strings',
+  conditions = ["STAT_table"],
+  rationale = """
+    On the STAT table, the "Italic" keyword must not be used on AxisValues for variation axes other than 'ital'.
+  """,
+  misc_metadata = {
+    'requested': "https://github.com/googlefonts/fontbakery/issues/2863"
+  }
+)
+def com_google_fonts_check_STAT_strings(ttFont):
+  """ Check correctness of STAT table strings """
+  passed = True
+  ital_axis_index = None
+  for index, axis in enumerate(ttFont["STAT"].table.DesignAxisRecord.Axis):
+    if axis.AxisTag == 'ital':
+      ital_axis_index = index
+      break
+
+  nameIDs = []
+  for value in ttFont["STAT"].table.AxisValueArray.AxisValue:
+    if value.AxisIndex != ital_axis_index: nameIDs.append(value.ValueNameID)
+
+  bad_values = []
+  for name in ttFont['name'].names:
+    if name.nameID in nameIDs and "italic" in name.toUnicode().lower():
+      passed = False
+      bad_values.append(name.toUnicode())
+
+  if bad_values:
+    yield FAIL,\
+          Message("bad-italic",
+                  f'The following AxisValue entries on the STAT table'
+                  f' should not contain "Italic":\n'
+                  f' {bad_values}')
+
+  if passed:
+    yield PASS, "Looks good!"
 
 
 @check(
