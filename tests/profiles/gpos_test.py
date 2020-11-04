115ce34aed19562c7f36a8faa5a00e8c788654ea
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2020-11-03 22:30:08 -0300

    code-style tweaks

--- tests/profiles/gpos_test.py
+++ tests/profiles/gpos_test.py
@@ -56,15 +56,11 @@ def test_check_gpos_kerning_info():
 
 
 def test_check_gpos_kerning_info_monospaced_font():
-    """Does GPOS table have kerning information in
-    a monospaced font?
-    """
-    check = CheckTester(opentype_profile, "com.google.fonts/check/gpos_kerning_info")
-    # Our reference Overpass Mono lacks kerning info
-    # but it is a monospaced font and this is expected
-    ttFont = TTFont(TEST_FILE("overpassmono/OverpassMono-Regular-post-edit.subset.ttf"))
-
-    # So it must PASS the check:
-    assert_PASS(
-        check(ttFont), "with a font that is a monospaced font without kerning info..."
-    )
+    """ Does GPOS table have kerning information in a monospaced font? """
+    check = CheckTester(opentype_profile,
+                        "com.google.fonts/check/gpos_kerning_info")
+    # Even though our reference Overpass Mono lacks kerning info
+    # it is a monospaced font and this is expected:
+    font = TEST_FILE("overpassmono/OverpassMono-Regular-post-edit.subset.ttf")
+    assert_PASS(check(font),
+                "with a font that is a monospaced font without kerning info...")
