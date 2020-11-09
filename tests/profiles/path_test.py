00afd64957f17d322862c08b316add8944b2f4d6
Simon Cozens <simon@simon-cozens.org>
2020-11-09 14:17:49 +0000

    Don't use TTFont, fix bad test

--- tests/profiles/path_test.py
+++ tests/profiles/path_test.py
@@ -1,7 +1,5 @@
 import io
 
-from fontTools.ttLib import TTFont
-
 from fontbakery.checkrunner import WARN
 from fontbakery.codetesting import (assert_results_contain,
                                     CheckTester,
@@ -15,41 +13,56 @@ def test_check_path_alignment_miss():
     check = CheckTester(path_profile,
                         "com.google.fonts/check/path_alignment_miss")
 
-    ttFont = TTFont(TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf"))
-    results = check(ttFont)
+    filename = TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf")
+    results = check(filename)
     assert_results_contain(results,
                            WARN, 'found-misalignments')
     messages = "".join([m[1].message for m in results])
     assert "A: X=3.0,Y=-2.0 (should be at baseline 0?)" in messages
+
+def test_check_path_short_segments():
+    """ Check for short segments. """
+    check = CheckTester(path_profile,
+                        "com.google.fonts/check/path_short_segments")
+
+    filename = TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf")
+    results = check(filename)
+    assert_results_contain(results,
+                           WARN, 'found-short-segments')
+    messages = "".join([m[1].message for m in results])
+    assert "* E" in messages
+
 
 def test_check_path_colinear_vectors():
     """ Check for colinear line segments. """
     check = CheckTester(path_profile,
                         "com.google.fonts/check/path_colinear_vectors")
 
-    ttFont = TTFont(TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf"))
-    results = check(ttFont)
+    filename = TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf")
+    results = check(filename)
     assert_results_contain(results,
                            WARN, 'found-colinear-vectors')
     messages = "".join([m[1].message for m in results])
-    assert "* A" not in messages
-    assert "* B" not in messages
-    assert "* C" in messages
+    assert "A" not in messages
+    assert "B" not in messages
+    assert "C" in messages
+    assert "E" in messages
+    assert ".notdef" in messages
 
 def test_check_path_jaggy_segments():
     """ Check for jaggy segments. """
     check = CheckTester(path_profile,
                         "com.google.fonts/check/path_jaggy_segments")
 
-    ttFont = TTFont(TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf"))
-    results = check(ttFont)
+    filename = TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf")
+    results = check(filename)
     assert_results_contain(results,
                            WARN, 'found-jaggy-segments')
     messages = "".join([m[1].message for m in results])
     assert "* E" in messages
 
-    ttFont = TTFont(TEST_FILE("familysans/FamilySans-Regular.ttf"))
-    assert_PASS(check(ttFont))
+    filename = TEST_FILE("familysans/FamilySans-Regular.ttf")
+    assert_PASS(check(filename))
 
 
 def test_check_path_semi_vertical():
@@ -57,8 +70,8 @@ def test_check_path_semi_vertical():
     check = CheckTester(path_profile,
                         "com.google.fonts/check/path_semi_vertical")
 
-    ttFont = TTFont(TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf"))
-    results = check(ttFont)
+    filename = TEST_FILE("wonky_paths/WonkySourceSansPro-Regular.otf")
+    results = check(filename)
     assert_results_contain(results,
                            WARN, 'found-semi-vertical')
     messages = "".join([m[1].message for m in results])
