2d68420d9e3c1ad3060f980060120de2969d8afd
Aurélio A. Heckert <aurium@gmail.com>
2021-03-10 17:26:50 -0300

    Test status log formating.

--- tests/commands/test_usage.py
+++ tests/commands/test_usage.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-
+import re
 import pytest
 
 
@@ -33,6 +33,33 @@ def test_command_check_googlefonts():
 
     with pytest.raises(subprocess.CalledProcessError):
         subprocess.check_output(["fontbakery", "check-googlefonts"])
+
+
+def test_status_log_is_indented():
+    """Test if statuses are printed in a limited boundary."""
+    test_font = os.path.join("data", "test", "nunito", "Nunito-Regular.ttf")
+
+    result = subprocess.run(["fontbakery", "check-googlefonts",
+        "-c", "old_ttfautohint", "-c", "font_copyright",
+        test_font], capture_output=True)
+
+    p = re.compile('([^][a-zA-Z0-9@:,.()\'"\n ])', re.I | re.M)
+    stdout = p.sub('#', result.stdout.decode()).split('\n')
+    assert '\n'.join(stdout[24:30]) == '\n'.join([
+    '     #[0#31#40mFAIL#[0m Name Table entry: Copyright notices should match a      ',
+    '          pattern similar to: "Copyright 2019 The Familyname Project Authors    ',
+    '          (git url)"                                                            ',
+    '          But instead we have got:                                              ',
+    '          "Copyright 2014 The Nunito Project Authors (contact@sansoxygen.com)"  ',
+    '          [code: bad#notice#format]                                             '
+    ])
+    assert '\n'.join(stdout[10:15]) == '\n'.join([
+    '     #[0#36#40mINFO#[0m Could not detect which version of ttfautohint was used  ',
+    '          in this font. It is typically specified as a comment in the font      ',
+    "          version entries of the 'name' table. Such font version strings are    ",
+    "          currently: ['Version 3.000', 'Version 3.000'] [code:                  ",
+    '          version#not#detected]                                                 '
+    ])
 
 
 def test_command_check_profile():
