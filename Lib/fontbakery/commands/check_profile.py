d1ae30a46f55ec53358bdd16e777d11bea7fac41
Simon Cozens <simon@simon-cozens.org>
2021-03-18 22:32:42 +0000

    I literally copied this from the Python 3 docs but apparently it’s not Python 3 enough

--- Lib/fontbakery/commands/check_profile.py
+++ Lib/fontbakery/commands/check_profile.py
@@ -51,7 +51,7 @@ class AddReporterAction(argparse.Action):
      def __init__(self, option_strings, dest, nargs=None, **kwargs):
         self.cls = kwargs["cls"]
         del kwargs["cls"]
-        super(AddReporterAction, self).__init__(option_strings, dest, **kwargs)
+        super().__init__(option_strings, dest, **kwargs)
 
      def __call__(self, parser, namespace, values, option_string=None):
         if not hasattr(namespace, "reporters"):
