dc9aa74102bd943311d762c1e499b286b5f42053
Simon Cozens <simon@simon-cozens.org>
2021-03-19 10:13:44 +0000

    Reformat with black

--- Lib/fontbakery/errors.py
+++ Lib/fontbakery/errors.py
@@ -24,19 +24,22 @@ class ProtocolViolationError(FontBakeryRunnerError):
 
 class FailedCheckError(FontBakeryRunnerError):
     def __init__(self, error, *args):
-        message = f'Failed with {type(error).__name__}: {error}'
+        message = f"Failed with {type(error).__name__}: {error}"
         self.error = error
         self.traceback = "".join(traceback.format_tb(error.__traceback__))
         super().__init__(message, *args)
 
 
 class FailedConditionError(FontBakeryRunnerError):
-    """ This is a serious problem with the check suite profile
+    """This is a serious problem with the check suite profile
     and it must be solved.
     """
+
     def __init__(self, condition, error, *args):
-        message = (f'The condition {condition} had an error:'
-                   f' {type(error).__name__}: {error}')
+        message = (
+            f"The condition {condition} had an error:"
+            f" {type(error).__name__}: {error}"
+        )
         self.condition = condition
         self.error = error
         self.traceback = "".join(traceback.format_tb(error.__traceback__))
@@ -44,12 +47,15 @@ class FailedConditionError(FontBakeryRunnerError):
 
 
 class MissingConditionError(FontBakeryRunnerError):
-    """ This is a serious problem with the check suite profile
+    """This is a serious problem with the check suite profile
     and it must be solved, most probably a typo.
     """
+
     def __init__(self, condition_name, error, *args):
-        message = (f'The condition named {condition_name} is missing:'
-                   f' {type(error).__name__}: {error}')
+        message = (
+            f"The condition named {condition_name} is missing:"
+            f" {type(error).__name__}: {error}"
+        )
         self.error = error
         self.traceback = "".join(traceback.format_tb(error.__traceback__))
         super().__init__(message, *args)
@@ -57,8 +63,7 @@ class MissingConditionError(FontBakeryRunnerError):
 
 class FailedDependenciesError(FontBakeryRunnerError):
     def __init__(self, check, error, *args):
-        message = (f'The check {check} had an error:'
-                   f' {type(error).__name__}: {error}')
+        message = f"The check {check} had an error:" f" {type(error).__name__}: {error}"
         self.check = check
         self.error = error
         self.traceback = "".join(traceback.format_tb(error.__traceback__))
