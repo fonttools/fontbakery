f885afc0a6dfc37986722e9d731f850f68d3ebbc
Marc Foley <m.foley.88@gmail.com>
2021-03-23 11:42:16 +0000

    check/mandatory_avar_table: update description
    I've mentioned that an avar isn't necessary if the progressions are linear and added hints to fontmake and the need to visually proof.

--- Lib/fontbakery/profiles/googlefonts.py
+++ Lib/fontbakery/profiles/googlefonts.py
@@ -5121,9 +5121,11 @@ def com_google_fonts_check_metadata_designer_profiles(family_metadata):
 @check(
     id = 'com.google.fonts/check/mandatory_avar_table',
     rationale = """
-        All high quality variable fonts include an avar table to correctly define axes progression rates.
+        Most variable fonts should include an avar table to correctly define axes progression rates.
 
         For example, a weight axis from 0% to 100% doesn't map directly to 100 to 1000, because a 10% progression from 0% may be too much to define the 200, while 90% may be too little to define the 900.
+        
+        If the progression rates of axes is linear, this check can be ignored. Fontmake will also skip adding an avar table if the progression rates are linear. However, we still recommend designers visually proof each instance is at the desired weight, width etc.
     """,
     conditions = ["is_variable_font"],
     misc_metadata = {
