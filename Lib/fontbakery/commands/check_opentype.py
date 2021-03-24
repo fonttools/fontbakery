ef039506ab3df24231611116f2a5d6fba89d08c4
Simon Cozens <simon@simon-cozens.org>
2021-03-24 11:18:42 +0000

    Just use the profile directly
    Now all the check_... things are parallel.

--- Lib/fontbakery/commands/check_opentype.py
+++ Lib/fontbakery/commands/check_opentype.py
@@ -2,12 +2,10 @@
 import sys
 from functools import partial
 
-import fontbakery.profiles.opentype
-from fontbakery.commands.check_profile import (main as super_main,
-                                               get_module_profile)
+from fontbakery.profiles.opentype import profile
+from fontbakery.commands.check_profile import main as super_main
 
-main = partial(super_main,
-               get_module_profile(fontbakery.profiles.opentype))
+main = partial(super_main, profile)
 
 if __name__ == '__main__':
     sys.exit(main())
