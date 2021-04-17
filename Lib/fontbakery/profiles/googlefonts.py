e0a77256c62bebe33fad24045889cdeb2fc1fdac
Denis Moyogo Jacquerye <moyogo@gmail.com>
2021-04-17 11:36:17 +0100

    whitespace

--- Lib/fontbakery/profiles/googlefonts.py
+++ Lib/fontbakery/profiles/googlefonts.py
@@ -5130,7 +5130,7 @@ def com_google_fonts_check_metadata_designer_profiles(family_metadata):
         Most variable fonts should include an avar table to correctly define axes progression rates.
 
         For example, a weight axis from 0% to 100% doesn't map directly to 100 to 1000, because a 10% progression from 0% may be too much to define the 200, while 90% may be too little to define the 900.
-        
+
         If the progression rates of axes is linear, this check can be ignored. Fontmake will also skip adding an avar table if the progression rates are linear. However, we still recommend designers visually proof each instance is at the desired weight, width etc.
     """,
     conditions = ["is_variable_font"],
