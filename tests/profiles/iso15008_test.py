730202f6ae8fcedfcb7ff736faaf724ab9dcacfd
Simon Cozens <simon@simon-cozens.org>
2021-04-28 15:19:51 +0100

    Intercharacter spacing check (Fixes #3252)

--- tests/profiles/iso15008_test.py
+++ tests/profiles/iso15008_test.py
@@ -36,6 +36,7 @@ def test_check_iso15008_proportions():
         "The proportion of H width to H height",
     )
 
+
 def test_check_iso15008_stem_width():
     """Check if 0.10 <= (stem width / ascender) <= 0.82"""
     check = CheckTester(iso15008, "com.google.fonts/check/iso15008_stem_width")
@@ -59,4 +60,41 @@ def test_check_iso15008_stem_width():
         FAIL,
         "invalid-proportion",
         "The proportion of stem width to ascender",
+    )
+
+
+def test_check_iso15008_intercharacter_spacing():
+    """Check if spacing between characters is adequate for display use"""
+    check = CheckTester(
+        iso15008, "com.google.fonts/check/iso15008_intercharacter_spacing"
+    )
+
+    ttFont = TTFont(TEST_FILE("cabin/Cabin-Regular.ttf"))
+    assert_PASS(check(ttFont), "with a good font...")
+
+    ttFont = TTFont(TEST_FILE("cabin/Cabin-SemiBold.ttf"))
+    # l stem width is 111, LSB at x-height is 59, RSB at x-Height is 83
+    # 142 / 111 = 128%, so this font is too tight.
+    assert_results_contain(
+        check(ttFont),
+        FAIL,
+        "bad-vertical-vertical-spacing",
+        "The space between vertical strokes",
+    )
+    # v LSB is 5, lv kern is -6 (!) so lv distance is 83+5-6 = 82
+    # 82 / 111 = 0.73%, so that fails too.
+    assert_results_contain(
+        check(ttFont),
+        FAIL,
+        "bad-vertical-diagonal-spacing",
+        "The space between vertical and diagonal strokes",
+    )
+
+    ttFont = TTFont(TEST_FILE("montserrat/Montserrat-Black.ttf"))
+    # vv touches
+    assert_results_contain(
+        check(ttFont),
+        FAIL,
+        "bad-diagonal-diagonal-spacing",
+        "Diagonal strokes (vv) were touching",
     )
