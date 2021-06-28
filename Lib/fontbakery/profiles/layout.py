d169dbcddf1e7a785c08873fa8db904ab2f0dcc8
Simon Cozens <simon@simon-cozens.org>
2021-06-28 16:12:15 +0100

    Add layout checks to opentype profile

--- Lib/fontbakery/profiles/layout.py
+++ Lib/fontbakery/profiles/layout.py
@@ -1,6 +1,9 @@
 from fontbakery.callable import check
 from fontbakery.status import PASS, FAIL
 from fontbakery.message import Message
+from fontbakery.fonts_profile import (
+    profile_factory,
+)  # NOQA pylint: disable=unused-import
 
 
 from opentypespec.tags import FEATURE_TAGS, SCRIPT_TAGS, LANGUAGE_TAGS
