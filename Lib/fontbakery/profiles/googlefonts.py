11ac4b3b8cd0105bdeda7be1a4a4919b93da7acf
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2021-07-14 12:14:34 -0300

    fix code-style

--- Lib/fontbakery/profiles/googlefonts.py
+++ Lib/fontbakery/profiles/googlefonts.py
@@ -5501,26 +5501,24 @@ def com_google_fonts_check_meta_script_lang_tags(ttFont):
 
 
 @check(
-    id="com.google.fonts/check/no_debugging_tables",
-    rationale="""
-        Tables such as `Debg` are useful in the pre-production stages of font
-        development, but add unnecessary bloat to a production font and should
-        be removed before release.
+    id = "com.google.fonts/check/no_debugging_tables",
+    rationale = """
+        Tables such as `Debg` are useful in the pre-production stages of font development, but add unnecessary bloat to a production font and should be removed before release.
     """,
-    severity=6,
-    proposal="https://github.com/googlefonts/fontbakery/issues/3357",
+    severity = 6,
+    proposal = 'https://github.com/googlefonts/fontbakery/issues/3357',
 )
 def com_google_fonts_check_no_debugging_tables(ttFont):
-    """Ensure fonts do not contain any preproduction tables."""
+    """Ensure fonts do not contain any pre-production tables."""
 
     DEBUGGING_TABLES = ["Debg", "FFTM"]
     found = [t for t in DEBUGGING_TABLES if t in ttFont]
     if found:
-        yield WARN, Message(
-            "has-debugging-tables",
-            "This font file contains the following pre-production tables:"
-            + ", ".join(found),
-        )
+        tables_list = ", ".join(found)
+        yield WARN,\
+              Message("has-debugging-tables",
+                      f"This font file contains the following"
+                      f" pre-production tables: {tables_list}")
     else:
         yield PASS, "OK"
 
