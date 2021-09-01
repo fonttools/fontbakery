ef65e15bc20e9e2830f372dcf77bceb9933b6a88
Simon Cozens <simon@simon-cozens.org>
2021-09-01 19:25:05 +0100

    Use CheckTester everywhere in ufo sources profile

--- Lib/fontbakery/codetesting.py
+++ Lib/fontbakery/codetesting.py
@@ -93,14 +93,15 @@ class CheckTester:
                       'fonts': [values.reader.file.name],
                       'ttFont': values,
                       'ttFonts': [values]}
-        elif isinstance(values, list) and values:
-            if isinstance(values[0], str):
-                values = {'fonts': values}
-            elif isinstance(values[0], TTFont):
-                values = {'fonts': [v.reader.file.name for v in values],
-                          'ttFonts': values}
-        else:
-            values = {}
+        elif isinstance(values, list):
+            if values:
+                if isinstance(values[0], str):
+                    values = {'fonts': values}
+                elif isinstance(values[0], TTFont):
+                    values = {'fonts': [v.reader.file.name for v in values],
+                              'ttFonts': values}
+            else:
+                values = {}
 
         self.runner = CheckRunner(self.profile, values, Configuration(explicit_checks=[self.check_id]))
         for check_identity in self.runner.order:
--- tests/profiles/ufo_sources_test.py
+++ tests/profiles/ufo_sources_test.py
@@ -5,7 +5,9 @@ import pytest
 
 from fontbakery.checkrunner import (DEBUG, ERROR, FAIL, INFO, PASS, SKIP, WARN)
 from fontbakery.codetesting import (assert_PASS,
-                                    assert_results_contain)
+                                    assert_results_contain,
+                                    CheckTester)
+from fontbakery.profiles.ufo_sources import profile as ufo_sources_profile
 
 check_statuses = (ERROR, FAIL, SKIP, PASS, WARN, INFO, DEBUG)
 
@@ -19,23 +21,24 @@ def empty_ufo_font(tmpdir):
 
 
 def test_check_ufolint(empty_ufo_font):
-    from fontbakery.profiles.ufo_sources import com_daltonmaag_check_ufolint as check
+    check = CheckTester(ufo_sources_profile, "com.daltonmaag/check/ufolint")
+
     _, ufo_path = empty_ufo_font
 
-    assert_PASS(check(ufo_path),
+    assert_PASS(check({"ufo": ufo_path}),
                 'with an empty UFO.')
 
     os.remove(os.path.join(ufo_path, "metainfo.plist"))
-    assert_results_contain(check(ufo_path),
+    assert_results_contain(check({"ufo": ufo_path}),
                            FAIL, "ufolint-fail",
                            'with maimed UFO.')
 
 
 def test_check_required_fields(empty_ufo_font):
-    from fontbakery.profiles.ufo_sources import com_daltonmaag_check_required_fields as check
+    check = CheckTester(ufo_sources_profile, "com.daltonmaag/check/ufo-required-fields")
     ufo, _ = empty_ufo_font
 
-    assert_results_contain(check(ufo),
+    assert_results_contain(check({}, {"ufo_font": ufo}),
                            FAIL, "missing-required-fields",
                            'with an empty UFO.')
 
@@ -46,16 +49,15 @@ def test_check_required_fields(empty_ufo_font):
     ufo.info.capHeight = 700
     ufo.info.familyName = "Test"
 
-    assert_PASS(check(ufo),
+    assert_PASS(check({}, {"ufo_font": ufo}),
                 'with an almost empty UFO.')
 
 
 def test_check_recommended_fields(empty_ufo_font):
-    from fontbakery.profiles.ufo_sources import (
-        com_daltonmaag_check_recommended_fields as check)
+    check = CheckTester(ufo_sources_profile, "com.daltonmaag/check/ufo-recommended-fields")
     ufo, _ = empty_ufo_font
 
-    assert_results_contain(check(ufo),
+    assert_results_contain(check({}, {"ufo_font": ufo}),
                            WARN, "missing-recommended-fields",
                            'with an empty UFO.')
 
@@ -67,15 +69,15 @@ def test_check_recommended_fields(empty_ufo_font):
     ufo.info.copyright = "Test"
     ufo.info.openTypeOS2Panose = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-    assert_PASS(check(ufo),
+    assert_PASS(check({}, {"ufo_font": ufo}),
                 'with an almost empty UFO.')
 
 
 def test_check_unnecessary_fields(empty_ufo_font):
-    from fontbakery.profiles.ufo_sources import com_daltonmaag_check_unnecessary_fields as check
+    check = CheckTester(ufo_sources_profile, "com.daltonmaag/check/ufo-unnecessary-fields")
     ufo, _ = empty_ufo_font
 
-    assert_PASS(check(ufo),
+    assert_PASS(check({}, {"ufo_font": ufo}),
                 'with an empty UFO.')
 
     ufo.info.openTypeNameUniqueID = "aaa"
@@ -83,7 +85,7 @@ def test_check_unnecessary_fields(empty_ufo_font):
     ufo.info.postscriptUniqueID = -1
     ufo.info.year = 2018
 
-    assert_results_contain(check(ufo),
+    assert_results_contain(check({}, {"ufo_font": ufo}),
                            WARN, "unnecessary-fields",
                            'with unnecessary fields in UFO.')
 
