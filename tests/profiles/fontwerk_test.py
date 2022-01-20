796bfb50926c0690592bae772c6417e35745ccfd
Olli Meier <hallo@olli-meier.de>
2022-01-20 11:59:04 +0100

    New Fontwerk profile <https://fontwerk.com/>
    And an initial check:
    com.fontwerk/check/no_mac_entries
    "Check if font has Mac name table entries (platform=1)"
    (PR #3545)

---
+++ tests/profiles/fontwerk_test.py
@@ -0,0 +1,22 @@
+from fontbakery.message import Message
+from fontbakery.checkrunner import FAIL
+from fontbakery.codetesting import (assert_PASS,
+                                    assert_results_contain,
+                                    CheckTester,
+                                    TEST_FILE)
+from fontbakery.profiles import fontwerk as fontwerk_profile
+
+
+def test_check_name_no_mac_entries():
+    check = CheckTester(fontwerk_profile,
+                        'com.fontwerk/check/no_mac_entries')
+
+    font = TEST_FILE('abeezee/ABeeZee-Italic.ttf')
+    assert_results_contain(check(font),
+                           FAIL, 'mac-names',
+                           'with a font containing Mac names')
+
+    font = TEST_FILE('source-sans-pro/OTF/SourceSansPro-Regular.otf')
+    assert_PASS(check(font),
+                'with a font without Mac names')
+
