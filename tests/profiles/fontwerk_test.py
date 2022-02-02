cbde30694f0153e7159d608bb477a9fd286fd8fa
Olli Meier <hallo@olli-meier.de>
2022-02-02 23:35:02 +0100

    Fix unittest issues.

--- tests/profiles/fontwerk_test.py
+++ tests/profiles/fontwerk_test.py
@@ -1,4 +1,4 @@
-from fontbakery.message import Message
+from fontTools.ttLib import TTFont
 from fontbakery.checkrunner import FAIL
 from fontbakery.codetesting import (assert_PASS,
                                     assert_results_contain,
@@ -26,12 +26,13 @@ def test_check_vendor_id():
                         'com.fontwerk/check/vendor_id')
 
     font = TEST_FILE('abeezee/ABeeZee-Italic.ttf')
-    assert_results_contain(check(font),
+    ttFont = TTFont(font)
+    assert_results_contain(check(ttFont),
                            FAIL, 'bad-vendor-id',
                            ", but should be 'WERK'.")
 
-    font['OS/2'].achVendID = 'WERK'
-    assert_PASS(check(font),
+    ttFont['OS/2'].achVendID = 'WERK'
+    assert_PASS(check(ttFont),
                 "'WERK' is correct.")
 
 
@@ -39,10 +40,12 @@ def test_check_weight_class_fvar():
     check = CheckTester(fontwerk_profile,
                         'com.fontwerk/check/weight_class_fvar')
 
-    font = TEST_FILE('varfont/OpenSans[wdth, wght].ttf')
-    assert_PASS(check(font), "matches fvar default value.")
+    font = TEST_FILE('varfont/Oswald-VF.ttf')
+    ttFont = TTFont(font)
+    assert_PASS(check(ttFont),
+                "matches fvar default value.")
 
-    font['OS/2'].usWeightClass = 333
-    assert_results_contain(check(font),
+    ttFont['OS/2'].usWeightClass = 333
+    assert_results_contain(check(ttFont),
                            FAIL, 'bad-weight-class',
-                           "'but should match fvar default value'.")
+                           "but should match fvar default value.")
