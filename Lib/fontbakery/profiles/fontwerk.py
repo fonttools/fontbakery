98e78e0c6f4a0a932b8b127e2976f0c10bb23d57
Olli Meier <moontypespace@users.noreply.github.com>
2022-02-04 00:34:16 +0100

    Fontwerk further adjustments
    Updates to the `fontwerk` profile:
    * include most `googlefonts` profile checks.
    * new check: **com.fontwerk/check/vendor_id**: "Vendor ID must for 'WERK' on FontWerk fonts."
    * new check: **com.fontwerk/check/weight_class_fvar**: "usWeightclass must match fvar default value."
    (PR #3579)

--- Lib/fontbakery/profiles/fontwerk.py
+++ Lib/fontbakery/profiles/fontwerk.py
@@ -7,16 +7,30 @@ from fontbakery.section import Section
 from fontbakery.status import PASS, FAIL
 from fontbakery.fonts_profile import profile_factory
 from fontbakery.message import Message
-from fontbakery.profiles.universal import UNIVERSAL_PROFILE_CHECKS
+from fontbakery.profiles.googlefonts import GOOGLEFONTS_PROFILE_CHECKS
 
-profile_imports = ('fontbakery.profiles.universal',)
+profile_imports = ('fontbakery.profiles.googlefonts',)
 profile = profile_factory(default_section=Section("Fontwerk"))
 
-FONTWERK_PROFILE_CHECKS = \
-    UNIVERSAL_PROFILE_CHECKS + [
-        'com.fontwerk/check/no_mac_entries'
-    ]
+CHECKS_DONT_DO = [
+    # don't do the following checks
+    'com.google.fonts/check/canonical_filename',
+    'com.google.fonts/check/vendor_id',
+    'com.google.fonts/check/fstype',
+    'com.google.fonts/check/gasp',
 
+    # Skip the following checks,
+    # because they may need some improvements first.
+    'com.google.fonts/check/family/italics_have_roman_counterparts',
+]
+
+FONTWERK_PROFILE_CHECKS = \
+    [check for check in GOOGLEFONTS_PROFILE_CHECKS
+     if check not in CHECKS_DONT_DO] + [
+        'com.fontwerk/check/no_mac_entries',
+        'com.fontwerk/check/vendor_id',
+        'com.fontwerk/check/weight_class_fvar',
+    ]
 
 @check(
     id = 'com.fontwerk/check/no_mac_entries',
@@ -25,7 +39,7 @@ FONTWERK_PROFILE_CHECKS = \
         Even Apple stopped producing name tables with platform 1.
         Please see for example the following system font:
         /System/Library/Fonts/SFCompact.ttf
-        
+
         Also, Dave Opstad, who developed Apple's TrueType specifications, told Olli Meier a couple years ago (as of January/2022) that these entries are outdated and should not be produced anymore.
     """,
     proposal = 'https://github.com/googlefonts/gftools/issues/469'
@@ -45,5 +59,53 @@ def com_fontwerk_check_name_no_mac_entries(ttFont):
         yield PASS, 'No Mac name table entries.'
 
 
-profile.auto_register(globals())
+@check(
+    id = 'com.fontwerk/check/vendor_id',
+    rationale = """
+        Vendor ID must be WERK for Fontwerk fonts.
+    """,
+)
+def com_fontwerk_check_vendor_id(ttFont):
+    """Checking OS/2 achVendID."""
+
+    vendor_id = ttFont['OS/2'].achVendID
+    if vendor_id != 'WERK':
+        yield FAIL,\
+              Message("bad-vendor-id",
+                      f"OS/2 VendorID is '{vendor_id}', but should be 'WERK'.")
+    else:
+        yield PASS, f"OS/2 VendorID '{vendor_id}' is correct."
+
+@check(
+    id = 'com.fontwerk/check/weight_class_fvar',
+    rationale = """
+        According to Microsoft's OT Spec the OS/2 usWeightClass should match the fvar default value.
+    """,
+    conditions=["is_variable_font"],
+    proposal='https://github.com/googlefonts/gftools/issues/477'
+)
+def com_fontwerk_check_weight_class_fvar(ttFont):
+    """Checking if OS/2 usWeightClass matches fvar."""
+
+    fvar = ttFont['fvar']
+    default_axis_values = {a.axisTag: a.defaultValue for a in fvar.axes}
+
+    fvar_value = default_axis_values.get('wght', None)
+    os2_value = ttFont["OS/2"].usWeightClass
+
+    if fvar_value is None:
+        return
+
+    if os2_value != int(fvar_value):
+        yield FAIL, \
+              Message("bad-weight-class",
+                      f"OS/2 usWeightClass is '{os2_value}', "
+                      f"but should match fvar default value '{fvar_value}'.")
+
+    else:
+        yield PASS, f"OS/2 usWeightClass '{os2_value}' matches fvar default value."
+
+profile.auto_register(globals(),
+                      filter_func=lambda type, id, _:
+                      not (type == 'check' and id in CHECKS_DONT_DO))
 profile.test_expected_checks(FONTWERK_PROFILE_CHECKS, exclusive=True)
--- tests/profiles/fontwerk_test.py
+++ tests/profiles/fontwerk_test.py
@@ -1,4 +1,4 @@
-from fontbakery.message import Message
+from fontTools.ttLib import TTFont
 from fontbakery.checkrunner import FAIL
 from fontbakery.codetesting import (assert_PASS,
                                     assert_results_contain,
@@ -20,3 +20,32 @@ def test_check_name_no_mac_entries():
     assert_PASS(check(font),
                 'with a font without Mac names')
 
+
+def test_check_vendor_id():
+    check = CheckTester(fontwerk_profile,
+                        'com.fontwerk/check/vendor_id')
+
+    font = TEST_FILE('abeezee/ABeeZee-Italic.ttf')
+    ttFont = TTFont(font)
+    assert_results_contain(check(ttFont),
+                           FAIL, 'bad-vendor-id',
+                           ", but should be 'WERK'.")
+
+    ttFont['OS/2'].achVendID = 'WERK'
+    assert_PASS(check(ttFont),
+                "'WERK' is correct.")
+
+
+def test_check_weight_class_fvar():
+    check = CheckTester(fontwerk_profile,
+                        'com.fontwerk/check/weight_class_fvar')
+
+    font = TEST_FILE('varfont/Oswald-VF.ttf')
+    ttFont = TTFont(font)
+    assert_PASS(check(ttFont),
+                "matches fvar default value.")
+
+    ttFont['OS/2'].usWeightClass = 333
+    assert_results_contain(check(ttFont),
+                           FAIL, 'bad-weight-class',
+                           "but should match fvar default value.")
