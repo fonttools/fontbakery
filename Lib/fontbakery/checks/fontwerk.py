3f888f63d163c1cf8d5f535e48586e5ce192d486
Olli Meier <hallo@olli-meier.de>
2022-02-10 12:07:00 +0100

    new check: com.fontwerk/check/names_match_default_fvar
    "Checking if names match default fvar."
    
    EXPERIMENTAL - com.fontwerk/check/names_match_default_fvar
    Added to the FontWerk profile
    
    (PR #3604)

--- Lib/fontbakery/checks/fontwerk.py
+++ Lib/fontbakery/checks/fontwerk.py
@@ -57,6 +57,91 @@ def com_fontwerk_check_vendor_id(ttFont):
 
 
 @check(
+    id="com.fontwerk/check/names_match_default_fvar",
+    rationale="""
+        Check if the font names match default fvar instance name.
+    """,
+    conditions=["is_variable_font"],
+    proposal="https://github.com/fonttools/fontbakery/pull/3698",
+    experimental="Since 2024/Feb/08",
+)
+def com_fontwerk_check_names_match_default_fvar(ttFont):
+    """Checking if names match default fvar instance name."""
+    from fontbakery.constants import NameID
+
+    passed = True
+    default_axis_values = {
+        axis.axisTag: axis.defaultValue for axis in ttFont["fvar"].axes
+    }
+    default_name_id = None
+    for instance in ttFont["fvar"].instances:
+        if instance.coordinates == default_axis_values:
+            default_name_id = instance.subfamilyNameID
+            break
+
+    if default_name_id is None:
+        passed = False
+        return FAIL, Message(
+            "missing-default-name-id", "fvar is missing a default instance name ID."
+        )
+
+    fam_name = ttFont["name"].getBestFamilyName()
+    subfam_name = ttFont["name"].getDebugName(default_name_id)
+
+    if subfam_name is None:
+        passed = False
+        return FAIL, Message(
+            "missing-name-id",
+            f"Name ID {default_name_id} stored in"
+            f" fvar instance is missing in name table.",
+        )
+
+    default_name = f"{fam_name} {subfam_name}"
+
+    # check if default_name match family + subfamily name
+    for fam_id, subfam_id in [
+        (NameID.WWS_FAMILY_NAME, NameID.WWS_SUBFAMILY_NAME),
+        (NameID.TYPOGRAPHIC_FAMILY_NAME, NameID.TYPOGRAPHIC_SUBFAMILY_NAME),
+        (NameID.FONT_FAMILY_NAME, NameID.FONT_SUBFAMILY_NAME),
+    ]:
+        name_fam = ttFont["name"].getDebugName(fam_id)
+        name_subfam = ttFont["name"].getDebugName(subfam_id)
+
+        if (fam_id, subfam_id) in [
+            (NameID.WWS_FAMILY_NAME, NameID.WWS_SUBFAMILY_NAME),
+            (NameID.TYPOGRAPHIC_FAMILY_NAME, NameID.TYPOGRAPHIC_SUBFAMILY_NAME),
+        ]:
+            if [name_fam, name_subfam] == [None, None]:
+                return SKIP, Message(
+                    "missing-name-ids",
+                    f"It's not a requirement that a font has "
+                    f"to have these name IDs {fam_id} and {subfam_id}.",
+                )
+
+        if name_fam == None:
+            passed = False
+            yield FAIL, Message("missing-name-id", "Missing name ID {fam_id}.")
+        elif name_subfam == None:
+            passed = False
+            yield FAIL, Message("missing-name-id", "Missing name ID {subfam_id}.")
+        else:
+            possible_names = [f"{name_fam} {name_subfam}"]
+            if name_subfam.lower() == "regular":
+                possible_names.append(name_fam)
+
+            if default_name not in possible_names:
+                passed = False
+                yield FAIL, Message(
+                    "bad-name",
+                    f"Name {possible_names} does not match fvar"
+                    f" default name '{default_name}'",
+                )
+
+    if passed:
+        yield PASS, f"Name matches fvar default name '{default_name}'."
+
+
+@check(
     id="com.fontwerk/check/weight_class_fvar",
     rationale="""
         According to Microsoft's OT Spec the OS/2 usWeightClass
