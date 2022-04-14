dc1c0054c7087e0abc80b329b752c824e336f6b5
Miguel Sousa <msousa@adobe.com>
2022-04-13 19:43:00 -0700

    New check: com.adobe.fonts/check/varfont/same_size_instance_records
    Added to the OpenType Profile
    Validates that all of the instance records in a given font have the same size, with all either including or omitting the postScriptNameID field
    (issue #3705)

--- Lib/fontbakery/profiles/fvar.py
+++ Lib/fontbakery/profiles/fvar.py
@@ -410,3 +410,37 @@ def com_adobe_fonts_check_varfont_valid_default_instance_nameids(ttFont):
                 )
 
     yield PASS, "The default instance nameID values are valid"
+
+
+@check(
+    id="com.adobe.fonts/check/varfont/same_size_instance_records",
+    rationale="""
+        According to the 'fvar' documentation in OpenType spec v1.9
+        https://docs.microsoft.com/en-us/typography/opentype/spec/fvar
+
+        All of the instance records in a given font must be the same size, with
+        all either including or omitting the postScriptNameID field. [...]
+        If the value is 0xFFFF, then the value is ignored, and no PostScript name
+        equivalent is provided for the instance.
+    """,
+    conditions=["is_variable_font"],
+    proposal="https://github.com/googlefonts/fontbakery/issues/3705",
+)
+def com_adobe_fonts_check_varfont_same_size_instance_records(ttFont):
+    """Validates that all of the instance records in a given font have the same size."""
+
+    font_ps_nameids_not_provided = set(
+        [inst.postscriptNameID == 0xFFFF for inst in ttFont["fvar"].instances]
+    )
+
+    # 'font_ps_nameids_not_provided' is a set whose values can only be
+    # {True}, {False}, or {True, False}. So if the size of the set is not 1,
+    # it means that some instance records have postscriptNameID values while
+    # others do not.
+    if len(font_ps_nameids_not_provided) != 1:
+        yield FAIL, Message(
+            "different-size-instance-records",
+            "Instance records don't all have the same size.",
+        )
+
+    yield PASS, "All instance records have the same size"
