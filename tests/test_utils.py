31f223c275ca5f568e8962e267edaa1dafceb521
Miguel Sousa <msousa@adobe.com>
2022-04-17 18:24:20 -0700

    Support soft-return (U+2029) in rationale strings. Add unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,8 @@
-import pytest
-from fontbakery.utils import text_flow, can_shape
+from fontbakery.utils import (
+    can_shape,
+    text_flow,
+    unindent_and_unwrap_rationale,
+)
 from fontTools.ttLib import TTFont
 from fontbakery.codetesting import portable_path
 
@@ -49,9 +52,40 @@ def test_text_flow():
 #                                             "      Two   \n"
 #                                             "      Three ")
 
+
 def test_can_shape():
     font = TTFont(portable_path(
         "data/test/source-sans-pro/OTF/SourceSansPro-Regular.otf"
     ))
     assert can_shape(font, "ABC")
     assert not can_shape(font, "こんにちは")
+
+
+def test_unindent_and_unwrap_rationale():
+    rationale = """
+        This is a line that is very long, so long in fact that it must be hard wrapped
+        because it is longer than 88 lines, including the two 4-space indents.
+
+        This is a new paragraph. This paragraph is also too long to fit within the
+        maximum width, so it must be hard wrapped.
+        This is a new line that was NOT soft-wrapped, so it will end up appendend to
+        the previous line. 
+        This is yet another line, but this one was soft-wrapped (Shift+Return), which
+        means that it will not be appendend to the end of the previous line.
+
+        This is the last paragraph.
+    """
+    expected_rationale = (
+        "This is a line that is very long, so long in fact that it must be hard wrapped"
+        " because it is longer than 88 lines, including the two 4-space indents.\n"
+        "\n"
+        "This is a new paragraph. This paragraph is also too long to fit within the"
+        " maximum width, so it must be hard wrapped."
+        " This is a new line that was NOT soft-wrapped, so it will end up appendend to"
+        " the previous line.\n"
+        "This is yet another line, but this one was soft-wrapped (Shift+Return), which"
+        " means that it will not be appendend to the end of the previous line.\n"
+        "\n"
+        "This is the last paragraph."
+    )
+    assert unindent_and_unwrap_rationale(rationale) == expected_rationale
