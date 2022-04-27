27b6f46604d7bf1d8ba4a1c1e74acf558b23942b
Miguel Sousa <msousa@adobe.com>
2022-04-26 23:33:15 -0700

    Downgrade "com.adobe.fonts/check/find_empty_letters" to WARN
    only for a specific set of hangul characters

--- Lib/fontbakery/profiles/adobefonts.py
+++ Lib/fontbakery/profiles/adobefonts.py
@@ -4,6 +4,10 @@ Checks for Adobe Fonts (formerly known as Typekit).
 import unicodedata
 
 from fontbakery.callable import check
+from fontbakery.constants import (
+    ALL_HANGUL_SYLLABLES_CODEPOINTS,
+    MODERN_HANGUL_SYLLABLES_CODEPOINTS,
+)
 from fontbakery.fonts_profile import profile_factory
 from fontbakery.message import Message, KEEP_ORIGINAL_MESSAGE
 from fontbakery.profiles.fontwerk import FONTWERK_PROFILE_CHECKS
@@ -285,18 +289,27 @@ def _quick_and_dirty_glyph_is_empty(font, glyph_name):
         underlying assumption that letters (i.e. characters with Unicode general
         category 'Ll', 'Lm', 'Lo', 'Lt', or 'Lu', which includes CJK ideographs and
         Hangul syllables) with entries in the 'cmap' table have glyphs with ink (with
-        a few exceptions, notably the Hangul "filler" characters).
+        a few exceptions, notably the four Hangul "filler" characters: U+115F, U+1160,
+        U+3164, U+FFA0).
 
         This check is intended to identify fonts in which such letters have been mapped
         to empty glyphs (typically done as a form of subsetting). Letters with empty
         glyphs should have their entries removed from the 'cmap' table, even if the
         empty glyphs are left in place (e.g. for CID consistency).
+
+        The check will yield only a WARN if the blank glyph maps to a character in the
+        range of Korean hangul syllable code-points, which are known to be used by font
+        designers as a workaround to undesired behavior from InDesign's Korean IME
+        (Input Method Editor).
+        More details available at https://github.com/googlefonts/fontbakery/issues/2894
     """,
     proposal="https://github.com/googlefonts/fontbakery/pull/2460",
 )
 def com_adobe_fonts_check_find_empty_letters(ttFont):
     """Letters in font have glyphs that are not empty?"""
     cmap = ttFont.getBestCmap()
+    blank_ok_set = ALL_HANGUL_SYLLABLES_CODEPOINTS - MODERN_HANGUL_SYLLABLES_CODEPOINTS
+    num_blank_hangul_glyphs = 0
     passed = True
 
     # http://unicode.org/reports/tr44/#General_Category_Values
@@ -316,8 +329,14 @@ def com_adobe_fonts_check_find_empty_letters(ttFont):
     }
     for unicode_val, glyph_name in cmap.items():
         category = unicodedata.category(chr(unicode_val))
-        if (
-            _quick_and_dirty_glyph_is_empty(ttFont, glyph_name)
+        glyph_is_empty = _quick_and_dirty_glyph_is_empty(ttFont, glyph_name)
+
+        if glyph_is_empty and unicode_val in blank_ok_set:
+            num_blank_hangul_glyphs += 1
+            passed = False
+
+        elif (
+            glyph_is_empty
             and (category in letter_categories)
             and (unicode_val not in invisible_letters)
         ):
@@ -327,8 +346,15 @@ def com_adobe_fonts_check_find_empty_letters(ttFont):
                 f"but its glyph ({glyph_name!r}) is empty.",
             )
             passed = False
+
     if passed:
         yield PASS, "No empty glyphs for letters found."
+
+    elif num_blank_hangul_glyphs:
+        yield WARN, Message(
+            "empty-hangul-letter",
+            f"Found {num_blank_hangul_glyphs} empty hangul glyph(s).",
+        )
 
 
 @check(
