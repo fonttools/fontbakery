f48ecb14210b90328c1ae56d3860af5a611aa3e3
Miguel Sousa <msousa@adobe.com>
2022-04-26 23:33:15 -0700

    Downgrade "com.adobe.fonts/check/find_empty_letters" to WARN
    only for a specific set of hangul characters

--- tests/profiles/adobefonts_test.py
+++ tests/profiles/adobefonts_test.py
@@ -102,6 +102,16 @@ def test_check_find_empty_letters():
     ttFont = TTFont(TEST_FILE("source-sans-pro/TTF/SourceSansPro-Bold.ttf"))
     assert assert_PASS(check(ttFont)) == PASS_MSG
 
+    # Add 2 Korean hangul syllable characters to cmap table mapped to the 'space' glyph.
+    # These characters are part of the set whose glyphs are allowed to be blank.
+    # The check should only yield a WARN.
+    for cmap_table in ttFont["cmap"].tables:
+        if cmap_table.format != 4:
+            cmap_table.cmap[0xB646] = "space"
+            cmap_table.cmap[0xD7A0] = "space"
+    msg = assert_results_contain(check(ttFont), WARN, "empty-hangul-letter")
+    assert msg == "Found 2 empty hangul glyph(s)."
+
     # this font has empty glyphs for several letters,
     # the first of which is 'B' (U+0042)
     ttFont = TTFont(TEST_FILE("familysans/FamilySans-Regular.ttf"))
