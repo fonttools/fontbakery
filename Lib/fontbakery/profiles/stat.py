2fcc74a1d647bfb50b1bac25190ccc24c011d098
Miguel Sousa <msousa@adobe.com>
2022-05-02 00:14:29 -0700

    Use bullet_list in FAIL message

--- Lib/fontbakery/profiles/stat.py
+++ Lib/fontbakery/profiles/stat.py
@@ -1,6 +1,7 @@
 from fontbakery.callable import check
 from fontbakery.message import Message
 from fontbakery.status import FAIL, PASS
+from fontbakery.utils import bullet_list
 
 # used to inform get_module_profile whether and how to create a profile
 from fontbakery.fonts_profile import (  # NOQA pylint: disable=unused-import
@@ -21,7 +22,7 @@ profile_imports = [(".shared_conditions", ("is_variable_font",))]
     conditions=["is_variable_font"],
     proposal="https://github.com/googlefonts/fontbakery/pull/3017",
 )
-def com_google_fonts_check_varfont_stat_axis_record_for_each_axis(ttFont):
+def com_google_fonts_check_varfont_stat_axis_record_for_each_axis(ttFont, config):
     """All fvar axes have a correspondent Axis Record on STAT table?"""
     fvar_axes = set(a.axisTag for a in ttFont["fvar"].axes)
     stat_axes = set(a.AxisTag for a in ttFont["STAT"].table.DesignAxisRecord.Axis)
@@ -29,8 +30,8 @@ def com_google_fonts_check_varfont_stat_axis_record_for_each_axis(ttFont):
     if len(missing_axes) > 0:
         yield FAIL, Message(
             "missing-axis-records",
-            f"STAT table is missing Axis Records for"
-            f" the following axes: {missing_axes}",
+            f"STAT table is missing Axis Records for the following axes:\n\n"
+            f"{bullet_list(config, sorted(missing_axes))}",
         )
     else:
-        yield PASS, "STAT table has all necessary Axis Records"
+        yield PASS, "STAT table has all necessary Axis Records."
