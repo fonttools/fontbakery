00788d265a6aa9864ebdc5e649c904e202f42633
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2023-05-08 14:14:39 +0100

    Added rationale to linegaps check & migrated it to universal profile
    check-id: com.google.fonts/check/linegaps
    From: OpenType profile
    To: Universal profile
    
    (issue #4133)

--- tests/profiles/universal_test.py
+++ tests/profiles/universal_test.py
@@ -1242,3 +1242,36 @@ def test_check_math_signs_width():
     font = TEST_FILE("montserrat/Montserrat-Regular.ttf")
     assert_results_contain(check(font),
                            WARN, "width-outliers")
+
+
+def test_check_linegaps():
+    """ Checking Vertical Metric Linegaps. """
+    check = CheckTester(universal_profile,
+                        "com.google.fonts/check/linegaps")
+
+    # Our reference Mada Regular is know to be bad here.
+    ttFont = TTFont(TEST_FILE("mada/Mada-Regular.ttf"))
+
+    # But just to be sure, we first explicitely set
+    # the values we're checking for:
+    ttFont['hhea'].lineGap = 1
+    ttFont['OS/2'].sTypoLineGap = 0
+    assert_results_contain(check(ttFont),
+                           FAIL, 'hhea',
+                           'with non-zero hhea.lineGap...')
+
+    # Then we run the check with a non-zero OS/2.sTypoLineGap:
+    ttFont['hhea'].lineGap = 0
+    ttFont['OS/2'].sTypoLineGap = 1
+    assert_results_contain(check(ttFont),
+                           FAIL, 'OS/2',
+                           'with non-zero OS/2.sTypoLineGap...')
+
+    # And finaly we fix it by making both values equal to zero:
+    ttFont['hhea'].lineGap = 0
+    ttFont['OS/2'].sTypoLineGap = 0
+    assert_PASS(check(ttFont))
+
+    # Confirm the check yields FAIL if the font doesn't have a required table
+    del ttFont['OS/2']
+    assert_results_contain(check(ttFont), FAIL, "lacks-table")
