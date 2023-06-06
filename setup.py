2b5683a1e19584be7ac497b24b12397dca0ab66e
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2023-06-06 14:06:52 +0100

    Declaring extras for specific profiles,
    so that we can minimize the dependency tree based on which profiles the
    user intends to run.
    (issue #3874)

--- setup.py
+++ setup.py
@@ -21,6 +21,31 @@ try:
 except IOError:
     readme = ''
 
+
+# Vendor-specific dependencies:
+googlefonts_extras = [
+    'axisregistry>=0.3.0',
+    'beautifulsoup4',  # For parsing registered vendor IDs from Microsoft's webpage
+    'dehinter>=3.1.0',  # 3.1.0 added dehinter.font.hint function
+    'font-v',
+    'gflanguages>=0.3.0',  # There was an api simplification/update on v0.3.0
+                           # (see https://github.com/googlefonts/gflanguages/pull/7)
+    'glyphsets>=0.5.0',
+    'lxml',
+    'protobuf>=3.7.0, <4',  # 3.7.0 fixed a bug on parsing some METADATA.pb files.
+                            # We cannot use v4 because our protobuf files have been compiled with v3.
+                            # (see https://github.com/googlefonts/fontbakery/issues/2200)
+    'unicodedata2',
+]
+
+fontval_extras = [
+    'lxml',  # Note: lxml is actually installed as a sub-dependency of the core ones.
+             #       But I am keeping it listed here explicitely, just in case...
+]
+
+all_extras = googlefonts_extras \
+           + fontval_extras
+
 setup(
     name="fontbakery",
     use_scm_version={"write_to": "Lib/fontbakery/_version.py"},
@@ -64,7 +89,13 @@ setup(
     install_requires=[
         # --- core dependencies
         'fontTools[ufo,lxml,unicode]>=4.39.0',  # Python 3.8+ required
+        'freetype-py!=2.4.0',  # Avoiding 2.4.0 due to seg-fault described at
+                               # https://github.com/googlefonts/fontbakery/issues/4143
+        'munkres',  # For interpolation compatibility checking (fontTools dependency)
         'opentypespec',
+        'opentype-sanitizer>=7.1.9',  # 7.1.9 fixes caret value format = 3 bug
+                                      # (see https://github.com/khaledhosny/ots/pull/182)
+
 
         # --- for parsing Configuration files
         'PyYAML',
@@ -75,32 +106,24 @@ setup(
         'rich',
 
         # --- for checking Font Bakery's version
-        'packaging',
-        'pip-api',
-        'requests',
+        'packaging',  # Universal profile
+        'pip-api',    # Universal profile
+        'requests',   # Universal & googlefonts profiles
 
-        # TODO: move the packages below to Extras
-        'axisregistry>=0.3.0',
-        'beautifulsoup4',
-        'beziers>=0.5.0', # Uses new fontTools glyph outline access
-        'collidoscope>=0.5.2', # 0.5.1 did not yet support python 3.11
-                               # (see https://github.com/googlefonts/fontbakery/issues/3970)
-        'defcon',
-        'dehinter>=3.1.0', # 3.1.0 added dehinter.font.hint function
-        'font-v',
-        'gflanguages>=0.3.0', # there was an api simplification/update on v0.3.0 (see https://github.com/googlefonts/gflanguages/pull/7)
-        'glyphsets>=0.5.0',
-        'lxml',
-        'munkres',  # for interpolation compatibility checking
-        # 3.7.0 fixed a bug on parsing some METADATA.pb files.
-        # We cannot use v4 because our protobuf files have been compiled with v3.
-        'protobuf>=3.7.0, <4',
-                            # (see https://github.com/googlefonts/fontbakery/issues/2200)
-        'stringbrewer',
-        'ufolint',
-        'ufo2ft>=2.25.2',  # 2.25.2 updated the script lists for Unicode 14.0
-        'unicodedata2',
-        'vharfbuzz>=0.2.0',  # API update
+
+        # TODO: Try to split the packages below into feature-specific extras.
+        'beziers>=0.5.0',  # Opentype, iso15008, Shaping (& Universal) profiles
+                           # Uses new fontTools glyph outline access
+        'collidoscope>=0.5.2',  # Shaping (& Universal) profiles
+                                # 0.5.1 did not yet support python 3.11
+                                # (see https://github.com/googlefonts/fontbakery/issues/3970)
+        'defcon',  # Universal, UFO sources & OpenType profiles
+        'stringbrewer',  # Shaping (& Universal) profiles
+        'ufolint',  # ufo_sources profile
+        'ufo2ft>=2.25.2',  # Shaping (& Universal), Opentype, ufo_sources profiles
+                           # 2.25.2 updated the script lists for Unicode 14.0
+        'vharfbuzz>=0.2.0',  # Googlefonts, Shaping (& Universal) profiles
+                             # v0.2.0 had an API update
     ],
     extras_require={
         'docs': [
@@ -108,12 +131,9 @@ setup(
             'sphinx >= 1.4',
             'sphinx_rtd_theme',
         ],
-        'freetype': [
-            'freetype-py!=2.4.0', # Avoiding 2.4.0 due to seg-fault described at https://github.com/googlefonts/fontbakery/issues/4143
-        ],
-        'ots': [
-            'opentype-sanitizer>=7.1.9',  # 7.1.9 fixes caret value format = 3 bug (see https://github.com/khaledhosny/ots/pull/182)
-        ],
+        'all': all_extras,
+        'googlefonts': googlefonts_extras,
+        'fontval': fontval_extras,
     },
     entry_points={
         'console_scripts': ['fontbakery=fontbakery.cli:main'],
