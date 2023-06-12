9c66146bb967e5a32c57c55f6958a8b331333734
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2023-06-12 23:40:54 +0100

    update setup.py
    to use extras based on the needs of specific profiles
    (issue #3874)

--- setup.py
+++ setup.py
@@ -17,9 +17,43 @@
 from setuptools import setup
 
 try:
-    readme = open("README.md").read()
+    readme = open("README.md", encoding="utf8").read()
 except IOError:
     readme = ""
+
+
+FONTTOOLS_VERSION = ">=4.39.0"  # Python 3.8+ required
+UFO2FT_VERSION = ">=2.25.2"
+
+# Profile-specific dependencies:
+ufo_sources_extras = [
+    "defcon",
+    f"fontTools[ufo]{FONTTOOLS_VERSION}",
+    f"ufo2ft{UFO2FT_VERSION}",
+    "ufolint",
+]
+
+googlefonts_extras = [
+    "axisregistry>=0.3.0",
+    "beautifulsoup4",  # For parsing registered vendor IDs from Microsoft's webpage
+    "dehinter>=3.1.0",  # 3.1.0 added dehinter.font.hint function
+    "font-v",
+    f"fontTools[lxml,unicode]{FONTTOOLS_VERSION}",
+    "gflanguages>=0.3.0",  # There was an api simplification/update on v0.3.0
+    # (see https://github.com/googlefonts/gflanguages/pull/7)
+    "glyphsets>=0.5.0",
+    "protobuf>=3.7.0, <4",  # 3.7.0 fixed a bug on parsing some METADATA.pb files.
+    # We cannot use v4 because our protobuf files have been compiled with v3.
+    # (see https://github.com/googlefonts/fontbakery/issues/2200)
+] + ufo_sources_extras
+
+fontval_extras = [
+    "lxml",
+]
+
+docs_extras = []
+
+all_extras = set(docs_extras + googlefonts_extras + fontval_extras + ufo_sources_extras)
 
 setup(
     name="fontbakery",
@@ -37,16 +71,17 @@ setup(
         " Nikolaus Waxweiler,"
         " Chris Simpkins,"
         " Jens Kutilek,"
-        " Vitaly Volkov"
+        " Vitaly Volkov,"
+        " Simon Cozens,"
+        " Miguel Sousa"
     ),
-    author_email="dave@lab6.com",
+    author_email="juca@members.fsf.org",
     package_dir={"": "Lib"},
     packages=[
         "fontbakery",
         "fontbakery.reporters",
         "fontbakery.profiles",
         "fontbakery.commands",
-        "fontbakery.sphinx_extensions",
     ],
     package_data={"fontbakery": ["data/*.cache", "data/googlefonts/*_exceptions.txt"]},
     classifiers=[
@@ -55,8 +90,11 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
     python_requires=">=3.8",
     setup_requires=[
@@ -64,52 +102,51 @@ setup(
         "setuptools_scm[toml]>=6.2",
     ],
     install_requires=[
-        "axisregistry>=0.3.0",
-        "beautifulsoup4",
-        "beziers>=0.5.0",  # Uses new fontTools glyph outline access
-        "cmarkgfm",
-        "collidoscope>=0.5.2",  # 0.5.1 did not yet support python 3.11
-        # (see https://github.com/googlefonts/fontbakery/issues/3970)
-        "defcon",
-        "dehinter>=3.1.0",  # 3.1.0 added dehinter.font.hint function
-        "fontTools[ufo,lxml,unicode]>=4.36.0",  # allows for passing location to glyphsets
-        "font-v",
-        "gflanguages>=0.3.0",  # there was an api simplification/update on v0.3.0 (see https://github.com/googlefonts/gflanguages/pull/7)
-        "glyphsets>=0.5.0",
-        "lxml",
-        "munkres",  # for interpolation compatibility checking
+        # --- core dependencies
+        f"fontTools{FONTTOOLS_VERSION}",
+        "freetype-py!=2.4.0",  # Avoiding 2.4.0 due to seg-fault described at
+        # https://github.com/googlefonts/fontbakery/issues/4143
+        "munkres",  # For interpolation compatibility checking (fontTools dependency)
+        "opentypespec",
         "opentype-sanitizer>=7.1.9",  # 7.1.9 fixes caret value format = 3 bug
         # (see https://github.com/khaledhosny/ots/pull/182)
-        "opentypespec",
-        "packaging",  # needed for checking Font Bakery's version
-        "pip-api",  # needed for checking Font Bakery's version
-        # 3.7.0 fixed a bug on parsing some METADATA.pb files.
-        # We cannot use v4 because our protobuf files have been compiled with v3.
-        "protobuf>=3.7.0, <4",
-        # (see https://github.com/googlefonts/fontbakery/issues/2200)
+        #
+        # --- for parsing Configuration files
         "PyYAML",
-        "requests",
-        "rich",
-        "stringbrewer",
         "toml",
-        "ufolint",
-        "ufo2ft>=2.25.2",  # 2.25.2 updated the script lists for Unicode 14.0
-        "unicodedata2",
-        "vharfbuzz>=0.2.0",  # API update
+        #
+        # --- used by Reporters
+        "cmarkgfm",
+        "rich",
+        #
+        # --- for checking FontBakery's version
+        "packaging",  # Universal profile
+        "pip-api",  # Universal profile
+        "requests",  # Universal & googlefonts profiles
+        #
+        # TODO: Try to split the packages below into feature-specific extras.
+        "beziers>=0.5.0",  # Opentype, iso15008, Shaping (& Universal) profiles
+        # Uses new fontTools glyph outline access
+        "collidoscope>=0.5.2",  # Shaping (& Universal) profiles
+        # 0.5.1 did not yet support python 3.11
+        # (see https://github.com/googlefonts/fontbakery/issues/3970)
+        "stringbrewer",  # Shaping (& Universal) profiles
+        f"ufo2ft{UFO2FT_VERSION}",  # Shaping
+        # 2.25.2 updated the script lists for Unicode 14.0
+        "vharfbuzz>=0.2.0",  # Googlefonts, Shaping (& Universal) profiles
+        # v0.2.0 had an API update
     ],
     extras_require={
-        "docs": [
-            "recommonmark",
-            "sphinx >= 1.4",
-            "sphinx_rtd_theme",
-        ],
-        "freetype": [
-            "freetype-py!=2.4.0",  # Avoiding 2.4.0 due to seg-fault described at https://github.com/googlefonts/fontbakery/issues/4143
-        ],
+        "all": all_extras,
+        "docs": docs_extras,
+        "googlefonts": googlefonts_extras,
+        "fontval": fontval_extras,
+        "ufo-sources": ufo_sources_extras,
     },
     entry_points={
         "console_scripts": ["fontbakery=fontbakery.cli:main"],
     },
+
     # TODO: review this and make it cross-platform:
     #    data_files=[
     #        ('/etc/bash_completion.d', ['snippets/fontbakery.bash-completion']),
