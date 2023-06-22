miguelsousa/openbakery

Fix F403 'from <module> import *' used; unable to detect undefined names
and F405 '<name>' may be undefined, or defined from star imports

Lib/openbakery/profiles/cff.py
@@ -3,9 +3,7 @@ from openbakery.status import FAIL, PASS, WARN
 from openbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from openbakery.fonts_profile import (
-    profile_factory,
-)  # NOQA pylint: disable=unused-import
+from openbakery.fonts_profile import profile_factory  # noqa: F401
 
 profile_imports = [(".shared_conditions", ("is_cff", "is_cff2"))]
 

Lib/openbakery/profiles/dsig.py
@@ -3,9 +3,7 @@ from openbakery.status import WARN, PASS
 from openbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from openbakery.fonts_profile import (
-    profile_factory,
-)  # NOQA pylint: disable=unused-import
+from openbakery.fonts_profile import profile_factory  # noqa: F401
 
 
 @check(

Lib/openbakery/profiles/gdef.py
@@ -3,9 +3,7 @@ from openbakery.status import FAIL, PASS, WARN, SKIP
 from openbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from openbakery.fonts_profile import (
-    profile_factory,
-)  # NOQA pylint: disable=unused-import
+from openbakery.fonts_profile import profile_factory  # noqa: F401
 
 
 def _is_non_spacing_mark_char(charcode):

Lib/openbakery/profiles/googlefonts.py
@@ -6,7 +6,7 @@ from openbakery.profiles.universal import UNIVERSAL_PROFILE_CHECKS
 from openbakery.profiles.ufo_sources import UFO_PROFILE_CHECKS
 from openbakery.status import INFO, WARN, ERROR, SKIP, PASS, FAIL
 from openbakery.section import Section
-from openbakery.callable import check, disable
+from openbakery.callable import check, condition, disable
 from openbakery.utils import (
     add_check_overrides,
     bullet_list,
@@ -22,12 +22,13 @@ from openbakery.constants import (
     WindowsLanguageID,
     MacintoshEncodingID,
     MacintoshLanguageID,
+    UnicodeEncodingID,
     LATEST_TTFAUTOHINT_VERSION,
 )
-from .googlefonts_conditions import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
-
-profile_imports = ((".", ("universal", "outline", "ufo_sources")),)
+profile_imports = (
+    (".", ("googlefonts_conditions", "universal", "outline", "ufo_sources")),
+)
 profile = profile_factory(default_section=Section("Google Fonts"))
 
 profile.configuration_defaults = {
@@ -1721,17 +1722,18 @@ def com_google_fonts_check_hinting_impact(font, hinting_stats):
 def com_google_fonts_check_file_size(font):
     """Ensure files are not too large."""
     size = os.stat(font).st_size
-    if size > FAIL_SIZE:
+    if size > FAIL_SIZE:  # noqa: F821
         yield FAIL, Message(
             "massive-font",
             f"Font file is {filesize_formatting(size)}, "
-            f"larger than limit {filesize_formatting(FAIL_SIZE)}",
+            f"larger than limit {filesize_formatting(FAIL_SIZE)}",  # noqa: F821
         )
-    elif size > WARN_SIZE:
+    elif size > WARN_SIZE:  # noqa: F821
         yield WARN, Message(
             "large-font",
             f"Font file is {filesize_formatting(size)}; "
-            f"ideally it should be less than {filesize_formatting(WARN_SIZE)}",
+            f"ideally it should be less than "
+            f"{filesize_formatting(WARN_SIZE)}",  # noqa: F821
         )
     else:
         yield PASS, "Font had a reasonable file size"

Lib/openbakery/profiles/googlefonts_conditions.py
@@ -9,6 +9,10 @@ from openbakery.constants import (
     UnicodeEncodingID,
     WindowsLanguageID,
 )
+
+# used to inform get_module_profile whether and how to create a profile
+from openbakery.fonts_profile import profile_factory  # noqa: F401
+
 from .shared_conditions import style
 
 

Lib/openbakery/profiles/layout.py
@@ -1,12 +1,11 @@
+from opentypespec.tags import FEATURE_TAGS, SCRIPT_TAGS, LANGUAGE_TAGS
+
 from openbakery.callable import check
 from openbakery.status import PASS, FAIL
 from openbakery.message import Message
-from openbakery.fonts_profile import (
-    profile_factory,
-)  # NOQA pylint: disable=unused-import
 
-
-from opentypespec.tags import FEATURE_TAGS, SCRIPT_TAGS, LANGUAGE_TAGS
+# used to inform get_module_profile whether and how to create a profile
+from openbakery.fonts_profile import profile_factory  # noqa: F401
 
 
 def feature_tags(ttFont):

Lib/openbakery/profiles/post.py
@@ -3,9 +3,7 @@ from openbakery.status import FAIL, PASS, WARN
 from openbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from openbakery.fonts_profile import (
-    profile_factory,
-)  # NOQA pylint: disable=unused-import
+from openbakery.fonts_profile import profile_factory  # noqa: F401
 
 profile_imports = [(".shared_conditions", ("is_ttf",))]
 

tests/profiles/external_profile_test.py
@@ -125,12 +125,17 @@ def test_profile_imports():
         # just as an example: import a check and a dependency/condition of
         # that check from the googlefonts specific profile:
         (
+            "openbakery.profiles.googlefonts_conditions",
+            (
+                # This condition is a dependency of the check below:
+                "familyname",
+            ),
+        ),
+        (
             "openbakery.profiles.googlefonts",
             (
                 # "License URL matches License text on name table?"
                 "com_google_fonts_check_name_license_url",
-                # This condition is a dependency of the check above:
-                "familyname",
             ),
         ),
     ]

fonttools/fontbakery

Lib/fontbakery/profiles/cff.py
@@ -3,9 +3,7 @@ from fontbakery.status import FAIL, PASS, WARN
 from fontbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from fontbakery.fonts_profile import (
-    profile_factory,  # NOQA pylint: disable=unused-import
-)
+from fontbakery.fonts_profile import profile_factory  # noqa: F401
 
 profile_imports = [(".shared_conditions", ("is_cff", "is_cff2"))]
 

Lib/fontbakery/profiles/dsig.py
@@ -3,9 +3,7 @@ from fontbakery.status import WARN, PASS
 from fontbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from fontbakery.fonts_profile import (  # NOQA pylint: disable=unused-import
-    profile_factory,
-)
+from fontbakery.fonts_profile import profile_factory  # noqa: F401
 
 
 @check(

Lib/fontbakery/profiles/gdef.py
@@ -3,9 +3,7 @@ from fontbakery.status import FAIL, PASS, WARN, SKIP
 from fontbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from fontbakery.fonts_profile import (  # NOQA pylint: disable=unused-import
-    profile_factory,
-)
+from fontbakery.fonts_profile import profile_factory  # noqa: F401
 
 
 def _is_non_spacing_mark_char(charcode):

Lib/fontbakery/profiles/googlefonts.py
@@ -6,7 +6,7 @@ from fontbakery.profiles.universal import UNIVERSAL_PROFILE_CHECKS
 from fontbakery.profiles.ufo_sources import UFO_PROFILE_CHECKS
 from fontbakery.status import INFO, WARN, ERROR, SKIP, PASS, FAIL
 from fontbakery.section import Section
-from fontbakery.callable import check, disable
+from fontbakery.callable import check, condition, disable
 from fontbakery.utils import (
     add_check_overrides,
     bullet_list,
@@ -22,12 +22,13 @@ from fontbakery.constants import (
     WindowsLanguageID,
     MacintoshEncodingID,
     MacintoshLanguageID,
+    UnicodeEncodingID,
     LATEST_TTFAUTOHINT_VERSION,
 )
-from .googlefonts_conditions import *  # pylint: disable=wildcard-import,unused-wildcard-import
 
-
-profile_imports = ((".", ("universal", "outline", "ufo_sources")),)
+profile_imports = (
+    (".", ("googlefonts_conditions", "universal", "outline", "ufo_sources")),
+)
 profile = profile_factory(default_section=Section("Google Fonts"))
 
 profile.configuration_defaults = {
@@ -1721,17 +1722,18 @@ def com_google_fonts_check_hinting_impact(font, hinting_stats):
 def com_google_fonts_check_file_size(font):
     """Ensure files are not too large."""
     size = os.stat(font).st_size
-    if size > FAIL_SIZE:
+    if size > FAIL_SIZE:  # noqa: F821
         yield FAIL, Message(
             "massive-font",
             f"Font file is {filesize_formatting(size)}, "
-            f"larger than limit {filesize_formatting(FAIL_SIZE)}",
+            f"larger than limit {filesize_formatting(FAIL_SIZE)}",  # noqa: F821
         )
-    elif size > WARN_SIZE:
+    elif size > WARN_SIZE:  # noqa: F821
         yield WARN, Message(
             "large-font",
             f"Font file is {filesize_formatting(size)}; "
-            f"ideally it should be less than {filesize_formatting(WARN_SIZE)}",
+            f"ideally it should be less than "
+            f"{filesize_formatting(WARN_SIZE)}",  # noqa: F821
         )
     else:
         yield PASS, "Font had a reasonable file size"

Lib/fontbakery/profiles/googlefonts_conditions.py
@@ -9,6 +9,10 @@ from fontbakery.constants import (
     UnicodeEncodingID,
     WindowsLanguageID,
 )
+
+# used to inform get_module_profile whether and how to create a profile
+from fontbakery.fonts_profile import profile_factory  # noqa: F401
+
 from .shared_conditions import style
 
 

Lib/fontbakery/profiles/layout.py
@@ -1,11 +1,11 @@
+from opentypespec.tags import FEATURE_TAGS, SCRIPT_TAGS, LANGUAGE_TAGS
+
 from fontbakery.callable import check
 from fontbakery.status import PASS, FAIL
 from fontbakery.message import Message
-from fontbakery.fonts_profile import (  # NOQA pylint: disable=unused-import
-    profile_factory,
-)
 
-from opentypespec.tags import FEATURE_TAGS, SCRIPT_TAGS, LANGUAGE_TAGS
+# used to inform get_module_profile whether and how to create a profile
+from fontbakery.fonts_profile import profile_factory  # noqa: F401
 
 
 def feature_tags(ttFont):

Lib/fontbakery/profiles/post.py
@@ -3,9 +3,7 @@ from fontbakery.status import FAIL, PASS, WARN
 from fontbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from fontbakery.fonts_profile import (  # NOQA pylint: disable=unused-import
-    profile_factory,
-)
+from fontbakery.fonts_profile import profile_factory  # noqa: F401
 
 profile_imports = [(".shared_conditions", ("is_ttf",))]
 

tests/profiles/external_profile_test.py
@@ -125,12 +125,17 @@ def test_profile_imports():
         # just as an example: import a check and a dependency/condition of
         # that check from the googlefonts specific profile:
         (
+            "fontbakery.profiles.googlefonts_conditions",
+            (
+                # This condition is a dependency of the check below:
+                "familyname",
+            ),
+        ),
+        (
             "fontbakery.profiles.googlefonts",
             (
                 # "License URL matches License text on name table?"
                 "com_google_fonts_check_name_license_url",
-                # This condition is a dependency of the check above:
-                "familyname",
             ),
         ),
     ]