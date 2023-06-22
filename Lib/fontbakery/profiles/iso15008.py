miguelsousa/openbakery

Wrap the imports in try/except. Sort imports

Lib/openbakery/profiles/iso15008.py
@@ -2,16 +2,29 @@
 Checks for suitability for in-car displays (ISO 15008).
 """
 
+from fontTools.pens.boundsPen import BoundsPen
+
 from openbakery.callable import check
-from openbakery.section import Section
-from openbakery.status import PASS, FAIL
 from openbakery.fonts_profile import profile_factory
 from openbakery.message import Message
-from fontTools.pens.boundsPen import BoundsPen
-from beziers.path import BezierPath
-from beziers.line import Line
-from beziers.point import Point
-import uharfbuzz as hb
+from openbakery.section import Section
+from openbakery.status import PASS, FAIL
+
+
+try:
+    from beziers.path import BezierPath
+    from beziers.line import Line
+    from beziers.point import Point
+    import uharfbuzz as hb
+except ImportError:
+    import sys
+
+    sys.exit(
+        "\nTo run the iso15008 profile, one needs to install\n"
+        "openbakery with the 'iso15008' extra, like this:\n"
+        "\n"
+        "python -m pip install -U 'openbakery[iso15008]'\n\n"
+    )
 
 
 profile = profile_factory(default_section=Section("Suitability for In-Car Display"))

fonttools/fontbakery

Lib/fontbakery/profiles/iso15008.py
@@ -2,16 +2,29 @@
 Checks for suitability for in-car displays (ISO 15008).
 """
 
+from fontTools.pens.boundsPen import BoundsPen
+
 from fontbakery.callable import check
-from fontbakery.section import Section
-from fontbakery.status import PASS, FAIL
 from fontbakery.fonts_profile import profile_factory
 from fontbakery.message import Message
-from fontTools.pens.boundsPen import BoundsPen
-from beziers.path import BezierPath
-from beziers.line import Line
-from beziers.point import Point
-import uharfbuzz as hb
+from fontbakery.section import Section
+from fontbakery.status import PASS, FAIL
+
+
+try:
+    from beziers.path import BezierPath
+    from beziers.line import Line
+    from beziers.point import Point
+    import uharfbuzz as hb
+except ImportError:
+    import sys
+
+    sys.exit(
+        "\nTo run the iso15008 profile, one needs to install\n"
+        "fontbakery with the 'iso15008' extra, like this:\n"
+        "\n"
+        "python -m pip install -U 'fontbakery[iso15008]'\n\n"
+    )
 
 
 profile = profile_factory(default_section=Section("Suitability for In-Car Display"))