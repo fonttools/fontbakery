miguelsousa/openbakery

Fix F401 '<module>' imported but unused

Lib/openbakery/profiles/fontbureau.py
@@ -4,7 +4,7 @@ Checks for Font Bureau.
 
 from openbakery.callable import check
 from openbakery.section import Section
-from openbakery.status import PASS, FAIL, WARN
+from openbakery.status import PASS, FAIL
 from openbakery.fonts_profile import profile_factory
 from openbakery.message import Message
 from openbakery.profiles.universal import UNIVERSAL_PROFILE_CHECKS

Lib/openbakery/profiles/gpos.py
@@ -3,7 +3,7 @@ from openbakery.status import PASS, WARN
 from openbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from openbakery.fonts_profile import profile_factory  # noqa: F401
+from openbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
 
 
 @condition

Lib/openbakery/profiles/layout.py
@@ -5,7 +5,7 @@ from openbakery.status import PASS, FAIL
 from openbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from openbakery.fonts_profile import profile_factory  # noqa: F401
+from openbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
 
 
 def feature_tags(ttFont):

Lib/openbakery/profiles/name.py
@@ -1,4 +1,4 @@
-from openbakery.callable import check, disable
+from openbakery.callable import check
 from openbakery.status import FAIL, PASS, WARN, INFO, SKIP
 from openbakery.message import Message
 from openbakery.constants import (
@@ -9,7 +9,7 @@ from openbakery.constants import (
 )
 
 # used to inform get_module_profile whether and how to create a profile
-from openbakery.fonts_profile import profile_factory  # noqa: F401
+from openbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
 
 profile_imports = [(".shared_conditions", ("glyph_metrics_stats", "is_ttf", "is_cff"))]
 

tests/profiles/adobefonts_test.py
@@ -8,7 +8,7 @@ from fontTools.ttLib import TTFont
 from fontTools.ttLib.tables.otTables import AxisValueRecord
 from requests.exceptions import ConnectionError
 
-from openbakery.checkrunner import WARN, FAIL, PASS, SKIP
+from openbakery.status import WARN, FAIL, PASS, SKIP
 from openbakery.codetesting import (
     assert_PASS,
     assert_results_contain,

tests/profiles/fontwerk_test.py
@@ -1,5 +1,6 @@
 from fontTools.ttLib import TTFont
-from openbakery.checkrunner import FAIL, SKIP
+
+from openbakery.status import FAIL, SKIP
 from openbakery.codetesting import (
     assert_PASS,
     assert_results_contain,

tests/profiles/fvar_test.py
@@ -1,7 +1,7 @@
 from fontTools.ttLib import TTFont
 from fontTools.ttLib.tables._f_v_a_r import Axis
 
-from openbakery.checkrunner import FAIL, WARN, SKIP
+from openbakery.status import FAIL, WARN, SKIP
 from openbakery.codetesting import (
     assert_PASS,
     assert_results_contain,

tests/profiles/googlefonts_test.py
@@ -7,7 +7,7 @@ from fontTools.ttLib import TTFont
 
 from openbakery.profiles.googlefonts import can_shape
 from openbakery.profiles.googlefonts_conditions import expected_font_names
-from openbakery.checkrunner import DEBUG, INFO, WARN, ERROR, SKIP, PASS, FAIL, ENDCHECK
+from openbakery.status import DEBUG, INFO, WARN, ERROR, SKIP, PASS, FAIL, ENDCHECK
 from openbakery.codetesting import (
     assert_results_contain,
     assert_PASS,
@@ -2897,7 +2897,6 @@ def test_check_font_names(fp, mod, result):
     # this repository already has good unit tests but this check will also include the previous
     # test cases found in openbakery.
     # https://github.com/googlefonts/axisregistry/blob/main/tests/test_names.py
-    from openbakery.profiles.googlefonts_conditions import expected_font_names
 
     check = CheckTester(googlefonts_profile, "com.google.fonts/check/font_names")
     ttFont = TTFont(fp)
@@ -3803,8 +3802,6 @@ def test_check_vertical_metrics_regressions(cabin_ttFonts):
     check = CheckTester(
         googlefonts_profile, "com.google.fonts/check/vertical_metrics_regressions"
     )
-    from openbakery.profiles.shared_conditions import family_directory
-
     ttFonts = [TTFont(f) for f in cabin_fonts]
 
     # Cabin test family should match by default

fonttools/fontbakery

Lib/fontbakery/profiles/fontbureau.py
@@ -4,7 +4,7 @@ Checks for Font Bureau.
 
 from fontbakery.callable import check
 from fontbakery.section import Section
-from fontbakery.status import PASS, FAIL, WARN
+from fontbakery.status import PASS, FAIL
 from fontbakery.fonts_profile import profile_factory
 from fontbakery.message import Message
 from fontbakery.profiles.universal import UNIVERSAL_PROFILE_CHECKS

Lib/fontbakery/profiles/gpos.py
@@ -3,7 +3,7 @@ from fontbakery.status import PASS, WARN
 from fontbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from fontbakery.fonts_profile import profile_factory  # noqa: F401
+from fontbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
 
 
 @condition

Lib/fontbakery/profiles/layout.py
@@ -5,7 +5,7 @@ from fontbakery.status import PASS, FAIL
 from fontbakery.message import Message
 
 # used to inform get_module_profile whether and how to create a profile
-from fontbakery.fonts_profile import profile_factory  # noqa: F401
+from fontbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
 
 
 def feature_tags(ttFont):

Lib/fontbakery/profiles/name.py
@@ -1,4 +1,4 @@
-from fontbakery.callable import check, disable
+from fontbakery.callable import check
 from fontbakery.status import FAIL, PASS, WARN, INFO, SKIP
 from fontbakery.message import Message
 from fontbakery.constants import (
@@ -9,7 +9,7 @@ from fontbakery.constants import (
 )
 
 # used to inform get_module_profile whether and how to create a profile
-from fontbakery.fonts_profile import profile_factory  # noqa: F401
+from fontbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
 
 profile_imports = [(".shared_conditions", ("glyph_metrics_stats", "is_ttf", "is_cff"))]
 

tests/profiles/adobefonts_test.py
@@ -8,7 +8,7 @@ from fontTools.ttLib import TTFont
 from fontTools.ttLib.tables.otTables import AxisValueRecord
 from requests.exceptions import ConnectionError
 
-from fontbakery.checkrunner import WARN, FAIL, PASS, SKIP
+from fontbakery.status import WARN, FAIL, PASS, SKIP
 from fontbakery.codetesting import (
     assert_PASS,
     assert_results_contain,

tests/profiles/fontwerk_test.py
@@ -1,5 +1,6 @@
 from fontTools.ttLib import TTFont
-from fontbakery.checkrunner import FAIL, SKIP
+
+from fontbakery.status import FAIL, SKIP
 from fontbakery.codetesting import (
     assert_PASS,
     assert_results_contain,

tests/profiles/fvar_test.py
@@ -1,7 +1,7 @@
 from fontTools.ttLib import TTFont
 from fontTools.ttLib.tables._f_v_a_r import Axis
 
-from fontbakery.checkrunner import FAIL, WARN, SKIP
+from fontbakery.status import FAIL, WARN, SKIP
 from fontbakery.codetesting import (
     assert_PASS,
     assert_results_contain,

tests/profiles/googlefonts_test.py
@@ -7,7 +7,7 @@ from fontTools.ttLib import TTFont
 
 from fontbakery.profiles.googlefonts import can_shape
 from fontbakery.profiles.googlefonts_conditions import expected_font_names
-from fontbakery.checkrunner import DEBUG, INFO, WARN, ERROR, SKIP, PASS, FAIL, ENDCHECK
+from fontbakery.status import DEBUG, INFO, WARN, ERROR, SKIP, PASS, FAIL, ENDCHECK
 from fontbakery.codetesting import (
     assert_results_contain,
     assert_PASS,
@@ -2897,7 +2897,6 @@ def test_check_font_names(fp, mod, result):
     # this repository already has good unit tests but this check will also include the previous
     # test cases found in fontbakery.
     # https://github.com/googlefonts/axisregistry/blob/main/tests/test_names.py
-    from fontbakery.profiles.googlefonts_conditions import expected_font_names
 
     check = CheckTester(googlefonts_profile, "com.google.fonts/check/font_names")
     ttFont = TTFont(fp)
@@ -3803,8 +3802,6 @@ def test_check_vertical_metrics_regressions(cabin_ttFonts):
     check = CheckTester(
         googlefonts_profile, "com.google.fonts/check/vertical_metrics_regressions"
     )
-    from fontbakery.profiles.shared_conditions import family_directory
-
     ttFonts = [TTFont(f) for f in cabin_fonts]
 
     # Cabin test family should match by default