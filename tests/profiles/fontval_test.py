miguelsousa/openbakery

Add tests for recent changes

tests/profiles/fontval_test.py
@@ -1,10 +1,24 @@
 import shutil
+import sys
 
 import pytest
+
+from conftest import ImportRaiser, remove_import_raiser, reload_module
 
 from openbakery.codetesting import TEST_FILE, assert_results_contain, CheckTester
 from openbakery.status import FAIL
 from openbakery.profiles import fontval as fontval_profile
+
+
+def test_extra_needed_exit(monkeypatch):
+    module_name = "lxml.etree"
+    sys.meta_path.insert(0, ImportRaiser(module_name))
+    monkeypatch.delitem(sys.modules, module_name, raising=False)
+
+    with pytest.raises(SystemExit):
+        reload_module("openbakery.profiles.fontval")
+
+    remove_import_raiser(module_name)
 
 
 def test_check_fontvalidator_not_installed():

tests/profiles/iso15008_test.py
@@ -1,3 +1,9 @@
+import sys
+
+import pytest
+
+from conftest import ImportRaiser, remove_import_raiser, reload_module
+
 from openbakery.status import FAIL
 from openbakery.codetesting import (
     assert_PASS,
@@ -6,6 +12,17 @@ from openbakery.codetesting import (
     TEST_FILE,
 )
 from openbakery.profiles import iso15008 as iso15008_profile
+
+
+def test_extra_needed_exit(monkeypatch):
+    module_name = "uharfbuzz"
+    sys.meta_path.insert(0, ImportRaiser(module_name))
+    monkeypatch.delitem(sys.modules, module_name, raising=False)
+
+    with pytest.raises(SystemExit):
+        reload_module("openbakery.profiles.iso15008")
+
+    remove_import_raiser(module_name)
 
 
 def test_check_iso15008_proportions():

tests/test_utils.py
@@ -1,11 +1,25 @@
+from unittest.mock import patch
+
 import pytest
 
 from openbakery.utils import (
     bullet_list,
+    exit_with_install_instructions,
     pretty_print_list,
     text_flow,
     unindent_and_unwrap_rationale,
 )
+
+
+def test_exit_with_install_instructions():
+    extras_name = "test-profile"
+    with patch("sys.exit") as mock_exit:
+        exit_with_install_instructions(extras_name)
+        mock_exit.assert_called_with(
+            f"\nTo run the {extras_name} profile, one needs to install\n"
+            f"openbakery with the '{extras_name}' extra, like this:\n\n"
+            f"    python -m pip install -U 'openbakery[{extras_name}]'\n\n"
+        )
 
 
 def test_text_flow():

fonttools/fontbakery

tests/profiles/fontval_test.py
@@ -1,10 +1,24 @@
 import shutil
+import sys
 
 import pytest
+
+from conftest import ImportRaiser, remove_import_raiser, reload_module
 
 from fontbakery.codetesting import TEST_FILE, assert_results_contain
 from fontbakery.status import ERROR
 from fontbakery.profiles import fontval as fontval_profile
+
+
+def test_extra_needed_exit(monkeypatch):
+    module_name = "lxml.etree"
+    sys.meta_path.insert(0, ImportRaiser(module_name))
+    monkeypatch.delitem(sys.modules, module_name, raising=False)
+
+    with pytest.raises(SystemExit):
+        reload_module("fontbakery.profiles.fontval")
+
+    remove_import_raiser(module_name)
 
 
 @pytest.mark.skipif(

tests/profiles/iso15008_test.py
@@ -1,3 +1,9 @@
+import sys
+
+import pytest
+
+from conftest import ImportRaiser, remove_import_raiser, reload_module
+
 from fontbakery.status import FAIL
 from fontbakery.codetesting import (
     assert_PASS,
@@ -6,6 +12,17 @@ from fontbakery.codetesting import (
     TEST_FILE,
 )
 from fontbakery.profiles import iso15008 as iso15008_profile
+
+
+def test_extra_needed_exit(monkeypatch):
+    module_name = "uharfbuzz"
+    sys.meta_path.insert(0, ImportRaiser(module_name))
+    monkeypatch.delitem(sys.modules, module_name, raising=False)
+
+    with pytest.raises(SystemExit):
+        reload_module("fontbakery.profiles.iso15008")
+
+    remove_import_raiser(module_name)
 
 
 def test_check_iso15008_proportions():

tests/test_utils.py
@@ -1,11 +1,25 @@
+from unittest.mock import patch
+
 import pytest
 
 from fontbakery.utils import (
     bullet_list,
+    exit_with_install_instructions,
     pretty_print_list,
     text_flow,
     unindent_and_unwrap_rationale,
 )
+
+
+def test_exit_with_install_instructions():
+    extras_name = "test-profile"
+    with patch("sys.exit") as mock_exit:
+        exit_with_install_instructions(extras_name)
+        mock_exit.assert_called_with(
+            f"\nTo run the {extras_name} profile, one needs to install\n"
+            f"fontbakery with the '{extras_name}' extra, like this:\n\n"
+            f"    python -m pip install -U 'fontbakery[{extras_name}]'\n\n"
+        )
 
 
 def test_text_flow():