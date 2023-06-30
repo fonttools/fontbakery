miguelsousa/openbakery

Refactor extras installation message

Lib/openbakery/profiles/fontval.py
@@ -6,22 +6,14 @@ from openbakery.fonts_profile import profile_factory
 from openbakery.status import ERROR, FAIL, INFO, PASS, WARN
 from openbakery.section import Section
 from openbakery.message import Message
+from openbakery.utils import exit_with_install_instructions
 
 from .shared_conditions import is_cff, is_variable_font
-
 
 try:
     from lxml import etree
 except ImportError:
-    import sys
-
-    sys.exit(
-        "\nTo run the fontval profile, one needs to install\n"
-        "openbakery with the 'fontval' extra, like this:\n"
-        "\n"
-        "python -m pip install -U 'openbakery[fontval]'\n\n"
-    )
-
+    exit_with_install_instructions("fontval")
 
 profile_imports = [".shared_conditions"]
 profile = profile_factory(

Lib/openbakery/profiles/googlefonts.py
@@ -23,6 +23,7 @@ from openbakery.constants import (
     UnicodeEncodingID,
     LATEST_TTFAUTOHINT_VERSION,
 )
+from openbakery.utils import exit_with_install_instructions
 
 profile_imports = (
     (".", ("googlefonts_conditions", "universal", "outline", "ufo_sources")),
@@ -250,7 +251,10 @@ GOOGLEFONTS_PROFILE_CHECKS = (
 )
 def com_google_fonts_check_canonical_filename(ttFont):
     """Checking file is named canonically."""
-    from axisregistry import build_filename
+    try:
+        from axisregistry import build_filename
+    except ImportError:
+        exit_with_install_instructions("googlefonts")
 
     current_filename = os.path.basename(ttFont.reader.file.name)
     expected_filename = build_filename(ttFont)

Lib/openbakery/profiles/googlefonts_conditions.py
@@ -9,6 +9,7 @@ from openbakery.constants import (
     UnicodeEncodingID,
     WindowsLanguageID,
 )
+from openbakery.utils import exit_with_install_instructions
 
 # used to inform get_module_profile whether and how to create a profile
 from openbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
@@ -183,14 +184,7 @@ def family_metadata(metadata_file):
     try:
         from google.protobuf import text_format
     except ImportError:
-        import sys
-
-        sys.exit(
-            "\nTo run the googlefonts profile, one needs to install\n"
-            "openbakery with the 'fontval' extra, like this:\n"
-            "\n"
-            "python -m pip install -U 'openbakery[googlefonts]'\n\n"
-        )
+        exit_with_install_instructions("googlefonts")
 
     from openbakery.utils import get_FamilyProto_Message
 

Lib/openbakery/profiles/iso15008.py
@@ -9,7 +9,7 @@ from openbakery.fonts_profile import profile_factory
 from openbakery.message import Message
 from openbakery.section import Section
 from openbakery.status import PASS, FAIL
-
+from openbakery.utils import exit_with_install_instructions
 
 try:
     from beziers.path import BezierPath
@@ -17,15 +17,7 @@ try:
     from beziers.point import Point
     import uharfbuzz as hb
 except ImportError:
-    import sys
-
-    sys.exit(
-        "\nTo run the iso15008 profile, one needs to install\n"
-        "openbakery with the 'iso15008' extra, like this:\n"
-        "\n"
-        "python -m pip install -U 'openbakery[iso15008]'\n\n"
-    )
-
+    exit_with_install_instructions("iso15008")
 
 profile = profile_factory(default_section=Section("Suitability for In-Car Display"))
 

Lib/openbakery/profiles/ufo_sources.py
@@ -3,7 +3,7 @@ from openbakery.status import ERROR, FAIL, PASS, WARN
 from openbakery.section import Section
 from openbakery.message import Message
 from openbakery.fonts_profile import profile_factory
-
+from openbakery.utils import exit_with_install_instructions
 
 profile = profile_factory(default_section=Section("UFO Sources"))
 
@@ -23,9 +23,12 @@ UFO_PROFILE_CHECKS = [
 def ufo_font(ufo):
     try:
         import defcon
+        from fontTools.ufoLib.errors import UFOLibError
 
         return defcon.Font(ufo)
-    except Exception:
+    except ImportError:
+        exit_with_install_instructions("ufo-sources")
+    except UFOLibError:
         return None
 
 

Lib/openbakery/utils.py
@@ -24,6 +24,14 @@ from fontTools.ttLib import TTFont
 from openbakery.constants import NO_COLORS_THEME, DARK_THEME, LIGHT_THEME
 
 
+def exit_with_install_instructions(extras_name):
+    sys.exit(
+        f"\nTo run the {extras_name} profile, one needs to install\n"
+        f"openbakery with the '{extras_name}' extra, like this:\n\n"
+        f"    python -m pip install -U 'openbakery[{extras_name}]'\n\n"
+    )
+
+
 # TODO: this should be part of OpenBakeryCheck and check.conditions
 # should be a tuple (negated, name)
 def is_negated(name):

fonttools/fontbakery

Lib/fontbakery/profiles/fontval.py
@@ -6,22 +6,14 @@ from fontbakery.fonts_profile import profile_factory
 from fontbakery.status import ERROR, FAIL, INFO, PASS, WARN
 from fontbakery.section import Section
 from fontbakery.message import Message
+from fontbakery.utils import exit_with_install_instructions
 
 from .shared_conditions import is_cff, is_variable_font
-
 
 try:
     from lxml import etree
 except ImportError:
-    import sys
-
-    sys.exit(
-        "\nTo run the fontval profile, one needs to install\n"
-        "fontbakery with the 'fontval' extra, like this:\n"
-        "\n"
-        "python -m pip install -U 'fontbakery[fontval]'\n\n"
-    )
-
+    exit_with_install_instructions("fontval")
 
 profile_imports = [".shared_conditions"]
 profile = profile_factory(

Lib/fontbakery/profiles/googlefonts.py
@@ -23,6 +23,7 @@ from fontbakery.constants import (
     UnicodeEncodingID,
     LATEST_TTFAUTOHINT_VERSION,
 )
+from fontbakery.utils import exit_with_install_instructions
 
 profile_imports = (
     (".", ("googlefonts_conditions", "universal", "outline", "ufo_sources")),
@@ -250,7 +251,10 @@ GOOGLEFONTS_PROFILE_CHECKS = (
 )
 def com_google_fonts_check_canonical_filename(ttFont):
     """Checking file is named canonically."""
-    from axisregistry import build_filename
+    try:
+        from axisregistry import build_filename
+    except ImportError:
+        exit_with_install_instructions("googlefonts")
 
     current_filename = os.path.basename(ttFont.reader.file.name)
     expected_filename = build_filename(ttFont)

Lib/fontbakery/profiles/googlefonts_conditions.py
@@ -9,6 +9,7 @@ from fontbakery.constants import (
     UnicodeEncodingID,
     WindowsLanguageID,
 )
+from fontbakery.utils import exit_with_install_instructions
 
 # used to inform get_module_profile whether and how to create a profile
 from fontbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
@@ -183,14 +184,7 @@ def family_metadata(metadata_file):
     try:
         from google.protobuf import text_format
     except ImportError:
-        import sys
-
-        sys.exit(
-            "\nTo run the googlefonts profile, one needs to install\n"
-            "fontbakery with the 'fontval' extra, like this:\n"
-            "\n"
-            "python -m pip install -U 'fontbakery[googlefonts]'\n\n"
-        )
+        exit_with_install_instructions("googlefonts")
 
     from fontbakery.utils import get_FamilyProto_Message
 

Lib/fontbakery/profiles/iso15008.py
@@ -9,7 +9,7 @@ from fontbakery.fonts_profile import profile_factory
 from fontbakery.message import Message
 from fontbakery.section import Section
 from fontbakery.status import PASS, FAIL
-
+from fontbakery.utils import exit_with_install_instructions
 
 try:
     from beziers.path import BezierPath
@@ -17,15 +17,7 @@ try:
     from beziers.point import Point
     import uharfbuzz as hb
 except ImportError:
-    import sys
-
-    sys.exit(
-        "\nTo run the iso15008 profile, one needs to install\n"
-        "fontbakery with the 'iso15008' extra, like this:\n"
-        "\n"
-        "python -m pip install -U 'fontbakery[iso15008]'\n\n"
-    )
-
+    exit_with_install_instructions("iso15008")
 
 profile = profile_factory(default_section=Section("Suitability for In-Car Display"))
 

Lib/fontbakery/profiles/ufo_sources.py
@@ -3,7 +3,7 @@ from fontbakery.status import ERROR, FAIL, PASS, WARN
 from fontbakery.section import Section
 from fontbakery.message import Message
 from fontbakery.fonts_profile import profile_factory
-
+from fontbakery.utils import exit_with_install_instructions
 
 profile = profile_factory(default_section=Section("UFO Sources"))
 
@@ -23,9 +23,12 @@ UFO_PROFILE_CHECKS = [
 def ufo_font(ufo):
     try:
         import defcon
+        from fontTools.ufoLib.errors import UFOLibError
 
         return defcon.Font(ufo)
-    except Exception:
+    except ImportError:
+        exit_with_install_instructions("ufo-sources")
+    except UFOLibError:
         return None
 
 

Lib/fontbakery/utils.py
@@ -24,6 +24,14 @@ from fontTools.ttLib import TTFont
 from fontbakery.constants import NO_COLORS_THEME, DARK_THEME, LIGHT_THEME
 
 
+def exit_with_install_instructions(extras_name):
+    sys.exit(
+        f"\nTo run the {extras_name} profile, one needs to install\n"
+        f"fontbakery with the '{extras_name}' extra, like this:\n\n"
+        f"    python -m pip install -U 'fontbakery[{extras_name}]'\n\n"
+    )
+
+
 # TODO: this should be part of FontBakeryCheck and check.conditions
 # should be a tuple (negated, name)
 def is_negated(name):