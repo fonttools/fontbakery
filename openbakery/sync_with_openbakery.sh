2ae50abc20a57cb6026c50af3650a6db6e9f3546
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2023-07-07 14:55:43 +0100

    use rsync on openbakery sync script

--- openbakery/sync_with_openbakery.sh
+++ openbakery/sync_with_openbakery.sh
@@ -4,8 +4,21 @@
 # Miguel Sousa is doing there.
 
 # Backup a few things:
-mv CHANGELOG.md CHANGELOG.md.fontbakery
-mv venv venv.fontbakery
+rm -rf ../fontbakery-tmp-backup
+mkdir ../fontbakery-tmp-backup
+mv CHANGELOG.md ../fontbakery-tmp-backup/
+mv venv ../fontbakery-tmp-backup
+mv .git ../fontbakery-tmp-backup
+mv docs ../fontbakery-tmp-backup/
+mv openbakery ../fontbakery-tmp-backup/
+
+# Also keeping these things that I still do not understand why Miguel deleted:
+mv snippets/ ../fontbakery-tmp-backup/
+mv MANIFEST.in ../fontbakery-tmp-backup/
+mv .readthedocs.yml ../fontbakery-tmp-backup/
+mv .pre-commit-config.yaml ../fontbakery-tmp-backup/
+mv .editorconfig ../fontbakery-tmp-backup/
+
 
 #### Get the latest contents from the Open Bakery github repo ####
 cd ~/devel/openbakery/
@@ -13,23 +26,21 @@ git fetch origin
 git rebase origin/main
 cd ~/fb
 
-cp ~/devel/openbakery/* . -rf  # Start by over-writting everything
-                               # on our clean FontBakery git repo.
+# Start by over-writting everything on our clean FontBakery git repo.
+rsync --inplace -av --delete-excluded ~/devel/openbakery/ .
 
-# Also copy the dot dirs (such as .github/workflows)
-mv .git .git_backup
-cp ~/devel/openbakery/.[^.]* . -rf
-rm -rf .git
-mv .git_backup .git
+# But the directory has the fork's name, so we move its contents and delete the directory
+mkdir Lib/fontbakery/
+cp Lib/openbakery/* -rf Lib/fontbakery/
+rm -rf Lib/openbakery
 
-cp Lib/openbakery/* -rf Lib/fontbakery/  # But the directory has the fork's name, so we
-rm -rf Lib/openbakery                    # move its contents and delete the directory
-
-
-# also rename this file, so that we can see whether there were updates on the Miguel's repo:
+# Also rename this file, so that we can see whether there were updates on the Miguel's repo:
 mv Lib/fontbakery/data/openbakery-microsoft-vendorlist.cache Lib/fontbakery/data/fontbakery-microsoft-vendorlist.cache
 
-# preserve correct API names
+# Bring back the patching & syncing directory ;-)
+mv ../fontbakery-tmp-backup/openbakery .
+
+# Preserve correct API names
 # (These are subtle case-name differences. We may want to address these quirks of the API on a major release in the future):
 git grep -rl OpenBakery -- ':(exclude)openbakery/' | xargs sed -i 's/OpenBakeryCallable/FontbakeryCallable/g'
 git grep -rl OpenBakery -- ':(exclude)openbakery/' | xargs sed -i 's/OpenBakeryReporter/FontbakeryReporter/g'
@@ -42,6 +53,9 @@ git grep -rl openbakery -- ':(exclude)openbakery/' | xargs sed -i 's/miguelsousa
 git grep -rl openbakery -- ':(exclude)openbakery/' | xargs sed -i 's/openbakery/fontbakery/g'
 git grep -rl OpenBakery -- ':(exclude)openbakery/' | xargs sed -i 's/OpenBakery/FontBakery/g'
 
+# fix author_email field on setup.py:
+sed -i 's/miguel.sousa@adobe.com/juca@members.fsf.org/g' setup.py
+
 # Instances of abusive, harassing, or otherwise unacceptable behavior may be
 # reported to the community leaders responsible for enforcement at:
 sed -i 's/miguel.sousa@adobe.com/dcrossland@google.com/g' CODE_OF_CONDUCT.md
@@ -52,15 +66,25 @@ sed -i 's/Miguel Sousa at miguel.sousa@adobe.com./us via the issue tracker or se
 
 ##### Now here's the patching out of the things we don't need: #####
 
+# Restore our .git
+rm -rf .git
+mv ../fontbakery-tmp-backup/.git .
+
 # We do not need the fork's logo:
 rm -f data/openbakery.jpg
 
 # We have our own changelog:
-mv CHANGELOG.md.fontbakery CHANGELOG.md
+mv ../fontbakery-tmp-backup/CHANGELOG.md .
 
 # And our own virtual environment
 rm -rf venv
-mv venv.fontbakery venv
+mv ../fontbakery-tmp-backup/venv .
+
+# And restore those things deleted by Miguel that I still have to look more carefully (TODO):
+mv ../fontbakery-tmp-backup/snippets/ .
+mv ../fontbakery-tmp-backup/MANIFEST.in .
+mv ../fontbakery-tmp-backup/.pre-commit-config.yaml .
+mv ../fontbakery-tmp-backup/.editorconfig .
 
 # We also have our own documentation for now,
 # even though I would be happy to merge both projects docs soon:
@@ -71,6 +95,8 @@ rm docs/_config.yml
 rm docs/developer-guide.md
 rm docs/index.md
 rm docs/user-guide.md
+mv ../fontbakery-tmp-backup/docs .
+mv ../fontbakery-tmp-backup/.readthedocs.yml .
 
 # We still use the sphinx dependency for building the Font Bakery Read The Docs pages.
 # We also use the header line
@@ -120,11 +146,11 @@ patch -p1 -R < openbakery/patches/0006-unshallow-fetch-for-setuptools-scm-otherw
 #
 # This was discussed at:
 # https://github.com/miguelsousa/openbakery/commit/489a2cc76e009a7c7a6d4bd3d4f3be1a9db641bd#commitcomment-119393361
-patch -p1 -R < openbakery/patches/0007-Removal-of-check-prefix-on-subcommands.patch
+# TODO: patch -p1 -R < openbakery/patches/0007-Removal-of-check-prefix-on-subcommands.patch
 
 
 # cleanup bad-strings:
-patch -p1 -R < openbakery/patches/0008-fixes-to-strings.patch
+# TODO: patch -p1 -R < openbakery/patches/0008-fixes-to-strings.patch
 
 
 # FontBakery won't change FontValidator ERROR into a FAIL.
@@ -135,4 +161,4 @@ patch -p1 -R < openbakery/patches/0008-fixes-to-strings.patch
 # When FontValidator is not installed, it is treated as a bad system setup, thus, it is classified as an ERROR. It is not a FAIL because it is not a font problem.
 #
 # This was discussed at https://github.com/miguelsousa/openbakery/issues/30#issuecomment-1600765260
-patch -p1 -R < openbakery/patches/0009-FontBakery-won-t-change-FontValidator-ERROR-into-a-F.patch
+# TODO: patch -p1 -R < openbakery/patches/0009-FontBakery-won-t-change-FontValidator-ERROR-into-a-F.patch
