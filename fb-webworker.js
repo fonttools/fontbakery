295f759f544b3d50c4057278063847e679dd4993
Simon Cozens <simon@simon-cozens.org>
2023-09-05 15:32:43 +0100

    Add fontbakery-web

---
+++ fb-webworker.js
@@ -0,0 +1,109 @@
+importScripts('https://cdn.jsdelivr.net/pyodide/v0.23.4/full/pyodide.js');
+
+const EXCLUDE_CHECKS = [
+  'com.google.fonts/check/fontbakery_version', // We download the latest each time
+  'com.daltonmaag/check/ufo_required_fields',
+  'com.daltonmaag/check/ufo_recommended_fields',
+  'com.google.fonts/check/designspace_has_sources',
+  'com.google.fonts/check/designspace_has_default_master',
+  'com.google.fonts/check/designspace_has_consistent_glyphset',
+  'com.google.fonts/check/designspace_has_consistent_codepoints',
+  'com.google.fonts/check/shaping/regression',
+  'com.google.fonts/check/shaping/forbidden',
+  'com.google.fonts/check/shaping/collides',
+  'com.google.fonts/check/fontv', // Requires a subprocess
+
+];
+
+/** Produce an absolute URL for a wheel
+*
+* @param {string} path: the wheel's relative URL
+* @return {string}
+*/
+function reroot(path) {
+  return self.location.href.replace('fb-webworker.js', path);
+}
+
+async function loadPyodideAndPackages() {
+  self.pyodide = await loadPyodide();
+  await pyodide.loadPackage('micropip');
+  const micropip = pyodide.pyimport('micropip');
+  await micropip.install('glyphsets', false, false);
+  await micropip.install([
+    'axisregistry',
+    'setuptools',
+    'lxml',
+    'fontTools>=4.39.0',
+    'opentypespec',
+    'munkres',
+    'mock',
+    'requests',
+    'beziers>=0.5.0',
+    'dehinter',
+  ]);
+  await micropip.install('fontbakery', false, false, null, true);
+  await pyodide.runPythonAsync(`
+    from pyodide.http import pyfetch
+    response = await pyfetch("${reroot('fbwebapi.py')}")
+    with open("fbwebapi.py", "wb") as f:
+        f.write(await response.bytes())
+  `);
+  await pyodide.pyimport('fbwebapi');
+}
+const pyodideReadyPromise = loadPyodideAndPackages();
+
+self.onmessage = async (event) => {
+  // make sure loading is done
+  const {id, files, profile, loglevels, fulllists} = event.data;
+  await pyodideReadyPromise;
+  self.postMessage({ready: true});
+  self.profile = profile;
+  if (id == 'justload') {
+    return;
+  }
+  if (id == 'listchecks') {
+    try {
+      const checks = await self.pyodide.runPythonAsync(`
+          from fbwebapi import dump_all_the_checks
+
+          dump_all_the_checks()
+      `);
+      self.postMessage({checks: checks.toJs()});
+    } catch (error) {
+      self.postMessage({error: error.message});
+    }
+    return;
+  }
+
+  const callback = (msg) => self.postMessage(msg.toJs());
+
+  // Write the files
+  const filenames = [];
+  for (const [name, buffer] of Object.entries(files)) {
+    pyodide.FS.writeFile(name, buffer);
+    filenames.push(name);
+  }
+
+  self.filenames = filenames;
+  self.callback = callback;
+  self.loglevels = loglevels;
+  self.fulllists = fulllists;
+  self.exclude_checks = EXCLUDE_CHECKS;
+  try {
+    await self.pyodide.runPythonAsync(`
+        from fbwebapi import run_fontbakery
+        from js import filenames, callback, exclude_checks, profile, loglevels, fulllists
+
+        run_fontbakery(filenames,
+          profilename=profile,
+          callback=callback,
+          loglevels=loglevels,
+          full_lists=fulllists,
+          exclude_checks=exclude_checks
+        )
+    `);
+    self.postMessage({done: true});
+  } catch (error) {
+    self.postMessage({error: error.message, id});
+  }
+};
---
+++ fbwebapi.py
@@ -0,0 +1,139 @@
+import sys
+import types
+from mock import MagicMock
+import unicodedata
+
+sys.modules["unicodedata2"] = sys.modules["unicodedata"]
+for notreal in [
+    "cmarkgfm",
+    "cmarkgfm.cmark",
+    "toml",
+    "rich",
+    "pyyaml",
+    "yaml",
+    "glyphsLib",
+    "glyphsLib.glyphdata",
+    "defcon",
+]:
+    sys.modules[notreal] = MagicMock()
+
+import glyphsets
+
+import argparse
+from fontbakery.commands.check_profile import get_module, log_levels
+from fontbakery.reporters.serialize import SerializeReporter
+from fontbakery.checkrunner import (
+    get_module_profile,
+    CheckRunner,
+    START,
+    ENDCHECK,
+    distribute_generator,
+)
+import fontbakery
+import pkgutil
+from importlib import import_module
+import re
+
+
+class ProgressReporter(SerializeReporter):
+    def __init__(self, callback, loglevels):
+        super().__init__(loglevels)
+        self.count = 0
+        self.callback = callback
+
+    def receive(self, event):
+        super().receive(event)
+        status, message, identity = event
+        section, check, iterargs = identity
+        key = self._get_key(identity)
+        done = self.count - self._counter["(not finished)"]
+        if status == START:
+            self.count = len(message)
+        elif status == ENDCHECK:
+            self._items[key]["key"] = check.id
+            self._items[key]["doc"] = check.__doc__
+            self.callback({"progress": 100 * done / float(self.count)} | self._counter)
+            if message >= self.loglevels:
+                self.callback(self._items[key])
+
+
+def run_fontbakery(
+    paths,
+    callback,
+    profilename="universal",
+    loglevels="INFO",
+    checks=None,
+    exclude_checks=None,
+    full_lists=False,
+):
+    loglevels = log_levels[loglevels]
+    profile = get_module_profile(get_module("fontbakery.profiles." + profilename))
+    argument_parser = argparse.ArgumentParser()
+    # Hack
+    argument_parser.add_argument(
+        "--list-checks",
+        default=False,
+        action="store_true",
+        help="List the checks available in the selected profile.",
+    )
+    values_keys = profile.setup_argparse(argument_parser)
+    args = argument_parser.parse_args(paths)
+    values = {}
+    for key in values_keys:
+        if hasattr(args, key):
+            values[key] = getattr(args, key)
+    runner = CheckRunner(
+        profile,
+        values=values,
+        config={
+            "custom_order": None,
+            "explicit_checks": checks,
+            "exclude_checks": exclude_checks,
+            "full_lists": full_lists,
+        },
+    )
+    prog = ProgressReporter(callback, loglevels)
+    prog.runner = runner
+    reporters = [prog.receive]
+    status_generator = runner.run()
+    distribute_generator(status_generator, reporters)
+
+
+def dump_all_the_checks():
+    checks = {}
+    profiles_modules = [
+        x.name
+        for x in pkgutil.walk_packages(fontbakery.__path__, "fontbakery.")
+        if x.name.startswith("fontbakery.profiles")
+    ]
+    for profile_name in profiles_modules:
+        try:
+            imported = import_module(profile_name, package=None)
+        except BaseException:
+            continue
+        profile = get_module_profile(imported)
+        if not profile:
+            continue
+        profile_name = profile_name[20:]
+        for section in profile._sections.values():
+            for check in section._checks:
+                if check.id not in checks:
+                    checks[check.id] = {
+                        "sections": set(),
+                        "profiles": set(),
+                    }
+                checks[check.id]["sections"].add(section.name)
+                checks[check.id]["profiles"].add(profile_name)
+                for attr in ["proposal", "rationale", "severity", "description"]:
+                    if getattr(check, attr):
+                        md = getattr(check, attr)
+                        if attr == "rationale":
+                            md = re.sub(r"(?m)^\s+", "", md)
+                            checks[check.id][attr] = md
+                        else:
+                            checks[check.id][attr] = md
+
+    for ck in checks.values():
+        ck["sections"] = list(ck["sections"])
+        ck["profiles"] = list(ck["profiles"])
+    return checks
