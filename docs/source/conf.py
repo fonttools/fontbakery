b5d3b333a5b0af7f26c4dd8bf6116d19ca82e0a8
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2023-09-12 11:03:58 +0100

    Update version on docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,7 @@ author = "The Font Bakery Authors"
 # The short X.Y version
 version = "0.9"
 # The full version, including alpha/beta/rc tags
-release = "0.9.0a3"
+release = "0.9.0"
 
 
 # -- General configuration ---------------------------------------------------
@@ -191,7 +191,7 @@ def linkcode_resolve(domain, info):
     # as seen on this URL:
     # https://github.com/fonttools/fontbakery/tree/v0.7.2/Lib/fontbakery/profiles
 
-    tree = "v0.9.0a3"
+    tree = "v0.9.0"
     # It's not planned for us to get the line number :-(
     # I had to hammer this data into the info.
     if "lineno" in info:
