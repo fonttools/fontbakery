8c9602176bb6fd28792083d47852d373c46db156
Simon Cozens <simon@simon-cozens.org>
2023-10-04 13:28:22 +0100

    Improve consistent repo URL check

--- Lib/fontbakery/profiles/googlefonts.py
+++ Lib/fontbakery/profiles/googlefonts.py
@@ -3318,7 +3318,9 @@ def com_google_fonts_check_metadata_canonical_style_names(ttFont, font_metadata)
     """,
     proposal="https://github.com/fonttools/fontbakery/issues/4056",
 )
-def com_google_fonts_check_metadata_consistent_repo_urls(config, family_metadata):
+def com_google_fonts_check_metadata_consistent_repo_urls(
+    config, family_metadata, license_contents, is_ofl, description
+):
     """
     METADATA.pb: Check URL on copyright string is the same as in repository_url field.
     """
@@ -3329,27 +3331,55 @@ def com_google_fonts_check_metadata_consistent_repo_urls(config, family_metadata
         )
         return
 
-    if repo_url.endswith(".git"):
-        repo_url = repo_url[:-4]
+    import re
 
-    bad_urls = set()
+    A_GITHUB_URL = r"github.com/\w+/\w+"
+
+    def clean_url(url):
+        if ")" in url:
+            url = url.split(")")[0].strip()
+        if url.endswith("/"):
+            url = url[:-1]
+        if url.endswith(".git"):
+            url = url[:-4]
+        return url
+
+    bad_urls = []
+    repo_url = clean_url(family_metadata.source.repository_url)
+    a_url = repo_url
+
     for font_md in family_metadata.fonts:
         if "http" in font_md.copyright:
-            link = "http" + font_md.copyright.split("http")[1]
+            link = clean_url("http" + font_md.copyright.split("http")[1])
+            if not a_url:
+                a_url = link
+            elif link != repo_url:
+                bad_urls.append(("font copyright string", link))
 
-            if ")" in link:
-                link = link.split(")")[0].strip()
+    if is_ofl and license_contents:
+        string = license_contents.strip().split("\n")[0]
+        if "http" in string:
+            link = clean_url("http" + string.split("http")[1])
+            if not a_url:
+                a_url = link
+            elif clean_url(link) != a_url:
+                bad_urls.append(("OFL text", link))
 
-            if link != repo_url:
-                bad_urls.add(link)
+    if a_url and description:
+        headless = re.sub(r"^https?://", "", a_url)
+        for match in re.findall(A_GITHUB_URL, description):
+            if clean_url(match) != headless:
+                bad_urls.append(("HTML description", match))
 
     if bad_urls:
         from fontbakery.utils import pretty_print_list
 
-        bad_urls = pretty_print_list(config, list(bad_urls))
+        bad_urls = pretty_print_list(
+            config, [f"{location} has '{url}'" for location, url in bad_urls]
+        )
         yield FAIL, Message(
             "mismatch",
-            f"repository_url: {repo_url}\n\nfont copyright string: {bad_urls}\n",
+            f"Repository URL is {a_url}\n\nBut: {bad_urls}\n",
         )
     else:
         yield PASS, "OK"
