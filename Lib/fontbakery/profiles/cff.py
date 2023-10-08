1b064fb49665254f8a3b9c064d230aadd470d671
Miguel Sousa <msousa@adobe.com>
2023-10-08 00:51:30 -0700

    Fix "cyclic-import" error
    R0401 (cyclic-import)  — Cyclic import (fontbakery.fonts_profile -> fontbakery.profiles.shared_conditions)

--- Lib/fontbakery/profiles/cff.py
+++ Lib/fontbakery/profiles/cff.py
@@ -5,7 +5,7 @@ from fontbakery.message import Message
 # used to inform get_module_profile whether and how to create a profile
 from fontbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
 
-profile_imports = [(".shared_conditions", ("is_cff", "is_cff2"))]
+profile_imports = ((".", ("shared_conditions",)),)
 
 
 class CFFAnalysis:
--- Lib/fontbakery/profiles/layout.py
+++ Lib/fontbakery/profiles/layout.py
@@ -7,6 +7,8 @@ from fontbakery.message import Message
 # used to inform get_module_profile whether and how to create a profile
 from fontbakery.fonts_profile import profile_factory  # noqa:F401 pylint:disable=W0611
 
+profile_imports = ((".", ("shared_conditions",)),)
+
 
 def feature_tags(ttFont):
     in_this_font = set()
