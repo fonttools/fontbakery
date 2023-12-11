3835475f01fa23d0f93e8657a4dcf53550f1331f
Simon Cozens <simon@simon-cozens.org>
2023-12-11 12:18:18 +0000

    A better check for long name table entries

--- Lib/fontbakery/profiles/googlefonts.py
+++ Lib/fontbakery/profiles/googlefonts.py
@@ -17,6 +17,7 @@ from fontbakery.utils import (
 from fontbakery.message import Message, KEEP_ORIGINAL_MESSAGE
 from fontbakery.fonts_profile import profile_factory
 from fontbakery.constants import (
+    RIBBI_STYLE_NAMES,
     NameID,
     PlatformID,
     WindowsEncodingID,
@@ -4906,52 +4907,73 @@ def com_google_fonts_check_kerning_for_non_ligated_sequences(
 @check(
     id="com.google.fonts/check/name/family_and_style_max_length",
     rationale="""
-        According to a GlyphsApp tutorial [1], in order to make sure all versions of
-        Windows recognize it as a valid font file, we must make sure that the
-        concatenated length of the familyname (NameID.FONT_FAMILY_NAME) and
-        style (NameID.FONT_SUBFAMILY_NAME) strings in the name table do not
-        exceed 20 characters.
+        This check ensures that the length of name table entries is not
+        too long, as this causes problems in some environments. For
+        details as to the latest requirements and the reason for them,
+        please see https://github.com/fonttools/fontbakery/issues/2179
 
-        After discussing the problem in more detail at FontBakery issue #2179 [2] we
-        decided that allowing up to 27 chars would still be on the safe side, though.
-
-        [1] https://glyphsapp.com/tutorials/multiple-masters-part-3-setting-up-instances
-        [2] https://github.com/fonttools/fontbakery/issues/2179
     """,
     proposal="https://github.com/fonttools/fontbakery/issues/1488",
     misc_metadata={
-        # Somebody with access to Windows should make some experiments
-        # and confirm that this is really the case.
         "affects": [("Windows", "unspecified")],
     },
 )
 def com_google_fonts_check_name_family_and_style_max_length(ttFont):
-    """Combined length of family and style must not exceed 27 characters."""
-    from fontbakery.utils import get_name_entries, get_name_entry_strings
+    """Combined length of family and style must not exceed 31 characters."""
+    from fontbakery.utils import get_name_entry_strings
+    import re
+
+    ribbi_re = " (" + "|".join(RIBBI_STYLE_NAMES) + ")$"
 
     passed = True
-    for familyname in get_name_entries(ttFont, NameID.FONT_FAMILY_NAME):
-        # we'll only match family/style name entries with the same platform ID:
-        plat = familyname.platformID
-        familyname_str = familyname.string.decode(familyname.getEncoding())
-        for stylename_str in get_name_entry_strings(
-            ttFont, NameID.FONT_SUBFAMILY_NAME, platformID=plat
-        ):
-            if len(familyname_str + stylename_str) > 27:
+
+    def strip_ribbi(x):
+        return re.sub(ribbi_re, "", x)
+
+    checks = [
+        [
+            NameID.FULL_FONT_NAME,
+            31,
+            "with the dropdown menu in old versions of Microsoft Word",
+            strip_ribbi,
+        ],
+        [
+            NameID.POSTSCRIPT_NAME,
+            27,
+            "with PostScript printers, especially on Mac platforms",
+            lambda x: x,
+        ],
+    ]
+    for nameid, maxlen, reason, transform in checks:
+        for the_name in get_name_entry_strings(ttFont, nameid):
+            the_name = transform(the_name)
+            if len(the_name) > maxlen:
                 passed = False
                 yield WARN, Message(
-                    "too-long",
-                    f"The combined length of family and style"
-                    f" exceeds 27 chars in the following"
-                    f" '{PlatformID(plat).name}' entries:\n"
-                    f" FONT_FAMILY_NAME = '{familyname_str}' /"
-                    f" SUBFAMILY_NAME = '{stylename_str}'\n"
-                    f"\n"
-                    f"Please take a look at the conversation at"
-                    f" https://github.com/fonttools/fontbakery/issues/2179"
-                    f" in order to understand the reasoning behind these"
-                    f" name table records max-length criteria.",
+                    f"nameid{nameid}-too-long",
+                    f"Name ID {nameid} '{the_name}' exceeds"
+                    f" {maxlen} characters. This has been found to"
+                    f" cause problems {reason}",
                 )
+
+    # name ID 1 + fvar instance name > 31 : WARN : problems with Windows
+    if "fvar" in ttFont:
+        for instance in ttFont["fvar"].instances:
+            for instance_name in get_name_entry_strings(
+                ttFont, instance.subfamilyNameID
+            ):
+                for family_name in get_name_entry_strings(
+                    ttFont, NameID.FONT_FAMILY_NAME
+                ):
+                    full_instance_name = instance_name + " " + family_name
+                    if len(full_instance_name) > 31:
+                        yield WARN, Message(
+                            "instance-too-long",
+                            f"Variable font instance name {full_instance_name}"
+                            " exceeds 31 characters. This has been found to "
+                            " cause problems in Microsoft Windows 11",
+                        )
+
     if passed:
         yield PASS, "All name entries are good."
 
