b8262f217049b02577ec226f25c23e9ed1a8ae0a
Marc Foley <m.foley.88@gmail.com>
2024-01-23 10:51:04 +0000

    refactor ofl license checks

--- Lib/fontbakery/profiles/googlefonts.py
+++ Lib/fontbakery/profiles/googlefonts.py
@@ -1447,17 +1447,42 @@ def com_google_fonts_check_license_OFL_copyright(license_contents):
 def com_google_fonts_check_license_OFL_body_text(license_contents):
     """Check OFL body text is correct."""
     from fontbakery.constants import OFL_BODY_TEXT
-    from fontbakery.utils import remove_white_space
+    from pprint import pformat
+    from difflib import Differ
+    # apply replacements so we get ideal license contents as of 2024.
+    # We want https and openfontslicense.org as the url. We also don't
+    # seem to care if the last line is an empty line.
+    # Not having these will raise warns in other checks.
+    if license_contents[-1] == "\n":
+        license_contents = license_contents[:-1]
+    license_contents = license_contents.replace(
+        "http://", "https://"
+    ).replace(
+        "https://scripts.sil.org/OFL",
+        "https://openfontlicense.org",
+    ).replace(
+        "<",
+        "\\<"
+    ).splitlines(keepends=True)[1:]
 
-    if remove_white_space(OFL_BODY_TEXT) not in remove_white_space(
-        license_contents.replace("http://", "https://")
-    ):
-        yield FAIL, Message(
+    diff = Differ()
+    res = diff.compare(
+        OFL_BODY_TEXT.splitlines(keepends=True),
+        license_contents
+    )
+
+    changed_lines = [f"\{l}".replace("\n", "\\n") for l in res if l.startswith(("-", "+"))]
+
+    if changed_lines:
+        output = "\n\n".join(changed_lines)
+        yield WARN, Message(
             "incorrect-ofl-body-text",
-            "The OFL.txt body text is incorrect. Please use"
-            " https://github.com/googlefonts/Unified-Font-Repository"
-            "/blob/main/OFL.txt as a template."
-            " You should only modify the first line.",
+            "The OFL.txt body text is incorrect. Please use "
+            "https://github.com/googlefonts/Unified-Font-Repository"
+            "/blob/main/OFL.txt as a template. "
+            "You should only modify the first line.\n\n"
+            "Lines changed:\n\n"
+            f"{output}\n\n"
         )
     else:
         yield PASS, "OFL license body text is correct"
@@ -1515,6 +1540,14 @@ def com_google_fonts_check_name_license(ttFont, license_filename):
                 value = "https://".join(value.split("http://"))
                 http_warn = True
 
+            if "scripts.sil.org/OFL" in value:
+                passed = True
+                yield WARN, Message(
+                    "old-old-url",
+                    "Please consider updating the url from 'https://scripts.sil.org/OFL' "
+                    "to 'https://openfontlicense.org'."
+                )
+                return
             if value != placeholder:
                 passed = False
                 yield FAIL, Message(
