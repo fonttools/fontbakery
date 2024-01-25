b020323140d0adbbf61463967be73f1e2d6a8fa3
Simon Cozens <simon@simon-cozens.org>
2024-01-25 14:33:18 +0000

    Add an indented paragraph renderable with optional first-line indent

--- Lib/fontbakery/utils.py
+++ Lib/fontbakery/utils.py
@@ -20,6 +20,7 @@ from typing import Text, Optional
 
 from fontTools.pens.basePen import BasePen
 from fontTools.ttLib import TTFont
+import rich
 
 from fontbakery.constants import NO_COLORS_THEME, DARK_THEME, LIGHT_THEME
 
@@ -631,3 +632,38 @@ class PointsPen(BasePen):
 
     def _addComponent(self, glyphName, transformation):
         self.glyphSet[glyphName].draw(self)
+
+
+class IndentedParagraph:
+    def __init__(self, renderable, left=4, right=0, first=None):
+        self.renderable = renderable
+        self.left = left
+        self.right = right
+        if first is not None:
+            self.first = first
+        else:
+            self.first = self.left
+
+    def __rich_console__(self, console, options):
+        style = console.get_style("none")
+        width = options.max_width
+        render_options = options.update_width(width - self.left - self.right)
+        lines = console.render_lines(
+            self.renderable, render_options, style=style, pad=True
+        )
+        _Segment = rich.segment.Segment
+
+        left = _Segment(" " * self.left, style) if self.left else None
+        first = _Segment(" " * self.first, style) if self.left else None
+        right = (
+            [_Segment(f'{" " * self.right}', style), _Segment.line()]
+            if self.right
+            else [_Segment.line()]
+        )
+        for ix, line in enumerate(lines):
+            if ix == 0:
+                yield first
+            else:
+                yield left
+            yield from line
+            yield from right
