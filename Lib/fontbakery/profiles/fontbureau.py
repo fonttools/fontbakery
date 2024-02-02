7a22eeefd5aaf9b8bcfdd09752e33a325043b259
Simon Cozens <simon@simon-cozens.org>
2024-02-02 13:58:42 +0000

    Port fontbureau profile

---
+++ Lib/fontbakery/profiles/fontbureau.py
@@ -0,0 +1,8 @@
+PROFILE = {
+    "include_profiles": ["universal"],
+    "sections": {
+        "Font Bureau Checks": [
+            "io.github.abysstypeco/check/ytlc_sanity",
+        ],
+    },
+}
