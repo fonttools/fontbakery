0544fd390a0c34ad4cdaf33d70b3a6380a961d6c
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-02-07 11:33:26 -0300

    move opentype checks to a subdirectory
    and reorganize tests directory structure accordingly

--- tests/checks/a_test_profile.py
+++ tests/checks/a_test_profile.py
@@ -1,4 +1,4 @@
 PROFILE = {
     "sections": {"Just a Test": ["com.google.fonts/check_for_testing/configuration"]},
-    "check_definitions": ["tests/profiles/a_test_profile_checks.py"],
+    "check_definitions": ["tests/checks/a_test_profile_checks.py"],
 }
