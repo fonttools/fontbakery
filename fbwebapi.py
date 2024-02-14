7d7806241f47f6c7fd02aa1c4ec855436b2db457
Simon Cozens <simon@simon-cozens.org>
2024-02-14 22:51:29 +0000

    Update to latest API

--- fbwebapi.py
+++ fbwebapi.py
@@ -4,6 +4,7 @@ from importlib import import_module
 import unicodedata
 import json
 
+
 sys.modules["unicodedata2"] = sys.modules["unicodedata"]
 for notreal in [
     "cmarkgfm",
@@ -27,16 +28,13 @@ for notreal in [
 
 import glyphsets
 
-import argparse
 from fontbakery.commands.check_profile import get_module, log_levels
+from fontbakery.fonts_profile import profile_factory, setup_context
 from fontbakery.reporters.serialize import SerializeReporter
 from fontbakery.checkrunner import (
     CheckRunner,
 )
-import fontbakery
-import pkgutil
 from importlib import import_module
-import re
 
 
 class ProgressReporter(SerializeReporter):
@@ -73,24 +71,11 @@ def run_fontbakery(
     full_lists=False,
 ):
     loglevels = [log_levels[loglevels]]
-    profile = import_module("fontbakery.profiles." + profilename).profile
-    argument_parser = argparse.ArgumentParser()
-    # Hack
-    argument_parser.add_argument(
-        "--list-checks",
-        default=False,
-        action="store_true",
-        help="List the checks available in the selected profile.",
-    )
-    values_keys = profile.setup_argparse(argument_parser)
-    args = argument_parser.parse_args(paths)
-    values = {}
-    for key in values_keys:
-        if hasattr(args, key):
-            values[key] = getattr(args, key)
+    profile = profile_factory(import_module("fontbakery.profiles." + profilename))
+    context = setup_context(paths)
     runner = CheckRunner(
         profile,
-        values=values,
+        context,
         config={
             "custom_order": None,
             "explicit_checks": checks,
@@ -102,43 +87,3 @@ def run_fontbakery(
     prog.runner = runner
     reporters = [prog]
     runner.run(reporters)
-
-
-def dump_all_the_checks():
-    checks = {}
-    profiles_modules = [
-        x.name
-        for x in pkgutil.walk_packages(fontbakery.__path__, "fontbakery.")
-        if x.name.startswith("fontbakery.profiles")
-    ]
-    for profile_name in profiles_modules:
-        try:
-            imported = import_module(profile_name, package=None)
-            profile = imported.profile
-        except BaseException:
-            continue
-        if not profile:
-            continue
-        profile_name = profile_name[20:]
-        for section in profile._sections.values():
-            for check in section._checks:
-                if check.id not in checks:
-                    checks[check.id] = {
-                        "sections": set(),
-                        "profiles": set(),
-                    }
-                checks[check.id]["sections"].add(section.name)
-                checks[check.id]["profiles"].add(profile_name)
-                for attr in ["proposal", "rationale", "severity", "description"]:
-                    if getattr(check, attr):
-                        md = getattr(check, attr)
-                        if attr == "rationale":
-                            md = re.sub(r"(?m)^\s+", "", md)
-                            checks[check.id][attr] = md
-                        else:
-                            checks[check.id][attr] = md
-
-    for ck in checks.values():
-        ck["sections"] = list(ck["sections"])
-        ck["profiles"] = list(ck["profiles"])
-    return checks
