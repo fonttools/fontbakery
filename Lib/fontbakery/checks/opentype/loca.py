16dd3302b4e2c262766d7e8804f8171d88d6876a
Simon Cozens <simon@simon-cozens.org>
2024-03-02 20:56:12 +0000

    Add various other check rationales
    com.google.fonts/check/maxadvancewidth
    com.google.fonts/check/loca/maxp_num_glyphs
    com.google.fonts/check/name/no_copyright_on_description
    com.adobe.fonts/check/postscript_name
    com.google.fonts/check/family_naming_recommendations
    On the Open Type Profile.
    
    (PR #4570)

--- Lib/fontbakery/checks/opentype/loca.py
+++ Lib/fontbakery/checks/opentype/loca.py
@@ -7,6 +7,13 @@ from fontbakery.message import Message
     id="com.google.fonts/check/loca/maxp_num_glyphs",
     conditions=["is_ttf"],
     proposal="legacy:check/180",
+    rationale="""
+        The 'maxp' table contains various statistics about the font, including the
+        number of glyphs in the font. The 'loca' table contains the offsets to the
+        locations of the glyphs in the font. The number of offsets in the 'loca' table
+        should match the number of glyphs in the 'maxp' table. A failure here indicates
+        a problem with the font compiler.
+    """,
 )
 def com_google_fonts_check_loca_maxp_num_glyphs(ttFont):
     """Does the number of glyphs in the loca table match the maxp table?"""
