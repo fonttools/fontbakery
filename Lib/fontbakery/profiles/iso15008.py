0a731fb5105ce6ebe00cd9e1d939b9f43e69c436
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-03-15 09:04:04 -0300

    sort check-ID lists on profile definitions
    (ignoring the reverse domain prefixes)

--- Lib/fontbakery/profiles/iso15008.py
+++ Lib/fontbakery/profiles/iso15008.py
@@ -1,11 +1,11 @@
 PROFILE = {
     "sections": {
         "ISO15008": [
+            "com.google.fonts/check/iso15008_intercharacter_spacing",
+            "com.google.fonts/check/iso15008_interline_spacing",
+            "com.google.fonts/check/iso15008_interword_spacing",
             "com.google.fonts/check/iso15008_proportions",
             "com.google.fonts/check/iso15008_stem_width",
-            "com.google.fonts/check/iso15008_intercharacter_spacing",
-            "com.google.fonts/check/iso15008_interword_spacing",
-            "com.google.fonts/check/iso15008_interline_spacing",
         ]
     }
 }
