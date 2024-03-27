76ef06c5c34c90070b61078e2caa84c8c83ab05e
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-03-26 21:15:20 -0300

    simplify PASS results - googlefonts profile (license)
    (issue #4612)

--- Lib/fontbakery/checks/googlefonts/license.py
+++ Lib/fontbakery/checks/googlefonts/license.py
@@ -109,8 +109,6 @@ def com_google_fonts_check_family_has_license(licenses, config):
             " upstream repo, which is fine, just make sure"
             " there is a temporary license file in the same folder.",
         )
-    else:
-        yield PASS, f"Found license at '{licenses[0]}'"
 
 
 @check(
@@ -129,10 +127,7 @@ def com_google_fonts_check_license_OFL_copyright(license_contents):
     import re
 
     string = license_contents.strip().split("\n")[0].lower()
-    does_match = re.search(EXPECTED_COPYRIGHT_PATTERN, string)
-    if does_match:
-        yield PASS, "looks good"
-    else:
+    if not re.search(EXPECTED_COPYRIGHT_PATTERN, string):
         yield FAIL, Message(
             "bad-format",
             f"First line in license file is:\n\n"
@@ -190,8 +185,6 @@ def com_google_fonts_check_license_OFL_body_text(license_contents):
             "Lines changed:\n\n"
             f"{output}\n\n",
         )
-    else:
-        yield PASS, "OFL license body text is correct"
 
 
 @check(
@@ -227,7 +220,6 @@ def com_google_fonts_check_name_license(ttFont, license_filename):
     """Check copyright namerecords match license file."""
     from fontbakery.constants import PLACEHOLDER_LICENSING_TEXT
 
-    passed = True
     http_warn = False
     placeholder = PLACEHOLDER_LICENSING_TEXT[license_filename]
     entry_found = False
@@ -247,7 +239,6 @@ def com_google_fonts_check_name_license(ttFont, license_filename):
                 http_warn = True
 
             if "scripts.sil.org/OFL" in value:
-                passed = True
                 yield WARN, Message(
                     "old-url",
                     "Please consider updating the url from "
@@ -256,7 +247,6 @@ def com_google_fonts_check_name_license(ttFont, license_filename):
                 )
                 return
             if value != placeholder:
-                passed = False
                 yield FAIL, Message(
                     "wrong",
                     f"License file {license_filename} exists but"
@@ -282,8 +272,6 @@ def com_google_fonts_check_name_license(ttFont, license_filename):
             f" (LICENSE DESCRIPTION). A proper licensing"
             f" entry must be set.",
         )
-    elif passed:
-        yield PASS, "Licensing entry on name table is correctly set."
 
 
 @check(
@@ -457,12 +445,7 @@ def com_google_fonts_check_name_license_url(ttFont, familyname):
 def com_google_fonts_check_metadata_license(family_metadata):
     """METADATA.pb license is "APACHE2", "UFL" or "OFL"?"""
     expected_licenses = ["APACHE2", "OFL", "UFL"]
-    if family_metadata.license in expected_licenses:
-        yield PASS, (
-            f"Font license is declared in METADATA.pb"
-            f' as "{family_metadata.license}"'
-        )
-    else:
+    if not family_metadata.license in expected_licenses:
         yield FAIL, Message(
             "bad-license",
             f'METADATA.pb license field ("{family_metadata.license}")'
@@ -493,8 +476,6 @@ def com_google_fonts_check_epar(ttFont):
             "EPAR table not present in font. To learn more see"
             " https://github.com/fonttools/fontbakery/issues/818",
         )
-    else:
-        yield PASS, "EPAR table present in font."
 
 
 # Although this is a /name/ check, it's really about licensing
@@ -518,7 +499,6 @@ def com_google_fonts_check_epar(ttFont):
 )
 def com_google_fonts_check_name_rfn(ttFont, familyname):
     """Name table strings must not contain the string 'Reserved Font Name'."""
-    ok = True
     for entry in ttFont["name"].names:
         string = entry.toUnicode()
         if "This license is copied below, and is also available with a FAQ" in string:
@@ -549,7 +529,3 @@ def com_google_fonts_check_name_rfn(ttFont, familyname):
                     f" from the currently used family name ({familyname}),"
                     f" which is fine.",
                 )
-            ok = False
-
-    if ok:
-        yield PASS, 'None of the name table strings contain "Reserved Font Name".'
