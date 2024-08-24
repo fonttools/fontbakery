cabc32c9281f4aaa482b19b0fa526905f2d207c6
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-08-24 12:23:53 -0300

    move test_check_unwanted_aat_tables out of googlefonts_test.py

---
+++ tests/checks/tables_test.py
@@ -0,0 +1,56 @@
+from fontTools.ttLib import TTFont
+
+from fontbakery.codetesting import (
+    TEST_FILE,
+    CheckTester,
+    assert_PASS,
+    assert_results_contain,
+)
+from fontbakery.status import FAIL
+
+
+def test_check_unwanted_aat_tables():
+    """Are there unwanted Apple tables ?"""
+    check = CheckTester("unwanted_aat_tables")
+
+    unwanted_tables = [
+        "EBSC",
+        "Zaph",
+        "acnt",
+        "ankr",
+        "bdat",
+        "bhed",
+        "bloc",
+        "bmap",
+        "bsln",
+        "fdsc",
+        "feat",
+        "fond",
+        "gcid",
+        "just",
+        "kerx",
+        "lcar",
+        "ltag",
+        "mort",
+        "morx",
+        "opbd",
+        "prop",
+        "trak",
+        "xref",
+    ]
+    # Our reference Mada Regular font is good here:
+    ttFont = TTFont(TEST_FILE("mada/Mada-Regular.ttf"))
+
+    # So it must PASS the check:
+    assert_PASS(check(ttFont), "with a good font...")
+
+    # We now add unwanted tables one-by-one to validate the FAIL code-path:
+    for unwanted in unwanted_tables:
+        ttFont = TTFont(TEST_FILE("mada/Mada-Regular.ttf"))
+        ttFont.reader.tables[unwanted] = "foo"
+        assert_results_contain(
+            check(ttFont),
+            FAIL,
+            "has-unwanted-tables",
+            f"with unwanted table {unwanted} ...",
+        )
