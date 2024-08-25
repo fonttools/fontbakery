37b521fc9f53cbcdd1851686d9ab12c1b2c7756b
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-08-25 05:08:05 -0300

    move stylisticset_description out of googlefonts directory

---
+++ tests/test_checks_stylistic_sets.py
@@ -0,0 +1,15 @@
+# from fontTools.ttLib import TTFont
+#
+# from fontbakery.codetesting import (
+#     assert_PASS,
+#     assert_results_contain,
+#     CheckTester,
+#     TEST_FILE,
+# )
+# from fontbakery.status import WARN
+
+
+def test_check_stylisticset_description():
+    """Ensure Stylistic Sets have description."""
+    # check = CheckTester("stylisticset_description")
+    # TODO: Implement-me!
