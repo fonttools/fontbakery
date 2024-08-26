fb6e55d21a87817b69bc6065011ebb1490b9dae3
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-08-25 23:30:55 -0300

    move cmap/format_12 out of notofonts directory

---
+++ tests/test_checks_cmap.py
@@ -0,0 +1,8 @@
+def NOT_IMPLEMENTED_test_check_cmap_format_12():
+    """Check that format 12 cmap subtables are correctly constituted."""
+    # check = CheckTester("cmap/format_12")
+    # TODO: Implement-me!
+    #
+    # code-paths:
+    # - FAIL, ""
+    # - PASS
