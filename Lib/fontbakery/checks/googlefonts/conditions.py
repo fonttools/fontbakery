949c45e0d930358a7552fadd43b121a07531d532
Marc Foley <m.foley.88@gmail.com>
2024-09-05 14:47:05 +0100

    remote_styles: retry getting best family name

--- Lib/fontbakery/checks/googlefonts/conditions.py
+++ Lib/fontbakery/checks/googlefonts/conditions.py
@@ -181,6 +181,11 @@ def familyname(font):
 
 
 @condition(Font)
+def best_familyname(font):
+    return font.ttFont["name"].getBestFamilyName()
+
+
+@condition(Font)
 def listed_on_gfonts_api(font):
     if not font.context.network:
         return
@@ -280,7 +285,7 @@ def remote_styles(font):
 
     # download_family_from_Google_Fonts
     dl_url = "https://fonts.google.com/download/list?family={}"
-    family = font.familyname
+    family = font.best_familyname
     url = dl_url.format(family.replace(" ", "%20"))
     data = json.loads(requests.get(url, timeout=10).text[5:])
     remote_fonts = []
