6986c2eaa58f4e1c9a86449272d51c09ab28942b
Guido Ferreyra <guido@tipografia.com.ar>
2024-09-18 18:06:09 -0300

    Remove leftover from deprecated 'typenetwork/fstype' check.
    On the TypeNetwork profile.
    
    (PR #4878)

--- Lib/fontbakery/checks/vendorspecific/typenetwork.py
+++ Lib/fontbakery/checks/vendorspecific/typenetwork.py
@@ -930,69 +930,6 @@ def check_family_valid_strikeout(ttFont):
 
 
 @check(
-    id="typenetwork/fstype",
-    rationale="""
-        The fsType in the OS/2 table is a legacy DRM-related field.
-        Type Network's EULA is more accurately represented by setting it to 4,
-        also known as 'Print & Preview'.
-
-        This setting indicates that the fonts may be embedded, and temporarily loaded
-        on the remote system, but documents that use it must not be editable.
-
-        More detailed info is available at:
-        https://docs.microsoft.com/en-us/typography/opentype/spec/os2#fstype
-    """,
-    proposal=["https://github.com/fonttools/fontbakery/pull/4260"],
-)
-def check_fstype(ttFont):
-    """Checking OS/2 fsType does not impose restrictions."""
-    value = ttFont["OS/2"].fsType
-
-    FSTYPE_RESTRICTIONS = {
-        0x0002: (
-            "* The font must not be modified, embedded or exchanged in"
-            " any manner without first obtaining permission of"
-            " the legal owner."
-        ),
-        0x0004: (
-            "The font may be embedded, and temporarily loaded on the"
-            " remote system, but documents that use it must"
-            " not be editable."
-        ),
-        0x0008: (
-            "The font may be embedded but must only be installed"
-            " temporarily on other systems."
-        ),
-        0x0100: ("The font may not be subsetted prior to embedding."),
-        0x0200: (
-            "Only bitmaps contained in the font may be embedded."
-            " No outline data may be embedded."
-        ),
-    }
-    restrictions = ""
-    for bit_mask in FSTYPE_RESTRICTIONS.keys():
-        if value & bit_mask:
-            restrictions += FSTYPE_RESTRICTIONS[bit_mask]
-
-    if value & 0b1111110011110001:
-        restrictions += (
-            "* There are reserved bits set, which indicates an invalid setting."
-        )
-
-    if value != 0x0004:
-        yield WARN, Message(
-            "no-preview-print",
-            f"In this font fsType is set to {value} meaning that:\n"
-            f"{restrictions}\n"
-            "\n"
-            "TN advises setting the fsType to bit 4, Print & Preview, which"
-            "matches TN’s EULA.",
-        )
-    else:
-        yield PASS, "OS/2 fsType is properly set to 'Print & Preview'."
-
-
-@check(
     id="typenetwork/composite_glyphs",
     rationale="""
         For performance reasons, it is recommended that TTF fonts use composite glyphs.
