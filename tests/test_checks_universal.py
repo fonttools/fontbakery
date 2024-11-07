1361bb7510187adfc889ae79701e57ad6c2e2be1
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-11-07 10:42:57 -0300

    'linegaps' code-test => test_checks_metrics.py

--- tests/test_checks_universal.py
+++ tests/test_checks_universal.py
@@ -724,36 +724,6 @@ def test_check_soft_hyphen(montserrat_ttFonts):
         assert_PASS(check(ttFont))
 
 
-def test_check_linegaps():
-    """Checking Vertical Metric Linegaps."""
-    check = CheckTester("linegaps")
-
-    # Our reference Mada Regular is know to be bad here.
-    ttFont = TTFont(TEST_FILE("mada/Mada-Regular.ttf"))
-
-    # But just to be sure, we first explicitely set
-    # the values we're checking for:
-    ttFont["hhea"].lineGap = 1
-    ttFont["OS/2"].sTypoLineGap = 0
-    assert_results_contain(check(ttFont), WARN, "hhea", "with non-zero hhea.lineGap...")
-
-    # Then we run the check with a non-zero OS/2.sTypoLineGap:
-    ttFont["hhea"].lineGap = 0
-    ttFont["OS/2"].sTypoLineGap = 1
-    assert_results_contain(
-        check(ttFont), WARN, "OS/2", "with non-zero OS/2.sTypoLineGap..."
-    )
-
-    # And finaly we fix it by making both values equal to zero:
-    ttFont["hhea"].lineGap = 0
-    ttFont["OS/2"].sTypoLineGap = 0
-    assert_PASS(check(ttFont))
-
-    # Confirm the check yields FAIL if the font doesn't have a required table
-    del ttFont["OS/2"]
-    assert_results_contain(check(ttFont), FAIL, "lacks-table")
-
-
 def test_check_STAT_in_statics():
     """Checking STAT table on static fonts."""
     check = CheckTester("STAT_in_statics")
