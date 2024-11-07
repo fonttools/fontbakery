e1bea19eb378e9c9793ad60faa65e2d8cd6f2b8f
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-11-07 00:27:38 -0300

    split out: 'legacy_accents' check

--- tests/test_checks_universal.py
+++ tests/test_checks_universal.py
@@ -409,30 +409,6 @@ def test_check_mandatory_glyphs():
     )
 
 
-def test_check_legacy_accents():
-    """Check that legacy accents aren't used in composite glyphs."""
-    check = CheckTester("legacy_accents")
-
-    test_font = TTFont(TEST_FILE("montserrat/Montserrat-Regular.ttf"))
-    assert_PASS(check(test_font))
-
-    test_font = TTFont(TEST_FILE("mada/Mada-Regular.ttf"))
-    assert_results_contain(
-        check(test_font),
-        FAIL,
-        "legacy-accents-gdef",
-        "for legacy accents being defined in GDEF as marks.",
-    )
-
-    test_font = TTFont(TEST_FILE("lugrasimo/Lugrasimo-Regular.ttf"))
-    assert_results_contain(
-        check(test_font),
-        FAIL,
-        "legacy-accents-width",
-        "for legacy accents having zero width.",
-    )
-
-
 def test_check_required_tables():
     """Font contains all required tables ?"""
     check = CheckTester("required_tables")
