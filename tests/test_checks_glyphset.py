2a43c3f92115d4c5471397ee35b492ac1c6e328f
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-11-12 09:21:13 -0300

    update implementation of 'control_chars' check
    to handle single font files instead of a whole family.
    
    On the Universal profile.
    
    (issue #4896)

--- tests/test_checks_glyphset.py
+++ tests/test_checks_glyphset.py
@@ -38,44 +38,19 @@ def test_check_family_control_chars(check):
     good_font = TEST_FILE(
         "bad_character_set/control_chars/FontbakeryTesterCCGood-Regular.ttf"
     )
-    onebad_cc_font = TEST_FILE(
+    bad_font = TEST_FILE(
         "bad_character_set/control_chars/FontbakeryTesterCCOneBad-Regular.ttf"
     )
-    multibad_cc_font = TEST_FILE(
-        "bad_character_set/control_chars/FontbakeryTesterCCMultiBad-Regular.ttf"
-    )
 
-    # No unacceptable control characters should pass with one file
-    fonts = [good_font]
-    assert_PASS(check(fonts), "with one good font...")
+    # No unacceptable control characters should pass
+    assert_PASS(check(good_font), "with a good font...")
 
-    # No unacceptable control characters should pass with multiple good files
-    fonts = [good_font, good_font]
-    assert_PASS(check(fonts), "with multiple good fonts...")
-
-    # Unacceptable control chars should fail with one file x one bad char in font
-    fonts = [onebad_cc_font]
+    # Unacceptable control chars should fail
     assert_results_contain(
-        check(fonts), FAIL, "unacceptable", "with one bad font that has one bad char..."
-    )
-
-    # Unacceptable control chars should fail with one file x multiple bad char in font
-    fonts = [multibad_cc_font]
-    assert_results_contain(
-        check(fonts),
+        check(bad_font),
         FAIL,
         "unacceptable",
-        "with one bad font that has multiple bad char...",
-    )
-
-    # Unacceptable control chars should fail with multiple files x multiple bad chars
-    # in fonts
-    fonts = [onebad_cc_font, multibad_cc_font]
-    assert_results_contain(
-        check(fonts),
-        FAIL,
-        "unacceptable",
-        "with multiple bad fonts that have multiple bad chars...",
+        "with a bad font that has one bad char...",
     )
 
 
