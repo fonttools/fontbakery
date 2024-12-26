a789ffa2b26348e385a16b775ad0a4a4a65e8958
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2024-12-26 13:30:16 -0300

    Merge opentype/fsselection_matches_macstyle into opentype/fsselection
    Backporting from FontSpector.
    (issue #4865)

--- Lib/fontbakery/checks/opentype/fsselection.py
+++ Lib/fontbakery/checks/opentype/fsselection.py
@@ -1,3 +1,4 @@
+from fontbakery.constants import FsSelection, MacStyle
 from fontbakery.prelude import check, Message, FAIL
 
 
@@ -5,70 +6,25 @@ from fontbakery.prelude import check, Message, FAIL
     id="opentype/fsselection",
     conditions=["style"],
     rationale="""
-    The OS/2.fsSelection field is a bit field used to specify the stylistic
-    qualities of the font - in particular, it specifies to some operating
-    systems whether the font is italic (bit 0), bold (bit 5) or regular
-    (bit 6).
+        The OS/2.fsSelection field is a bit field used to specify the stylistic
+        qualities of the font - in particular, it specifies to some operating
+        systems whether the font is italic (bit 0), bold (bit 5) or regular
+        (bit 6).
 
-    This check verifies that the fsSelection field is set correctly for the
-    font style. For a family of static fonts created in GlyphsApp, this is
-    set by using the style linking checkboxes in the exports settings.
+        This check verifies that the fsSelection field is set correctly for the
+        font style. For a family of static fonts created in GlyphsApp, this is
+        set by using the style linking checkboxes in the exports settings.
+
+        Additionally, the bold and italic bits in OS/2.fsSelection must match the
+        bold and italic bits in head.macStyle per the OpenType spec.
     """,
-    proposal="https://github.com/fonttools/fontbakery/issues/4829",  # legacy check
+    proposal=[
+        "https://github.com/fonttools/fontbakery/issues/4829",  # legacy check
+        "https://github.com/fonttools/fontbakery/pull/2382",
+    ],
 )
 def check_fsselection(ttFont, style):
     """Checking OS/2 fsSelection value."""
-    from fontbakery.constants import RIBBI_STYLE_NAMES, STATIC_STYLE_NAMES, FsSelection
-    from fontbakery.utils import check_bit_entry
-
-    # Checking fsSelection REGULAR bit:
-    expected = "Regular" in style or (
-        style in STATIC_STYLE_NAMES
-        and style not in RIBBI_STYLE_NAMES
-        and "Italic" not in style
-    )
-    yield check_bit_entry(
-        ttFont,
-        "OS/2",
-        "fsSelection",
-        expected,
-        bitmask=FsSelection.REGULAR,
-        bitname="REGULAR",
-    )
-
-    # Checking fsSelection ITALIC bit:
-    expected = "Italic" in style
-    yield check_bit_entry(
-        ttFont,
-        "OS/2",
-        "fsSelection",
-        expected,
-        bitmask=FsSelection.ITALIC,
-        bitname="ITALIC",
-    )
-
-    # Checking fsSelection BOLD bit:
-    expected = style in ["Bold", "BoldItalic"]
-    yield check_bit_entry(
-        ttFont,
-        "OS/2",
-        "fsSelection",
-        expected,
-        bitmask=FsSelection.BOLD,
-        bitname="BOLD",
-    )
-
-
-@check(
-    id="opentype/fsselection_matches_macstyle",
-    rationale="""
-        The bold and italic bits in OS/2.fsSelection must match the bold and italic
-        bits in head.macStyle per the OpenType spec.
-    """,
-    proposal="https://github.com/fonttools/fontbakery/pull/2382",
-)
-def check_fsselection_matches_macstyle(ttFont):
-    """Check if OS/2 fsSelection matches head macStyle bold and italic bits."""
 
     # Check both OS/2 and head are present.
     missing_tables = False
@@ -81,23 +37,34 @@ def check_fsselection_matches_macstyle(ttFont):
     if missing_tables:
         return
 
-    from fontbakery.constants import FsSelection, MacStyle
+    bold_expected = style in ["Bold", "BoldItalic"]
+    italic_expected = "Italic" in style
+    regular_expected = (not bold_expected) and (not italic_expected)
+    bold_seen = bool(ttFont["OS/2"].fsSelection & FsSelection.BOLD)
+    italic_seen = bool(ttFont["OS/2"].fsSelection & FsSelection.ITALIC)
+    regular_seen = bool(ttFont["OS/2"].fsSelection & FsSelection.REGULAR)
 
-    head_bold = (ttFont["head"].macStyle & MacStyle.BOLD) != 0
-    os2_bold = (ttFont["OS/2"].fsSelection & FsSelection.BOLD) != 0
-    if head_bold != os2_bold:
-        yield FAIL, Message(
-            "fsselection-macstyle-bold",
-            "The OS/2.fsSelection and head.macStyle bold settings do not match:\n\n"
-            f"* OS/2.fsSelection: BOLD is {'not ' if not os2_bold else ''}set\n"
-            f"* head.macStyle: BOLD is {'not ' if not head_bold else ''}set",
-        )
-    head_italic = (ttFont["head"].macStyle & MacStyle.ITALIC) != 0
-    os2_italic = (ttFont["OS/2"].fsSelection & FsSelection.ITALIC) != 0
-    if head_italic != os2_italic:
-        yield FAIL, Message(
-            "fsselection-macstyle-italic",
-            "The OS/2.fsSelection and head.macStyle italic settings do not match.\n\n"
-            f"* OS/2.fsSelection: ITALIC is {'not ' if not os2_italic else ''}set\n"
-            f"* head.macStyle: ITALIC is {'not ' if not head_italic else ''}set",
-        )
+    for flag, expected, label in [
+        (bold_seen, bold_expected, "Bold"),
+        (italic_seen, italic_expected, "Italic"),
+        (regular_seen, regular_expected, "Regular"),
+    ]:
+        if flag != expected:
+            yield FAIL, Message(
+                f"bad-{label.upper()}",
+                f"fsSelection {label} flag {flag}"
+                f" does not match font style {style}",
+            )
+
+    mac_bold = bool(ttFont["head"].macStyle & MacStyle.BOLD)
+    mac_italic = bool(ttFont["head"].macStyle & MacStyle.ITALIC)
+    for flag, expected, label in [
+        (bold_seen, mac_bold, "Bold"),
+        (italic_seen, mac_italic, "Italic"),
+    ]:
+        if flag != expected:
+            yield FAIL, Message(
+                f"fsselection-macstyle-{label.lower()}",
+                f"fsSelection {label} flag {flag}"
+                f" does not match macStyle {expected} flag",
+            )
