bee93e20fb7aa0234f75f06631db50d453856f25
Felipe Corrêa da Silva Sanches <juca@members.fsf.org>
2025-01-16 14:27:43 -0300

    update shaperglot dependency

--- Lib/fontbakery/checks/soft_dotted.py
+++ Lib/fontbakery/checks/soft_dotted.py
@@ -1,5 +1,10 @@
+import itertools
+
+from beziers.path import BezierPath
+from fontTools import unicodedata
+from vharfbuzz import Vharfbuzz
+
 from fontbakery.prelude import check, Message, PASS, WARN, SKIP
-from fontbakery.utils import exit_with_install_instructions
 
 
 @check(
@@ -25,18 +30,10 @@ from fontbakery.utils import exit_with_install_instructions
 def check_soft_dotted(ttFont):
     """Ensure soft_dotted characters lose their dot when combined with marks that
     replace the dot."""
-    try:
-        from vharfbuzz import Vharfbuzz
-    except ImportError:
-        exit_with_install_instructions("shaping")
-
-    import itertools
-    from beziers.path import BezierPath
-    from fontTools import unicodedata
 
     cmap = ttFont["cmap"].getBestCmap()
 
-    # Soft dotted strings know to be used in orthographies.
+    # Soft dotted strings known to be used in orthographies.
     ortho_soft_dotted_strings = set(
         "i̋ i̍ i᷆ i᷇ i̓ i̊ i̐ ɨ́ ɨ̀ ɨ̂ ɨ̋ ɨ̏ ɨ̌ ɨ̄ ɨ̃ ɨ̈ ɨ̧́ ɨ̧̀ ɨ̧̂ ɨ̧̌ ɨ̱́ ɨ̱̀ ɨ̱̈ "
         "į́ į̀ į̂ į̄ į̄́ į̄̀ į̄̂ į̄̌ į̃ į̌ ị́ ị̀ ị̂ ị̄ ị̃ ḭ́ ḭ̀ ḭ̄ j́ j̀ j̄ j̑ j̃ "
@@ -138,53 +135,6 @@ def check_soft_dotted(ttFont):
             f"The dot of soft dotted characters _should_ disappear in"
             f" other cases, for example: {warn_unchanged_strings}"
         )
-
-    # Calculate font's affected languages for additional information
-    if fail_unchanged_strings or warn_unchanged_strings:
-        from shaperglot.checker import Checker
-        from shaperglot.languages import Languages, gflangs
-
-        languages = Languages()
-
-        # Find all affected languages
-        ortho_soft_dotted_langs = set()
-        for c in ortho_soft_dotted_strings:
-            for lang in gflangs:
-                if (
-                    c in gflangs[lang].exemplar_chars.base
-                    or c in gflangs[lang].exemplar_chars.auxiliary
-                ):
-                    ortho_soft_dotted_langs.add(lang)
-        if ortho_soft_dotted_langs:
-            affected_languages = []
-            unaffected_languages = []
-            languages = Languages()
-            checker = Checker(ttFont.reader.file.name)
-
-            for lang in ortho_soft_dotted_langs:
-                reporter = checker.check(languages[lang])
-                string = (
-                    f"{gflangs[lang].name} ({gflangs[lang].script}, "
-                    f"{'{:,.0f}'.format(gflangs[lang].population)} speakers)"
-                )
-                if reporter.is_success:
-                    affected_languages.append(string)
-                else:
-                    unaffected_languages.append(string)
-
-            if affected_languages:
-                affected_languages = ", ".join(affected_languages)
-                message += (
-                    f"\n\nYour font fully covers the following languages that require"
-                    f" the soft-dotted feature: {affected_languages}. "
-                )
-
-            if unaffected_languages:
-                unaffected_languages = ", ".join(unaffected_languages)
-                message += (
-                    f"\n\nYour font does *not* cover the following languages that"
-                    f" require the soft-dotted feature: {unaffected_languages}."
-                )
 
     if fail_unchanged_strings or warn_unchanged_strings:
         yield WARN, Message("soft-dotted", message)
