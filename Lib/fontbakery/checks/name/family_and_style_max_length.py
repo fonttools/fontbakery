e051253491c9b224f6acbc76dcdbca522ae24ad5
Stephen Nixon <arrowtypeco@gmail.com>
2025-07-08 14:52:27 -0400

    slight refactor: eliminate magic numbers and unneeded try/except block

--- Lib/fontbakery/checks/name/family_and_style_max_length.py
+++ Lib/fontbakery/checks/name/family_and_style_max_length.py
@@ -16,17 +16,17 @@ from fontbakery.utils import get_name_entry_strings
         Background on length limit (credit to Aaron Bell at google/fonts/issues/9185):
 
         The font dropdown in Microsoft Office on PC is driven by the older
-        GDI font enumeration and rendering technology. GDI uses LOGFONTA 
-        (https://learn.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-logfonta) 
-        to define the attributes of a font, and CHAR lfFaceName[LF_FACESIZE]; 
-        to define the font name. The problem, though, is that the string lfFaceName 
+        GDI font enumeration and rendering technology. GDI uses LOGFONTA
+        (https://learn.microsoft.com/en-us/windows/win32/api/wingdi/ns-wingdi-logfonta)
+        to define the attributes of a font, and CHAR lfFaceName[LF_FACESIZE];
+        to define the font name. The problem, though, is that the string lfFaceName
         is restricted to 32 characters, including the terminating NULL.
 
-        In a variable font where the master location is at a location with a 
-        significant number of characters, say, “ExtraLight”, name ID 1 can become 
-        quite long (eg: “Chiron Hei HK ExtraLight”). However, in determining the 
-        font name, Microsoft will prioritize name ID 16 over name ID 1. So this 
-        lets one reduce the character count back to the standard font name 
+        In a variable font where the master location is at a location with a
+        significant number of characters, say, “ExtraLight”, name ID 1 can become
+        quite long (eg: “Chiron Hei HK ExtraLight”). However, in determining the
+        font name, Microsoft will prioritize name ID 16 over name ID 1. So this
+        lets one reduce the character count back to the standard font name
         (eg: “Chiron Hei HK”).
     """,
     proposal=[
@@ -40,12 +40,17 @@ def check_name_family_and_style_max_length(ttFont):
     def strip_ribbi(x):
         ribbi_re = " (" + "|".join(RIBBI_STYLE_NAMES) + ")$"
         return re.sub(ribbi_re, "", x)
+    
+    # constants for name length limits
+    NAME_LENGTH_LIMIT = 31
+    PSNAME_LENGTH_LIMIT = 27
+    ELIDABLE_FLAG = 2
 
     checks = [
         [
             FAIL,
             NameID.FONT_FAMILY_NAME,
-            31,
+            NAME_LENGTH_LIMIT,
             "cause a fallback font to appear for some accented letters, as well"
             " as in some scripts such as Thai, in"
             " Microsoft Word on Windows 10 and 11. It can also lead to names"
@@ -55,7 +60,7 @@ def check_name_family_and_style_max_length(ttFont):
         [
             WARN,
             NameID.POSTSCRIPT_NAME,
-            27,
+            PSNAME_LENGTH_LIMIT,
             "with PostScript printers, especially on Mac platforms. Per Thomas"
             " Phinney, this is likely limited to classic versions "
             " of Mac OS, pre-OS X (released in the year 2000).\n\n",
@@ -79,14 +84,14 @@ def check_name_family_and_style_max_length(ttFont):
         # NOTE: NameID 21 might be prefered by MS Word, but it is not known.
         # if it is used, we could use fonttools .getBestFamilyName() instead
         # of the try/except block below.
-        try:
+        if ttFont["name"].getName(NameID.TYPOGRAPHIC_FAMILY_NAME, 3, 1, 0x409):
             family_name = (
                 ttFont["name"]
                 .getName(NameID.TYPOGRAPHIC_FAMILY_NAME, 3, 1, 0x409)
                 .toUnicode()
             )
             family_name_id = NameID.TYPOGRAPHIC_FAMILY_NAME
-        except AttributeError:
+        else:
             family_name = (
                 ttFont["name"].getName(NameID.FONT_FAMILY_NAME, 3, 1, 0x409).toUnicode()
             )
@@ -95,7 +100,7 @@ def check_name_family_and_style_max_length(ttFont):
         styles_per_axis = defaultdict(list)
         for value in ttFont["STAT"].table.AxisValueArray.AxisValue:
             # if the value is marked as elidable, don’t count it
-            if value.Flags & 2:
+            if value.Flags & ELIDABLE_FLAG:
                 continue
             # otherwise, get the STAT style particle name and add it to the list
             styles_per_axis[value.AxisIndex].append(
@@ -109,7 +114,7 @@ def check_name_family_and_style_max_length(ttFont):
         ]
 
         for name in names:
-            if len(name) > 31:
+            if len(name) > NAME_LENGTH_LIMIT:
                 stat_style_combination = name.replace(f"{family_name} ", "")
                 yield FAIL, Message(
                     "familyname-plus-stat-entries-too-long",
@@ -132,12 +137,12 @@ def check_name_family_and_style_max_length(ttFont):
                 typo_family_names = {
                     (r.platformID, r.platEncID, r.langID): r
                     for r in ttFont["name"].names
-                    if r.nameID == 16
+                    if r.nameID == NameID.TYPOGRAPHIC_FAMILY_NAME
                 }
                 family_names = {
                     (r.platformID, r.platEncID, r.langID): r
                     for r in ttFont["name"].names
-                    if r.nameID == 1
+                    if r.nameID == NameID.FONT_FAMILY_NAME
                 }
                 for platform in family_names:
                     if platform in typo_family_names:
@@ -145,14 +150,14 @@ def check_name_family_and_style_max_length(ttFont):
                     else:
                         family_name = family_names[platform].toUnicode()
                     full_instance_name = family_name + " " + instance_name
-                    if len(full_instance_name) > 31:
+                    if len(full_instance_name) > NAME_LENGTH_LIMIT:
                         yield FAIL, Message(
                             "fvar-instance-too-long",
                             f"Variable font instance name '{full_instance_name}'"
                             f" formed by space-separated concatenation of"
                             f" font family name (nameID {NameID.FONT_FAMILY_NAME})"
                             f" and instance subfamily nameID {instance.subfamilyNameID}"
-                            f" exceeds 31 characters.\n\n"
+                            f" exceeds {NAME_LENGTH_LIMIT} characters.\n\n"
                             f" This has been found to"
                             f" cause a fallback font to appear for some accented letters, as well"
                             f" as in some scripts such as Thai, in"
