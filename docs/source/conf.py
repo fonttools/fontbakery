427cf95fcb72bb42c88fc33b1e325f35b1c5318c
Felipe Correa da Silva Sanches <juca@members.fsf.org>
2025-10-02 15:16:36 -0300

    update version on docs/source/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,9 +23,9 @@ copyright = "2025 The Font Bakery Authors"
 author = "The Font Bakery Authors"
 
 # The short X.Y version
-version = "1.0"
+version = "1.1"
 # The full version, including alpha/beta/rc tags
-release = "1.0.1"
+release = "1.1.0"
 
 
 # -- General configuration ---------------------------------------------------
